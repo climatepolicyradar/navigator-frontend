184c439e614b47dfae069a8b156e5dea24912305
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2023-02-10 11:40:14 +0000

    Fix useMutation async bug. Remove legacy hooks

--- src/hooks/useUpdateCountries.ts
+++ src/hooks/useUpdateCountries.ts
@@ -1,23 +1,15 @@
-import { useMutation, useQueryClient } from 'react-query';
+import { useMutation, useQueryClient } from "react-query";
 
 export default function useUpdateCountries() {
   const queryClient = useQueryClient();
 
-  return useMutation(
-    (value?: any) => {
-      const { regionName, regions, countries } = value;
-      const region = regions.find((item) => item.display_value === regionName);
-      let newList = countries;
-      if (region) {
-        newList = countries.filter((item: any) => item.parent_id === region.id);
-      }
-      return queryClient.setQueryData('filteredCountries', (old) => newList);
-    },
-
-    {
-      onError: (err) => {
-        console.log(err);
-      },
+  return useMutation(async (value?: any) => {
+    const { regionName, regions, countries } = value;
+    const region = regions.find((item) => item.slug === regionName);
+    let newList = countries;
+    if (region) {
+      newList = countries.filter((item: any) => item.parent_id === region.id);
     }
-  );
+    return queryClient.setQueryData("filteredCountries", () => newList);
+  });
 }
--- src/hooks/useUpdateSearchCriteria.ts
+++ src/hooks/useUpdateSearchCriteria.ts
@@ -1,20 +1,12 @@
-import { useMutation, useQueryClient } from 'react-query';
+import { useMutation, useQueryClient } from "react-query";
 
 export default function useUpdateSearchCriteria() {
   const queryClient = useQueryClient();
 
-  return useMutation(
-    (value: any) => {
-      return queryClient.setQueryData('searchCriteria', (old) => ({
-        ...old,
-        ...value,
-      }));
-    },
-
-    {
-      onError: (err) => {
-        console.log(err);
-      },
-    }
-  );
+  return useMutation(async (value: any) => {
+    return queryClient.setQueryData("searchCriteria", (old: any) => ({
+      ...old,
+      ...value,
+    }));
+  });
 }
