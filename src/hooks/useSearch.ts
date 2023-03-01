dffcc397c2bb655c0dc838290becc1366b40b043
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2023-03-01 15:58:29 +0000

    Add expiry control to cookie util

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -7,31 +7,40 @@ type TSearchReturned = {
   data: TSearch;
 };
 
-export default function useSearch(id: string, query = initialSearchCriteria) {
-  const config = {
+type TConfig = {
+  headers: {
+    accept: string;
+    "Content-Type": string;
+  };
+};
+
+async function getResults(query = initialSearchCriteria) {
+  const config: TConfig = {
     headers: {
       accept: "application/json",
       "Content-Type": "application/json",
     },
   };
 
-  const getResults = async () => {
-    const { data } = await getEnvFromServer();
-    const client = new ApiClient(data?.env?.api_url);
-    // TODO: remove this as/when the API returns families as default
-    query.group_documents = true;
-    const results = await client.post(`/searches`, query, config);
-    return results;
-  };
+  const { data } = await getEnvFromServer();
+  const client = new ApiClient(data?.env?.api_url);
+  // TODO: remove this as/when the API returns families as default
+  query.group_documents = true;
+  const results = await client.post(`/searches`, query, config);
+  return results;
+}
 
+export default function useSearch(id: string, query = initialSearchCriteria) {
   return useQuery<TSearchReturned>(
     id,
     async () => {
-      return getResults();
+      return getResults(query);
     },
     {
+      // refetchOnMount: false,
       refetchOnWindowFocus: false,
       cacheTime: 1000 * 60 * 60 * 24,
+      staleTime: 1000 * 60 * 60 * 24,
     }
   );
 }
