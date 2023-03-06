d2972b56e4e2a3663286c1dd9d615fa30bcc15a2
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2023-03-06 17:00:49 +0000

    Update search to use cache

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -1,11 +1,6 @@
-import { useQuery } from "react-query";
 import { ApiClient, getEnvFromServer } from "../api/http-common";
 import { initialSearchCriteria } from "../constants/searchCriteria";
 import { TSearch } from "../types";
-
-type TSearchReturned = {
-  data: TSearch;
-};
 
 type TConfig = {
   headers: {
@@ -14,7 +9,7 @@ type TConfig = {
   };
 };
 
-async function getResults(query = initialSearchCriteria) {
+export async function getSearch(query = initialSearchCriteria) {
   const config: TConfig = {
     headers: {
       accept: "application/json",
@@ -24,23 +19,8 @@ async function getResults(query = initialSearchCriteria) {
 
   const { data } = await getEnvFromServer();
   const client = new ApiClient(data?.env?.api_url);
-  // TODO: remove this as/when the API returns families as default
+  // TODO: remove this attribute as/when the API returns families as default
   query.group_documents = true;
-  const results = await client.post(`/searches`, query, config);
+  const results = await client.post<TSearch>(`/searches`, query, config);
   return results;
-}
-
-export default function useSearch(id: string, query = initialSearchCriteria) {
-  return useQuery<TSearchReturned>(
-    id,
-    async () => {
-      return getResults(query);
-    },
-    {
-      // refetchOnMount: false,
-      refetchOnWindowFocus: false,
-      cacheTime: 1000 * 60 * 60 * 24,
-      staleTime: 1000 * 60 * 60 * 24,
-    }
-  );
 }
