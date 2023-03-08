00b980e601cb983d79d9655f7391a5834127cfc8
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2023-03-08 15:03:09 +0000

    Add browse e2e test

---
+++ e2e/cypress/e2e/cclw/journeys/browseFlow.cy.js
@@ -0,0 +1,41 @@
+/// <reference types="cypress" />
+import { clickCookiePolicy } from "../../../utils/cookiePolicy";
+
+const searchResultsSelector = '[data-cy="search-results"]';
+const countryLinkSelector = '[data-cy="country-link"]';
+
+describe("Browse Flow", () => {
+  before(() => {
+    cy.visit("/");
+    clickCookiePolicy();
+  });
+
+  it("should display the input form and clickable button, which is clicked", () => {
+    cy.get('[data-cy="search-input"] + button').should("be.visible").click();
+  });
+
+  it("should now be on the search page", () => {
+    cy.url().should("include", "/search");
+  });
+
+  it("should not have any query string values", () => {
+    cy.url().should("not.include", "?");
+  });
+
+  it("should display list of search results", () => {
+    cy.get(searchResultsSelector).should("be.visible");
+  });
+
+  it("should display a list of 10 search results", () => {
+    cy.get(searchResultsSelector).children().should("have.length", 10);
+  });
+
+  it("should have a clickable country link, which is clicked", () => {
+    cy.get(searchResultsSelector).children().first().find(countryLinkSelector).should("be.visible").click();
+  });
+
+  it("should now be on the geography page", () => {
+    cy.url().should("include", "/geographies");
+  });
+
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,7 +22,11 @@ const IndexPage = () => {
 
   const handleSearchInput = (term: string, filter?: string, filterValue?: string) => {
     const newQuery = {};
-    newQuery[QUERY_PARAMS.query_string] = term;
+    if (term && term !== "") {
+      newQuery[QUERY_PARAMS.query_string] = term;
+    } else {
+      delete router.query[QUERY_PARAMS.query_string];
+    }
     if (filter && filterValue && filter.length && filterValue.length) {
       newQuery[filter] = [filterValue.toLowerCase()];
     }
