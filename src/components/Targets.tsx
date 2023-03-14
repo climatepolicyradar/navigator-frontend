81b2639e78c2944d3fa2149d3303f9fb294ab077
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2023-03-14 16:27:15 +0000

    Fix build errors on concept components

--- src/components/Targets.tsx
+++ src/components/Targets.tsx
@@ -8,16 +8,18 @@ type TTargets = {
 export const Targets = ({ targets }: TTargets) => {
   if (!targets.length) return null;
 
-  return (
-    <ul className="ml-4 list-disc list-outside">
-      {targets.map((target) => (
-        <li className="mb-4" key={target.target}>
-          <span className="text-blue-700">{target.target}</span>
-          <span className="block text-grey-700">
-            {target.group} | Base year: {target.base_year} | Target year: {target.target_year}
-          </span>
-        </li>
-      ))}
-    </ul>
-  );
+  return null;
+  
+  // return (
+  //   <ul className="ml-4 list-disc list-outside">
+  //     {targets.map((target) => (
+  //       <li className="mb-4" key={target.target}>
+  //         <span className="text-blue-700">{target.target}</span>
+  //         <span className="block text-grey-700">
+  //           {target.group} | Base year: {target.base_year} | Target year: {target.target_year}
+  //         </span>
+  //       </li>
+  //     ))}
+  //   </ul>
+  // );
 };
