e718373227bf2ed5740b87876da80238765d426a
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2023-03-15 11:45:35 +0000

    Add loader and cached search to family page

--- src/pages/document/[id].tsx
+++ src/pages/document/[id].tsx
@@ -1,13 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import { ApiClient } from "@api/http-common";
-import useGetSearcheResults from "@hooks/useGetSearchResults";
 import Layout from "@components/layouts/Main";
 import DocumentInfo from "@components/blocks/DocumentInfo";
 import { Timeline } from "@components/blocks/Timeline";
 import Event from "@components/blocks/Event";
-import { ExternalLinkIcon, GlobeIcon, DocumentIcon, PDFIcon } from "@components/svg/Icons";
-import { DocumentHead } from "@components/document/DocumentHead";
 import { FamilyHead } from "@components/document/FamilyHead";
 import { FamilyDocument } from "@components/document/FamilyDocument";
 import { ExternalLink } from "@components/ExternalLink";
@@ -16,8 +13,11 @@ import { ShowHide } from "@components/controls/ShowHide";
 import { Divider } from "@components/dividers/Divider";
 import { initialSummaryLength } from "@constants/document";
 import { truncateString } from "@helpers/index";
-import { getDocumentTitle } from "@helpers/getDocumentTitle";
-import { TEvent, TDocument, TFamilyDocument, TFamily, TFamilyPage, TTarget } from "@types";
+import { TFamilyPage, TMatchedFamily, TTarget } from "@types";
+import useSearch from "@hooks/useSearch";
+import { useRouter } from "next/router";
+import { QUERY_PARAMS } from "@constants/queryParams";
+import Loader from "@components/Loader";
 
 type TProps = {
   page: TFamilyPage;
@@ -25,10 +25,21 @@ type TProps = {
 };
 
 const FamilyPage: InferGetServerSidePropsType<typeof getServerSideProps> = ({ page, targets = [] }: TProps) => {
+  const router = useRouter();
   const [showTimeline, setShowTimeline] = useState(false);
   const [showCollectionDetail, setShowCollectionDetail] = useState(false);
   const [showFullSummary, setShowFullSummary] = useState(false);
   const [summary, setSummary] = useState("");
+
+  let searchFamily: TMatchedFamily = null;
+  const { status, families } = useSearch(router.query, !!router.query[QUERY_PARAMS.query_string]);
+  if (!!router.query[QUERY_PARAMS.query_string]) {
+    families.forEach((family) => {
+      if (page.slugs.includes(family.family_slug)) {
+        searchFamily = family;
+      }
+    });
+  }
 
   const handleCollectionClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
     e.preventDefault();
@@ -37,6 +48,23 @@ const FamilyPage: InferGetServerSidePropsType<typeof getServerSideProps> = ({ pa
       document.getElementById("collection").scrollIntoView({ behavior: "smooth" });
     }, 100);
   };
+
+  const getDocumentMatches = (docSlugs: string[]) => {
+    console.log(docSlugs, searchFamily);
+    if (searchFamily) {
+      const searchDocument = searchFamily.family_documents.find((doc) => docSlugs.includes(doc.document_slug));
+      if (searchDocument) {
+        return searchDocument.document_passage_matches.length;
+      }
+    }
+  };
+
+  // TODO: align with BE on an approach to sources and their logos
+  const sourceLogo = page?.organisation === "CCLW" ? "grantham-logo.png" : null;
+  const sourceName = page?.organisation === "CCLW" ? "Grantham Research Institute" : page?.organisation;
+
+  const mainDoc = page.documents.find((doc) => doc.variant === "MAIN");
+  const otherDocs = page.documents.filter((doc) => doc.variant !== "MAIN");
 
   useEffect(() => {
     if (page?.summary) {
@@ -49,121 +77,121 @@ const FamilyPage: InferGetServerSidePropsType<typeof getServerSideProps> = ({ pa
     }
   }, [page, showFullSummary]);
 
-  // TODO: align with BE on an approach to sources and their logos
-  const sourceLogo = page?.organisation === "CCLW" ? "grantham-logo.png" : null;
-  const sourceName = page?.organisation === "CCLW" ? "Grantham Research Institute" : page?.organisation;
-
-  const mainDoc = page.documents.find((doc) => doc.variant === "MAIN");
-  const otherDocs = page.documents.filter((doc) => doc.variant !== "MAIN");
-
   return (
     <Layout title={page.title}>
       <section className="mb-8">
         <FamilyHead family={page} onCollectionClick={handleCollectionClick} />
         <div className="container">
-          <div className="md:flex">
-            <section className="flex-1 md:w-0">
-              <section className="mt-6">
-                <div className="text-content mt-4" dangerouslySetInnerHTML={{ __html: summary }} />
-                {page.summary.length > initialSummaryLength && (
-                  <div className="mt-6 flex justify-end">
-                    {showFullSummary ? (
-                      <button onClick={() => setShowFullSummary(false)} className="text-blue-500 font-medium">
-                        Collapse summary
-                      </button>
-                    ) : (
-                      <button onClick={() => setShowFullSummary(true)} className="text-blue-500 font-medium">
-                        Show full summary
-                      </button>
-                    )}
-                  </div>
-                )}
-                {mainDoc && (
-                  <FamilyDocument
-                    title={mainDoc.title}
-                    date={page.published_date}
-                    slug={mainDoc.slugs[0]}
-                    variant={mainDoc.variant}
-                    contentType={mainDoc.content_type}
-                  />
-                )}
-              </section>
-
-              {otherDocs.length > 0 && (
-                <>
-                  <div className="mt-12">
-                    <Divider color="bg-lineBorder" />
-                  </div>
-
-                  <section className="mt-12">
-                    <h3>Related documents</h3>
-                    <div className="divide-solid divide-blue-100 divide-y">
-                      {otherDocs.map((doc, i) => (
-                        <div key={`${i}-${doc.title}`} className="mt-4">
-                          <FamilyDocument
-                            title={doc.title}
-                            date={page.published_date}
-                            slug={doc.slugs[0]}
-                            variant={doc.variant}
-                            contentType={doc.content_type}
-                          />
-                        </div>
-                      ))}
+          {status !== "fetched" ? (
+            <div className="w-full flex justify-center flex-1">
+              <Loader />
+            </div>
+          ) : (
+            <div className="md:flex">
+              <section className="flex-1 md:w-0">
+                <section className="mt-6">
+                  <div className="text-content mt-4" dangerouslySetInnerHTML={{ __html: summary }} />
+                  {page.summary.length > initialSummaryLength && (
+                    <div className="mt-6 flex justify-end">
+                      {showFullSummary ? (
+                        <button onClick={() => setShowFullSummary(false)} className="text-blue-500 font-medium">
+                          Collapse summary
+                        </button>
+                      ) : (
+                        <button onClick={() => setShowFullSummary(true)} className="text-blue-500 font-medium">
+                          Show full summary
+                        </button>
+                      )}
                     </div>
-                  </section>
-                </>
-              )}
+                  )}
+                  {mainDoc && (
+                    <FamilyDocument
+                      title={mainDoc.title}
+                      date={page.published_date}
+                      slug={mainDoc.slugs[0]}
+                      variant={mainDoc.variant}
+                      contentType={mainDoc.content_type}
+                      matches={getDocumentMatches(mainDoc.slugs)}
+                    />
+                  )}
+                </section>
 
-              {targets.length > 0 && (
-                <>
-                  <div className="mt-12">
-                    <Divider color="bg-lineBorder" />
-                  </div>
-
-                  <section className="mt-12">
-                    <div>
-                      <h3 className="flex flex-col mb-4 md:flex-row md:items-center justify-between">
-                        <span className="flex-1">Targets (3)</span>
-                        <span className="text-sm text-grey-700 flex-0">
-                          Download targets data (.csv){" "}
-                          <a href="#" onClick={(e) => e.preventDefault()} className="underline text-primary-600">
-                            this list
-                          </a>{" "}
-                          |{" "}
-                          <a href="#" onClick={(e) => e.preventDefault()} className="underline text-primary-600">
-                            whole database
-                          </a>
-                        </span>
-                      </h3>
-                      <Targets targets={targets} />
+                {otherDocs.length > 0 && (
+                  <>
+                    <div className="mt-12">
+                      <Divider color="bg-lineBorder" />
                     </div>
-                  </section>
-                </>
-              )}
 
-              {page.events.length > 0 && (
-                <>
-                  <div className="mt-12">
-                    <Divider color="bg-lineBorder" />
-                  </div>
-
-                  <section className="mt-12">
-                    <h3>Timeline</h3>
-                    <ShowHide show={showTimeline} onClick={() => setShowTimeline(!showTimeline)} className="mt-4" />
-                    {showTimeline && (
-                      <Timeline>
-                        {page.events.map((event, index: number) => (
-                          <React.Fragment key={`event-${index}`}>
-                            <Event event={event} index={index} last={index === page.events.length - 1 ? true : false} />
-                          </React.Fragment>
+                    <section className="mt-12">
+                      <h3>Related documents</h3>
+                      <div className="divide-solid divide-blue-100 divide-y">
+                        {otherDocs.map((doc, i) => (
+                          <div key={`${i}-${doc.title}`} className="mt-4">
+                            <FamilyDocument
+                              title={doc.title}
+                              date={page.published_date}
+                              slug={doc.slugs[0]}
+                              variant={doc.variant}
+                              contentType={doc.content_type}
+                              matches={getDocumentMatches(doc.slugs)}
+                            />
+                          </div>
                         ))}
-                      </Timeline>
-                    )}
-                  </section>
-                </>
-              )}
+                      </div>
+                    </section>
+                  </>
+                )}
 
-              {/* TODO: return collection information
+                {targets.length > 0 && (
+                  <>
+                    <div className="mt-12">
+                      <Divider color="bg-lineBorder" />
+                    </div>
+
+                    <section className="mt-12">
+                      <div>
+                        <h3 className="flex flex-col mb-4 md:flex-row md:items-center justify-between">
+                          <span className="flex-1">Targets (3)</span>
+                          <span className="text-sm text-grey-700 flex-0">
+                            Download targets data (.csv){" "}
+                            <a href="#" onClick={(e) => e.preventDefault()} className="underline text-primary-600">
+                              this list
+                            </a>{" "}
+                            |{" "}
+                            <a href="#" onClick={(e) => e.preventDefault()} className="underline text-primary-600">
+                              whole database
+                            </a>
+                          </span>
+                        </h3>
+                        <Targets targets={targets} />
+                      </div>
+                    </section>
+                  </>
+                )}
+
+                {page.events.length > 0 && (
+                  <>
+                    <div className="mt-12">
+                      <Divider color="bg-lineBorder" />
+                    </div>
+
+                    <section className="mt-12">
+                      <h3>Timeline</h3>
+                      <ShowHide show={showTimeline} onClick={() => setShowTimeline(!showTimeline)} className="mt-4" />
+                      {showTimeline && (
+                        <Timeline>
+                          {page.events.map((event, index: number) => (
+                            <React.Fragment key={`event-${index}`}>
+                              <Event event={event} index={index} last={index === page.events.length - 1 ? true : false} />
+                            </React.Fragment>
+                          ))}
+                        </Timeline>
+                      )}
+                    </section>
+                  </>
+                )}
+
+                {/* TODO: return collection information
               <div className="mt-12">
                 <Divider color="bg-lineBorder" />
               </div>
@@ -207,51 +235,52 @@ const FamilyPage: InferGetServerSidePropsType<typeof getServerSideProps> = ({ pa
                   </div>
                 )}
               </section> */}
-            </section>
-            <section className="mt-12 md:border-t-0 md:mt-6 md:w-2/5 lg:w-1/4 md:pl-12 flex-shrink-0">
-              <div className="md:pl-4 md:border-l md:border-lineBorder">
-                <h3>About this document</h3>
-                <div className="grid grid-cols-2 gap-x-2">
-                  <DocumentInfo id="category-tt" heading="Category" text={page.category} />
-                  <DocumentInfo id="type-tt" heading="Type" text={page.metadata.document_type} />
-                  {page.metadata.topic.length > 0 && <DocumentInfo id="topics-tt" heading="Topics" list={page.metadata.topic} />}
-                  {/* {page.metadata.languages.length > 0 && <DocumentInfo heading="Language" text={page.languages[0].name} />} */}
-                </div>
-
-                {page.metadata.keyword.length > 0 && <DocumentInfo id="keywords-tt" heading="Keywords" list={page.metadata.keyword} />}
-                {page.metadata.sector.length > 0 && <DocumentInfo id="sectors-tt" heading="Sectors" list={page.metadata.sector} />}
-                <div className="mt-8 border-t border-blue-100">
-                  <h3 className="mt-4">Note</h3>
-                  <div className="flex items-end my-4">
-                    {sourceLogo && (
-                      <div className="relative flex-shrink w-3/4 xmax-w-[40px] mr-1">
-                        {/* eslint-disable-next-line @next/next/no-img-element */}
-                        <img src={`/images/partners/${sourceLogo}`} alt={page.organisation} />
-                      </div>
-                    )}
-                    {page.organisation !== "CCLW" && <p className="text-sm">{sourceName}</p>}
+              </section>
+              <section className="mt-12 md:border-t-0 md:mt-6 md:w-2/5 lg:w-1/4 md:pl-12 flex-shrink-0">
+                <div className="md:pl-4 md:border-l md:border-lineBorder">
+                  <h3>About this document</h3>
+                  <div className="grid grid-cols-2 gap-x-2">
+                    <DocumentInfo id="category-tt" heading="Category" text={page.category} />
+                    <DocumentInfo id="type-tt" heading="Type" text={page.metadata.document_type} />
+                    {page.metadata.topic.length > 0 && <DocumentInfo id="topics-tt" heading="Topics" list={page.metadata.topic} />}
+                    {/* {page.metadata.languages.length > 0 && <DocumentInfo heading="Language" text={page.languages[0].name} />} */}
                   </div>
-                  <p>
-                    The summary of this document was written by researchers at the{" "}
-                    <ExternalLink
-                      url="http://lse.ac.uk/grantham"
-                      className="text-blue-500 hover:text-indigo-600 hover:underline transition duration-300"
-                    >
-                      Grantham Research Institute
-                    </ExternalLink>
-                    . If you want to use this summary, please check{" "}
-                    <ExternalLink
-                      url="https://www.lse.ac.uk/granthaminstitute/cclw-terms-and-conditions"
-                      className="text-blue-500 hover:text-indigo-600 hover:underline transition duration-300"
-                    >
-                      terms of use
-                    </ExternalLink>{" "}
-                    for citation and licensing of third party data.
-                  </p>
+
+                  {page.metadata.keyword.length > 0 && <DocumentInfo id="keywords-tt" heading="Keywords" list={page.metadata.keyword} />}
+                  {page.metadata.sector.length > 0 && <DocumentInfo id="sectors-tt" heading="Sectors" list={page.metadata.sector} />}
+                  <div className="mt-8 border-t border-blue-100">
+                    <h3 className="mt-4">Note</h3>
+                    <div className="flex items-end my-4">
+                      {sourceLogo && (
+                        <div className="relative flex-shrink w-3/4 xmax-w-[40px] mr-1">
+                          {/* eslint-disable-next-line @next/next/no-img-element */}
+                          <img src={`/images/partners/${sourceLogo}`} alt={page.organisation} />
+                        </div>
+                      )}
+                      {page.organisation !== "CCLW" && <p className="text-sm">{sourceName}</p>}
+                    </div>
+                    <p>
+                      The summary of this document was written by researchers at the{" "}
+                      <ExternalLink
+                        url="http://lse.ac.uk/grantham"
+                        className="text-blue-500 hover:text-indigo-600 hover:underline transition duration-300"
+                      >
+                        Grantham Research Institute
+                      </ExternalLink>
+                      . If you want to use this summary, please check{" "}
+                      <ExternalLink
+                        url="https://www.lse.ac.uk/granthaminstitute/cclw-terms-and-conditions"
+                        className="text-blue-500 hover:text-indigo-600 hover:underline transition duration-300"
+                      >
+                        terms of use
+                      </ExternalLink>{" "}
+                      for citation and licensing of third party data.
+                    </p>
+                  </div>
                 </div>
-              </div>
-            </section>
-          </div>
+              </section>
+            </div>
+          )}
         </div>
       </section>
     </Layout>
--- src/pages/documents/[id].tsx
+++ src/pages/documents/[id].tsx
@@ -6,19 +6,21 @@ import { DocumentHead } from "@components/document/DocumentHead";
 import Layout from "@components/layouts/Main";
 import EmbeddedPDF from "@components/EmbeddedPDF";
 import PassageMatches from "@components/PassageMatches";
-
 import { TPhysicalDocument } from "@types";
 import useSearch from "@hooks/useSearch";
 import { QUERY_PARAMS } from "@constants/queryParams";
+import Loader from "@components/Loader";
 
 type TProps = {
   document: TPhysicalDocument;
 };
 
 const DocumentPage: InferGetServerSidePropsType<typeof getServerSideProps> = ({ document }: TProps) => {
+  const [passageIndex, setPassageIndex] = useState(null);
   const router = useRouter();
-  const { status, families } = useSearch(router.query, !!router.query[QUERY_PARAMS.query_string]);
+
   const passageMatches = [];
+  const { status, families } = useSearch(router.query, !!router.query[QUERY_PARAMS.query_string]);
   if (!!router.query[QUERY_PARAMS.query_string]) {
     families.forEach((family) => {
       family.family_documents.forEach((doc) => {
@@ -29,33 +31,37 @@ const DocumentPage: InferGetServerSidePropsType<typeof getServerSideProps> = ({ 
     });
   }
 
-  const [passageIndex, setPassageIndex] = useState(null);
-
   return (
     <Layout title={document.name}>
       <section className="mb-8 flex-1 flex flex-col">
         <DocumentHead document={document} />
-        <section className="mt-4 flex-1 flex">
-          <div className="container flex-1">
-            {passageMatches.length > 0 && (
-              <div className="pb-4 border-b border-lineBorder">
-                <h3>Document matches for {`'${router.query[QUERY_PARAMS.query_string]}' (${passageMatches.length})`}</h3>
-              </div>
-            )}
-            <div className="md:flex md:h-[80vh]">
-              {passageMatches.length > 0 && (
-                <div className="md:block md:w-1/3 overflow-y-scroll pr-4 max-h-[30vh] md:max-h-full">
-                  <PassageMatches passages={passageMatches} setPassageIndex={setPassageIndex} activeIndex={passageIndex} />
-                </div>
-              )}
-              {status === "fetched" && (
-                <div className="md:block mt-4 flex-1 h-[400px] md:h-full">
-                  <EmbeddedPDF document={document} documentPassageMatches={passageMatches} passageIndex={passageIndex} />
-                </div>
-              )}
-            </div>
+        {status !== "fetched" ? (
+          <div className="w-full flex justify-center flex-1">
+            <Loader />
           </div>
-        </section>
+        ) : (
+          <section className="mt-4 flex-1 flex">
+            <div className="container flex-1">
+              {passageMatches.length > 0 && (
+                <div className="pb-4 border-b border-lineBorder">
+                  <h3>Document matches for {`'${router.query[QUERY_PARAMS.query_string]}' (${passageMatches.length})`}</h3>
+                </div>
+              )}
+              <div className="md:flex md:h-[80vh]">
+                {passageMatches.length > 0 && (
+                  <div className="md:block md:w-1/3 overflow-y-scroll pr-4 max-h-[30vh] md:max-h-full">
+                    <PassageMatches passages={passageMatches} setPassageIndex={setPassageIndex} activeIndex={passageIndex} />
+                  </div>
+                )}
+                {status === "fetched" && (
+                  <div className="md:block mt-4 flex-1 h-[400px] md:h-full">
+                    <EmbeddedPDF document={document} documentPassageMatches={passageMatches} passageIndex={passageIndex} />
+                  </div>
+                )}
+              </div>
+            </div>
+          </section>
+        )}
       </section>
     </Layout>
   );
