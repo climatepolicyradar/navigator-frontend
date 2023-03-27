03ac38693f7fc905651e7302459f6aa068904c64
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2023-03-27 16:04:09 +0100

    Meta tags consistent between pages

--- src/components/layouts/Main.tsx
+++ src/components/layouts/Main.tsx
@@ -22,8 +22,8 @@ const Layout: FC<TProps> = ({ children, title = "", description = null, screenHe
   return (
     <div className="h-full flex flex-col">
       <Head>
-        <title>{`${title} | ${getPageTitle(theme)}`}</title>
-        <meta property="og:title" content={`${title} | ${getPageTitle(theme)}`} />
+        <title>{`${title} - ${getPageTitle(theme)}`}</title>
+        <meta property="og:title" content={`${title} - ${getPageTitle(theme)}`} />
         <meta name="description" content={description ?? getPageDescription(theme)} key="desc" />
         <meta property="og:description" content={description ?? getPageDescription(theme)} />
         <meta charSet="utf-8" />
