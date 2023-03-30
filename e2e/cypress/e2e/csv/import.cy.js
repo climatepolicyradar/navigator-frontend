2ef3c765edf83830d1b6b29a5b9c9d11a9d97cdf
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2023-03-30 09:31:44 +0100

    Add csv test script

---
+++ e2e/cypress/e2e/csv/import.cy.js
@@ -0,0 +1,30 @@
+/// <reference types="cypress" />
+const neatCSV = require("neat-csv");
+
+const startingIndex = 0;
+const endIndex = 1;
+
+describe("CSV Import", () => {
+  let csvData = [];
+
+  before(() => {
+    cy.fixture("importData.csv")
+      .then(neatCSV)
+      .then((data) => {
+        csvData = data;
+      });
+  });
+
+  for (let index = startingIndex; index < endIndex; index++) {
+    it(`should import all data items successfully - ${index}`, () => {
+      cy.log(csvData.length);
+      const element = csvData[index];
+      // cy.log(element["CPR Family Slug"]);
+      if (!element || !element["CPR Family Slug"]) {
+        throw new Error("Element does not exist");
+      }
+      cy.visit(`/document/${element["CPR Family Slug"]}`);
+      cy.contains("h1", element["Family name"], { timeout: 250 }).should("be.visible");
+    });
+  }
+});
