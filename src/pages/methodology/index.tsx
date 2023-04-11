13a6779aea54d70d4cad27f5960fe00bfaa50ba4
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2023-04-11 11:23:12 +0100

    FRO 203 methodology updates (#43)
    * Add new methodology content for cclw
    * Add clickable accordion layout for methodology

--- src/pages/methodology/index.tsx
+++ src/pages/methodology/index.tsx
@@ -2,7 +2,6 @@ import { useContext } from "react";
 import { ThemeContext } from "@context/ThemeContext";
 import Layout from "@components/layouts/Main";
 
-import CPRMethodology from "@cpr/pages/methodology";
 import CCLWMethodology from "@cclw/pages/methodology";
 
 
@@ -11,7 +10,6 @@ const Methodology = () => {
 
   return (
     <Layout title={"Methodology"}>
-      {theme === "cpr" && <CPRMethodology />}
       {theme === "cclw" && <CCLWMethodology />}
     </Layout>
   );
