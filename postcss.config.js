340423eff99d90ae5e2d5540a185796b2b0c0111
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2023-04-11 11:23:38 +0100

    FRO 210 acknowledgments page (#44)
    * Ensure it is clear which tailwind config is being applied
    * Improve title sizes on small devices
    * Remove titles

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   plugins: {
-    // tailwindcss: { config: `./tailwind.${process.env.THEME || "cclw"}.config.js` },
     tailwindcss: { config: `./tailwind.cclw.config.js` },
     autoprefixer: {},
   },
