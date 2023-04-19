7d7010b3e6ca189c5f8d3e2c4f046f77d89c64db
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2023-04-19 11:52:56 +0100

    Add top level error boundary

--- src/components/error/ErrorBoundary.tsx
+++ src/components/error/ErrorBoundary.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from "@components/buttons/Button";
 
 // based on: https://nextjs.org/docs/advanced-features/error-handling
 
@@ -29,11 +30,15 @@ class ErrorBoundary extends React.Component<TProps, TState> {
     if (this.state.hasError) {
       // You can render any custom fallback UI
       return (
-        <div>
-          <h2>Oops, there is an error!</h2>
-          <button type="button" onClick={() => this.setState({ hasError: false })}>
-            Try again?
-          </button>
+        <div className="max-w-screen-sm m-auto h-full flex flex-col justify-center p-4">
+          <h2>Sorry, the app has encountered an error and needs to restart</h2>
+          <p className="my-4">Please click the button below to start the app.</p>
+          <div>
+            <Button extraClasses="bg-black text-white hover:border-black hover:bg-black" thin onClick={() => this.setState({ hasError: false })}>
+              Restart
+            </Button>
+          </div>
+          <p className="my-4">To report a problem email us at support@climatepolicyradar.org</p>
         </div>
       );
     }
