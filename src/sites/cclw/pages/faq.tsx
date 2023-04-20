f28418bfb81061137eedbe4979524f9c415c4faa
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2023-04-20 17:32:32 +0100

    Add faq specific instructions

--- src/sites/cclw/pages/faq.tsx
+++ src/sites/cclw/pages/faq.tsx
@@ -1,6 +1,7 @@
-import { AccordianItem } from "@cclw/components/AccordianItem";
-import { FAQS } from "@cclw/constants/faqs";
 import { Fragment } from "react";
+import { AccordianItem } from "@cclw/components/AccordianItem";
+import Instructions from "@cclw/components/Instructions";
+import { FAQS } from "@cclw/constants/faqs";
 
 const FAQ = () => {
   return (
@@ -8,7 +9,13 @@ const FAQ = () => {
       <div className="text-content px-4 container mb-12">
         <h1 className="my-8">How to use this resource</h1>
         <p>Find information to help you get started with our resource.</p>
+      </div>
 
+      <div className="gradient-container py-8 mb-12 text-white">
+        <Instructions isFAQ />
+      </div>
+
+      <div className="text-content px-4 container mb-12">
         <h2 className="my-6">FAQs</h2>
 
         {FAQS.map((faq, i) => (
