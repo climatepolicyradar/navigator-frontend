b528ef6dc39e62be07f34fcd99fd5f2b12a23363
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2023-04-21 11:11:43 +0100

    Add video and text to the faq page

--- src/sites/cclw/components/Instructions.tsx
+++ src/sites/cclw/components/Instructions.tsx
@@ -3,11 +3,7 @@ import { ExternalLink } from "@components/ExternalLink";
 import { LinkWithQuery } from "@components/LinkWithQuery";
 import { Translation } from "@components/svg/Icons";
 
-type TProps = {
-  isFAQ?: boolean;
-};
-
-const Instructions = ({ isFAQ = false }: TProps) => {
+const Instructions = () => {
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 text-center max-w-screen-lg mx-auto">
       <div className="py-5 lg:py-0 flex flex-col lg:px-4" data-cy="feature-search">
@@ -31,36 +27,26 @@ const Instructions = ({ isFAQ = false }: TProps) => {
         </div>
         <p className="mb-6">Find English translations of documents published in any language</p>
       </div>
-      {!isFAQ && (
-        <div
-          className="border-t md:border-t-0 lg:border-t-0 md:border-l border-white py-5 lg:py-0 lg:px-4 flex items-center justify-center flex-wrap"
-          data-cy="feature-litigation"
-        >
-          <p>
-            <LinkWithQuery href="/faq#litigation" className="font-bold underline">
-              Climate litigation
-            </LinkWithQuery>{" "}
-            is coming soon. Access it now at{" "}
-            <ExternalLink url="http://climatecasechart.com/" className="font-bold underline">
-              Climate Case Chart
-            </ExternalLink>
-          </p>
-          <p>
-            Learn more about{" "}
-            <LinkWithQuery href="/faq#litigation" className="font-bold underline">
-              how to use this site
-            </LinkWithQuery>
-          </p>
-        </div>
-      )}
-      {isFAQ && (
-        <div className="border-t md:border-t-0 md:border-l border-white py-5 lg:py-0 flex flex-col items-center lg:px-4" data-cy="feature-video">
-          <div className="mb-6 h-[80px] flex items-center justify-center relative">
-            <img src="/images/video.png" alt="Video icon" className="max-h-full" />
-          </div>
-          <p className="mb-6">Watch a quick demo to see how to use our resource</p>
-        </div>
-      )}
+      <div
+        className="border-t md:border-t-0 lg:border-t-0 md:border-l border-white py-5 lg:py-0 lg:px-4 flex items-center justify-center flex-wrap"
+        data-cy="feature-litigation"
+      >
+        <p>
+          <LinkWithQuery href="/faq#litigation" className="font-bold underline">
+            Climate litigation
+          </LinkWithQuery>{" "}
+          is coming soon. Access it now at{" "}
+          <ExternalLink url="http://climatecasechart.com/" className="font-bold underline">
+            Climate Case Chart
+          </ExternalLink>
+        </p>
+        <p>
+          Learn more about{" "}
+          <LinkWithQuery href="/faq#litigation" className="font-bold underline">
+            how to use this site
+          </LinkWithQuery>
+        </p>
+      </div>
     </div>
   );
 };
--- src/sites/cclw/pages/faq.tsx
+++ src/sites/cclw/pages/faq.tsx
@@ -1,6 +1,5 @@
 import { Fragment } from "react";
 import { AccordianItem } from "@cclw/components/AccordianItem";
-import Instructions from "@cclw/components/Instructions";
 import { FAQS } from "@cclw/constants/faqs";
 
 const FAQ = () => {
@@ -8,16 +7,25 @@ const FAQ = () => {
     <section>
       <div className="text-content px-4 container mb-12">
         <h1 className="my-8">How to use this resource</h1>
-        <p>Find information to help you get started with our resource.</p>
-      </div>
-
-      <div className="gradient-container py-8 mb-12 text-white">
-        <Instructions isFAQ />
+        <p>You can use this resource to:</p>
+        <ul>
+          <li>Search 3500+ climate law and policy documents from every country</li>
+          <li>See exact matches and related phrases highlighted in the text</li>
+        </ul>
+        <p>Watch the video below to learn how to use this resource</p>
+        <iframe
+          height="315"
+          src="https://www.youtube-nocookie.com/embed/QvM2RybWhMM"
+          title="Learn how to use Climate Laws of the World"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+          className="w-full h-[300px] md:h-[400px] lg:h-[500px] lg:w-2/3"
+        ></iframe>
       </div>
 
       <div className="text-content px-4 container mb-12">
         <h2 className="my-6">FAQs</h2>
-
         {FAQS.map((faq, i) => (
           <Fragment key={faq.title}>
             <AccordianItem title={faq.title} startOpen={i === 0}>
