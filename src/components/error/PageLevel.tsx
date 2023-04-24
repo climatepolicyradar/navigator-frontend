75de610e90184aa1fa7b71c401a684b63cc2c859
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2023-04-24 11:23:46 +0100

    Error boundaries (#49)
    * Add EB component
    * fix z-index issue on search dropdown

---
+++ src/components/error/PageLevel.tsx
@@ -0,0 +1,34 @@
+// import { useContext } from "react";
+// import { ThemeContext } from "@context/ThemeContext";
+import Layout from "@components/layouts/Main";
+import { ExternalLink } from "@components/ExternalLink";
+import Button from "@components/buttons/Button";
+
+type TProps = {
+  resetError: () => void;
+};
+
+const PageLevel = ({ resetError }: TProps) => {
+  // const theme = useContext(ThemeContext);
+
+  return (
+    <Layout title={"Application Error"}>
+      <section>
+        <div className="text-content px-4 container mb-12">
+          <h1 className="my-8">Sorry, the app has encountered an error</h1>
+          <p>Restarting the app might fix the problem. Click the button below to restart</p>
+          <div>
+            <Button thin onClick={resetError}>
+              Restart
+            </Button>
+          </div>
+          <p>
+            If this doesn't work, report a problem by emailing{" "}
+            <ExternalLink url="mailto:support@climatepolicyradar.org">support@climatepolicyradar.org</ExternalLink>
+          </p>
+        </div>
+      </section>
+    </Layout>
+  );
+};
+export default PageLevel;
