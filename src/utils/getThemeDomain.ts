982465845b88d22aaa1819a8c7e4f67a680a5fdd
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2023-04-25 14:32:56 +0100

    Correct name of method

--- src/utils/getThemeDomain.ts
+++ src/utils/getThemeDomain.ts
@@ -2,7 +2,7 @@ import { TTheme } from "@types";
 const CPR_DOMAIN = "climatepolicyradar.com";
 const CCLW_DOMAIN = "climate-laws.org";
 
-export default function getPageDescription(site: TTheme): string {
+export default function getThemeDomain(site: TTheme): string {
   let domain = CPR_DOMAIN;
   switch (site) {
     case "cclw":
