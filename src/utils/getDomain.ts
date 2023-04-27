080564cabdb0c4669f65d39d6f9bda8aa2c0653d
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2023-04-27 10:33:13 +0100

    Use hostname over origin to avoid error setting cookie (#68)

--- src/utils/getDomain.ts
+++ src/utils/getDomain.ts
@@ -2,8 +2,8 @@ const getDomain = () => {
   if (process.env.NODE_ENV === "development") {
     return "localhost";
   }
-  const { origin } = window.location;
-  const domain = origin.split(".").slice(-2).join(".");
+  const { hostname } = window.location;
+  const domain = hostname.split(".").slice(-2).join(".");
   return domain;
 };
 
