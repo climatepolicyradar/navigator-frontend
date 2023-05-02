3de1f1777eb857ebd552c7ee5b4518ba1bc1d6bf
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2023-05-02 15:34:36 +0100

    Update default meta description for CCLW

--- src/components/layouts/LandingPage.tsx
+++ src/components/layouts/LandingPage.tsx
@@ -2,19 +2,24 @@ import React, { ReactNode, useContext } from "react";
 import Head from "next/head";
 import { ThemeContext } from "@context/ThemeContext";
 import getPageTitle from "@utils/getPageTitle";
+import getPageDescription from "@utils/getPageDescription";
 
 type TProps = {
   children?: ReactNode;
   title?: string;
+  description?: string;
 };
 
-const Layout = ({ children, title = "" }: TProps) => {
+const Layout = ({ children, title = "", description = null }: TProps) => {
   const theme = useContext(ThemeContext);
 
   return (
     <div>
       <Head>
         <title>{`${title} - ${getPageTitle(theme)}`}</title>
+        <meta property="og:title" content={`${title} - ${getPageTitle(theme)}`} />
+        <meta name="description" content={description ?? getPageDescription(theme)} key="desc" />
+        <meta property="og:description" content={description ?? getPageDescription(theme)} />
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
