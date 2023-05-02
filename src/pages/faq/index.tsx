d6b273b3066f9423b36217ce11ec0ebf5fc6f21b
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2023-05-02 15:48:10 +0100

    Updating meta descriptions for CCLW pages

--- src/pages/faq/index.tsx
+++ src/pages/faq/index.tsx
@@ -7,6 +7,13 @@ import CCLWFAQ from "@cclw/pages/faq";
 const FAQ = () => {
   const theme = useContext(ThemeContext);
 
-  return <Layout title={"FAQ"}>{theme === "cclw" && <CCLWFAQ />}</Layout>;
+  return (
+    <Layout
+      title="FAQ"
+      description="Find quick tips for how you can use this resource to explore national-level climate change laws and policies from across the world."
+    >
+      {theme === "cclw" && <CCLWFAQ />}
+    </Layout>
+  );
 };
 export default FAQ;
