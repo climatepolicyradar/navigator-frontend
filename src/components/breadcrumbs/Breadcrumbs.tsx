5886ce2d0482cb05160ac21fd6d59d62ddd50213
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2023-05-15 12:15:14 +0100

    add data cypress tags to breadcrumbs

--- src/components/breadcrumbs/Breadcrumbs.tsx
+++ src/components/breadcrumbs/Breadcrumbs.tsx
@@ -5,6 +5,7 @@ type TBreadcrumbLink = {
   label: string | React.ReactNode;
   href?: string;
   last?: boolean;
+  cy?: string;
 };
 
 type TProps = {
@@ -14,13 +15,13 @@ type TProps = {
   label: string | React.ReactNode;
 };
 
-const BreadCrumb = ({ last = false, label, href = null }: TBreadcrumbLink) => {
+const BreadCrumb = ({ last = false, label, href = null, cy = "" }: TBreadcrumbLink) => {
   const labelShort =
     typeof label === "string" && label.toString().length > BREADCRUMB_MAXLENGTH ? `${label.toString().substring(0, BREADCRUMB_MAXLENGTH)}...` : label;
-    
+
   return (
     <>
-      <li>
+      <li data-cy={`breadcrumb ${cy}`}>
         {href ? (
           <LinkWithQuery className="underline" href={href}>
             {labelShort}
@@ -37,12 +38,12 @@ const BreadCrumb = ({ last = false, label, href = null }: TBreadcrumbLink) => {
 
 export const BreadCrumbs = ({ geography = null, category = null, family = null, label }: TProps) => {
   return (
-    <ul className="flex flex-wrap gap-2 pt-4">
-      <BreadCrumb label="Home" href="/" />
-      {geography && <BreadCrumb label={geography.label} href={geography.href} />}
-      {category && <BreadCrumb label={category.label} href={category.href} />}
-      {family && <BreadCrumb label={family.label} href={family.href} />}
-      <BreadCrumb label={label} last />
+    <ul className="flex flex-wrap gap-2 pt-4" data-cy="breadcrumbs">
+      <BreadCrumb label="Home" href="/" cy="home" />
+      {geography && <BreadCrumb label={geography.label} href={geography.href} cy="geography" />}
+      {category && <BreadCrumb label={category.label} href={category.href} cy="category" />}
+      {family && <BreadCrumb label={family.label} href={family.href} cy="family" />}
+      <BreadCrumb label={label} last cy="current" />
     </ul>
   );
 };
