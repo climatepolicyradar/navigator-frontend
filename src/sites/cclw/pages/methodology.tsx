92ae21ffe3d6a25656e39987ddf28adbfd3581a9
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2023-06-01 15:17:12 +0100

    Update copy on methodology

--- src/sites/cclw/pages/methodology.tsx
+++ src/sites/cclw/pages/methodology.tsx
@@ -1,5 +1,7 @@
 import { AccordianItem } from "@cclw/components/AccordianItem";
 import { METHODOLOGY } from "@cclw/constants/methodologyItems";
+import { ExternalLink } from "@components/ExternalLink";
+import { LinkWithQuery } from "@components/LinkWithQuery";
 import { BreadCrumbs } from "@components/breadcrumbs/Breadcrumbs";
 import { Fragment } from "react";
 
@@ -11,12 +13,19 @@ const Methodology = () => {
         <div className="text-content mb-12">
           <h1 className="my-8">Methodology</h1>
           <h2 id="introduction">Introduction</h2>
-          <p className="italic">This page was last updated on 30 March 2022.</p>
+          <p className="italic">This page was last updated on 1 June 2023.</p>
           <p>
             This page outlines the definitions, scope and principles used to collect and categorise the <b className="font-bold">laws and policies</b>{" "}
             contained in the Climate Change Laws of the World dataset. This dataset originates from a collaboration between the Grantham Research
             Institute at LSE and GLOBE International on a series of Climate Legislation Studies. The dataset is currently maintained by LSE, in
             partnership with Climate Policy Radar.
+          </p>
+          <p>
+            In addition to the Climate Change Laws of the World dataset, this database includes documents from UNFCCC portals. These documents are
+            offered alongside national level climate change laws and policies to help users in gaining a comprehensive understanding of national level
+            climate action. For details of the documents included, please visit our <LinkWithQuery href="/faq">FAQs</LinkWithQuery>. To understand
+            more about how new document types may be added in future, visit the{" "}
+            <ExternalLink url="https://github.com/climatepolicyradar/methodology">Climate Policy Radar methodology page</ExternalLink>.
           </p>
           <p>
             Classifications described below have been assigned manually. LSE, Climate Policy Radar, and our partners are currently working to develop
