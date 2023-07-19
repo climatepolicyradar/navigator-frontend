bf35b02807f353225d8898cfabc01337c01dc0c8
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2023-07-19 17:21:31 +0100

    Update styling on hero instructions

--- src/sites/cclw/components/Instructions.tsx
+++ src/sites/cclw/components/Instructions.tsx
@@ -3,6 +3,9 @@ import { ExternalLink } from "@components/ExternalLink";
 import { LinkWithQuery } from "@components/LinkWithQuery";
 import { Translation } from "@components/svg/Icons";
 
+const heroLinkClasses = "text-white font-bold underline hover:text-white";
+const heroSectionClasses = "border-t border-white py-5 flex items-center lg:py-0 lg:px-4";
+
 const Instructions = () => {
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 text-center max-w-screen-lg mx-auto">
@@ -12,37 +15,31 @@ const Instructions = () => {
         </div>
         <p className="mb-6">Search the full text of over 5000 laws, policies and UNFCCC submissions from every country.</p>
       </div>
-      <div className="border-t md:border-t-0 md:border-l border-white py-5 lg:py-0 flex flex-col items-center lg:px-4" data-cy="feature-highlights">
+      <div className={`${heroSectionClasses} flex-col md:border-t-0 md:border-l`} data-cy="feature-highlights">
         <div className="mb-6 h-[80px] flex items-center justify-center relative">
           <img src="/images/highlight.png" alt="Phrase highlighting" className="max-h-full" />
         </div>
         <p className="mb-6">See exact matches and related phrases highlighted in the text</p>
       </div>
-      <div
-        className="hidden border-t lg:border-t-0 lg:border-l border-white py-5 lg:py-0 flex flex-col items-center lg:px-4"
-        data-cy="feature-translations"
-      >
+      <div className={`${heroSectionClasses} flex-col hidden lg:border-t-0 lg:border-l`} data-cy="feature-translations">
         <div className="mb-6 h-[80px] flex items-center justify-center">
           <Translation height="60" />
         </div>
         <p className="mb-6">Find English translations of documents published in any language</p>
       </div>
-      <div
-        className="border-t md:border-t-0 lg:border-t-0 md:border-l border-white py-5 lg:py-0 lg:px-4 flex items-center justify-center flex-wrap"
-        data-cy="feature-litigation"
-      >
+      <div className={`${heroSectionClasses} md:border-t-0 lg:border-t-0 md:border-l justify-center flex-wrap`} data-cy="feature-litigation">
         <p>
-          <LinkWithQuery href="/faq" hash="litigation-data" className="font-bold underline">
+          <LinkWithQuery href="/faq" hash="litigation-data" className={heroLinkClasses}>
             Climate litigation
           </LinkWithQuery>{" "}
           is coming soon. Access it now at{" "}
-          <ExternalLink url="http://climatecasechart.com/" className="font-bold underline">
+          <ExternalLink url="http://climatecasechart.com/" className={heroLinkClasses}>
             Climate Case Chart
           </ExternalLink>
         </p>
         <p>
           Learn more about{" "}
-          <LinkWithQuery href="/faq" className="font-bold underline">
+          <LinkWithQuery href="/faq" className={heroLinkClasses}>
             how to use this site
           </LinkWithQuery>
         </p>
