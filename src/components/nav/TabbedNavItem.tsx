847dfc4bdca133ced24e61d86921e815abbbcf1f
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2023-07-20 12:44:18 +0100

    update nav styles

--- src/components/nav/TabbedNavItem.tsx
+++ src/components/nav/TabbedNavItem.tsx
@@ -6,7 +6,7 @@ interface TabbedNavItemProps {
 }
 
 const TabbedNavItem = ({ title, index, activeTab, onClick }: TabbedNavItemProps) => {
-  const cssClass = `text-left mt-4 md:mt-0 font-medium text-lg text-indigo-400 hover:text-blue-600 mr-8 ${activeTab === index ? "tabbed-nav__active" : ""}`;
+  const cssClass = `text-left mt-4 md:mt-0 text-lg hover:text-blue-600 mr-8 ${activeTab === index ? "tabbed-nav__active" : ""}`;
   return (
     <button onClick={onClick} className={cssClass}>
       {title}
