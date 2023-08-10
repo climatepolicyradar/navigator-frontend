b8c8e6c48a9542331ba130099660693f8d41a314
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2023-08-10 17:38:41 +0100

    Add interfaces to Window

---
+++ custom.d.ts
@@ -0,0 +1,12 @@
+interface Window {
+  _paq?: (Dimensions | number[] | string[] | number | string | null | undefined)[][] | null;
+  Cypress?: any;
+  queryClient: any;
+}
+declare namespace NodeJS {
+  interface Global {
+    _paq?: (Dimensions | number[] | string[] | number | string | null | undefined)[][] | null;
+    Cypress?: any;
+    queryClient: any;
+  }
+}
