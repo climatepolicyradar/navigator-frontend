2e665fbf33b7e47d7b1819daf00c0924a1a789c8
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2023-08-15 10:24:39 +0100

    Add mtm to global type decs

--- custom.d.ts
+++ custom.d.ts
@@ -1,11 +1,13 @@
 interface Window {
   _paq?: (Dimensions | number[] | string[] | number | string | null | undefined)[][] | null;
+  _mtm?: any;
   Cypress?: any;
   queryClient: any;
 }
 declare namespace NodeJS {
   interface Global {
     _paq?: (Dimensions | number[] | string[] | number | string | null | undefined)[][] | null;
+    _mtm?: any;
     Cypress?: any;
     queryClient: any;
   }
