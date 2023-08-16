8d519160b504dc0ca612efc87446d41fd489eac4
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2023-08-16 14:31:24 +0100

    Update filter copy

--- src/components/buttons/FilterToggle.tsx
+++ src/components/buttons/FilterToggle.tsx
@@ -8,7 +8,7 @@ interface FilterToggleProps {
 const FilterToggle = ({ toggle, isOpen }: FilterToggleProps) => {
   return (
     <button onClick={toggle} className="text-sm flex items-center bg-blue-500 mt-2 text-white flex-nowrap rounded-md px-4 py-2 md:hidden">
-      <span>Filter</span>{" "}
+      <span>{isOpen ? "Hide" : "Show"} filters</span>{" "}
       <div className={`ml-2 ${isOpen && "rotate-180"}`}>
         <DownArrowIcon />
       </div>
