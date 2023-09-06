f62fff4f2540ade235f407c5e481edb9a0c0a0f8
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2023-09-06 12:14:17 +0100

    Add responsive logos for mobile

--- src/sites/cclw/components/Header.tsx
+++ src/sites/cclw/components/Header.tsx
@@ -11,46 +11,47 @@ const Header = ({ background = true }) => {
   const router = useRouter();
 
   // Do not show search on specific pages
-  const showSearch = () => {
-    return !NON_SEARCH_PAGES.includes(router.pathname);
-  };
+  const showSearch = !NON_SEARCH_PAGES.includes(router.pathname);
 
   return (
-    <header data-cy="header" className={`${background ? "bg-cclw-dark" : ""} w-full md:pt-2 lg:pt-0`}>
+    <header data-cy="header" className={`${background ? "bg-cclw-dark" : ""} w-full pt-2 lg:pt-0`}>
       <div className="container">
-        <div className="grid grid-cols-2 auto-cols-auto lg:my-2">
-          <div className="items-end flex flex-grow-0">
+        <div className="flex flex-nowrap lg:my-2">
+          <div className="items-end flex flex-nowrap flex-1">
             <ExternalLink className="flex" url="https://www.lse.ac.uk/">
-              <div className="flex" data-cy="lse-logo">
-                <Image src="/images/partners/lse-logo.png" alt="LSE logo" width={34} height={35} />
-              </div>
+              <span className="flex" data-cy="lse-logo">
+                <Image src="/images/partners/lse-logo.png" alt="LSE logo" width={34} height={35} className="hidden md:block" />
+                <Image src="/images/partners/lse-logo.png" alt="LSE logo" width={23} height={24} className="md:hidden" />
+              </span>
             </ExternalLink>
             <ExternalLink className="flex" url="https://www.lse.ac.uk/granthaminstitute/">
-              <div className="flex" data-cy="gri-logo">
-                <Image src="/images/cclw/partners/gri-3-line-logo.png" alt="GRI logo" width={203} height={35} />
-              </div>
+              <span className="flex" data-cy="gri-logo">
+                <Image src="/images/cclw/partners/gri-3-line-logo.png" alt="GRI logo" width={203} height={35} className="hidden md:block" />
+                <Image src="/images/cclw/partners/gri-3-line-logo.png" alt="GRI logo" width={139} height={24} className="md:hidden" />
+              </span>
             </ExternalLink>
-            {showSearch() && (
+            {showSearch && (
               <div className="hidden ml-2 md:block md:w-[220px] lg:[280px]">
                 <FloatingSearch />
               </div>
             )}
           </div>
-          <div className="flex justify-self-end text-sm flex-grow-0 items-end">
+          <div className="flex text-sm items-end justify-end">
             <ExternalLink className="flex text-white hover:text-white hover:no-underline" url="https://www.climatepolicyradar.org">
               <div data-cy="cpr-logo" className="flex flex-col">
-                <span className="text-[11px] leading-none">Powered by</span>
-                <Image width={157} height={24} alt="Climate Policy Radar logo" src="/images/cpr-logo-horizontal.png" />
+                <span className="text-[10px] leading-none md:text-[11px]">Powered by</span>
+                <Image width={157} height={24} alt="Climate Policy Radar logo" src="/images/cpr-logo-horizontal.png" className="hidden md:block" />
+                <Image width={131} height={20} alt="Climate Policy Radar logo" src="/images/cpr-logo-horizontal.png" className="md:hidden" />
               </div>
             </ExternalLink>
             <Menu />
           </div>
-          <div className="col-span-2 flex-1 flex justify-center text-white order-last items-end basis-full text-center my-2 lg:my-0 lg:mt-2">
-            <div className="cclw-font font-bold text-xl md:text-2xl">
-              <LinkWithQuery href={`/`} className="text-white hover:no-underline hover:text-white" cypress="cclw-logo">
-                Climate Change Laws of the World
-              </LinkWithQuery>
-            </div>
+        </div>
+        <div className="text-white text-center my-2">
+          <div className="cclw-font font-bold text-xl md:text-2xl">
+            <LinkWithQuery href={`/`} className="text-white hover:no-underline hover:text-white" cypress="cclw-logo">
+              Climate Change Laws of the World
+            </LinkWithQuery>
           </div>
         </div>
       </div>
