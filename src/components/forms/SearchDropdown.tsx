ec847031ec1de20a6f88099df87145050ae6fdb8
Katy Baulch <kbaulch22@googlemail.com>
2023-10-02 14:49:50 +0100

    PDCT-345 Updated equality operator for null.

--- src/components/forms/SearchDropdown.tsx
+++ src/components/forms/SearchDropdown.tsx
@@ -27,6 +27,29 @@ export const SearchDropdown = ({ show = false, term, handleSearchClick, largeSpa
 
   const termWithoutGeography = (geography: string) => term.toLowerCase().replace(geography.toLowerCase(), "").trim();
 
+  function selectProps(...props) {
+    return function (obj) {
+      const newObj = {};
+      props.forEach((name) => {
+        newObj[name] = obj[name];
+      });
+
+      return newObj;
+    };
+  }
+
+  const termWithoutGeographies = (geographies: TGeography[]) => {
+    let intendedGeographies = geographies.map((geography) => geography.display_value.toLowerCase());
+    console.log(intendedGeographies);
+    // intendedGeographies = geographies.map(selectProps("display_value"));
+    // console.log(intendedGeographies);
+    for (let index = 0; index < geographies.length; index++) {
+      term = term.toLowerCase().replace(geographies[index].display_value.toLowerCase(), "").trim();
+    }
+    console.log(term);
+    return term.trim();
+  };
+
   if (!term || !show) return null;
 
   const handleClick = (e: any) => {
@@ -48,36 +71,64 @@ export const SearchDropdown = ({ show = false, term, handleSearchClick, largeSpa
     `flex flex-wrap items-center cursor-pointer py-2 px-4 block text-cpr-dark hover:bg-gray-200 focus:bg-gray-200 ${last ? "rounded-b-lg" : ""}`;
 
   const renderSearchSuggestion = (geographies: TGeography[]) => {
-    if (!geographies || geographies == null || geographies.length === 0) return;
+    if (!geographies || geographies === null || geographies.length === 0) return;
 
     // When multiple geography matches are found e.g., Sudan and South Sudan, select the geography with the longest
     // display name.
     let geography;
-    if (geographies.length > 1) {
-      geography = geographies.reduce(function (prev, current) {
-        return prev && prev.display_value.length > current.display_value.length ? prev : current;
-      });
-      geography = Object.keys(geography).length === 1 ? geography[0] : geography;
-    } else geography = geographies[0];
-    
+    // if (geographies.length > 1) {
+    //   // geography = geographies.reduce(function (prev, current) {
+    //   //   return prev && prev.display_value.length > current.display_value.length ? prev : current;
+    //   // });
+    //   // geography = Object.keys(geography).length === 1 ? geography[0] : geography;
+    //   geography = geographies;
+    // } else geography = geographies[0];
+
+    geography = geographies.length === 1 ? geographies[0] : geographies;
     const intendedGeography = geography;
     if (Object.keys(intendedGeography).length === 0) return;
 
-    if (Object.keys(intendedGeography).length === 1) {
-      if (!term.toLowerCase().includes(intendedGeography.display_value.toLowerCase())) return;
-      if (!termWithoutGeography(intendedGeography.display_value).trim().length) return;
-    }
+    console.log(Object.keys(intendedGeography).length);
+    // if (Object.keys(intendedGeography).length === 1) {
+    //   if (!term.toLowerCase().includes(intendedGeography.display_value.toLowerCase())) return;
+    //   if (!termWithoutGeography(intendedGeography.display_value).trim().length) return;
+    // }
 
     return (
       <ul>
-        {intendedGeography.length === 1}
+        {intendedGeography.length > 1 && (
+          <>
+            <li key={"intendedGeography.slug"}>
+              <p>
+                {intendedGeography.map((geography, index) => {
+                  const last = index + 1 === intendedGeography.length;
+                  return (
+                    <a href="#" className={anchorClasses(false)} onClick={(e) => handleSuggestionClick(e, intendedGeography[index])}>
+                      Did you mean to search for&nbsp;
+                      <span className="font-bold text-black">{termWithoutGeographies(intendedGeography[index])}</span>&nbsp;in&nbsp;
+                      <span className="font-bold text-black">{geography.display_value}</span>?
+                    </a>
+                  );
+                })}
+              </p>
+              <a href="#" className={anchorClasses(false)} onClick={(e) => handleSuggestionClick(e, intendedGeography)}>
+                Did you mean to search for&nbsp;
+                <span className="font-bold text-black">{termWithoutGeographies(intendedGeography)}</span>&nbsp;in&nbsp;
+                <span className="font-bold text-black">{intendedGeography.display_value}</span>&nbsp;and&nbsp;
+                <span className="font-bold text-black">{intendedGeography.display_value}</span>?
+              </a>
+            </li>
+          </>
+        )}
+
+        {/* {intendedGeography.length === 1}
         <li key={intendedGeography.slug}>
           <a href="#" className={anchorClasses(false)} onClick={(e) => handleSuggestionClick(e, intendedGeography)}>
             Did you mean to search for&nbsp;
             <span className="font-bold text-black">{termWithoutGeography(intendedGeography.display_value)}</span>&nbsp;in&nbsp;
             <span className="font-bold text-black">{intendedGeography.display_value}</span>?
           </a>
-        </li>
+        </li> */}
       </ul>
     );
   };
