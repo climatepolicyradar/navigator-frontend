005c1dc6cd15ee75764a783447639b6192652aca
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2023-11-22 09:07:54 +0000

    Initialise GTM correctly (#142)

--- src/components/cookies/CookieConsent.tsx
+++ src/components/cookies/CookieConsent.tsx
@@ -70,7 +70,6 @@ export const CookieConsent = () => {
           </div>
         </div>
       </div>
-      <Script async src="https://www.googletagmanager.com/gtm.js?id=GTM-NTNH983" />
       <Script id="google-analytics" strategy="afterInteractive">
         {`
             window.dataLayer = window.dataLayer || [];
@@ -79,6 +78,18 @@ export const CookieConsent = () => {
               ad_storage: "denied",
               analytics_storage: "denied",
             });
+          `}
+      </Script>
+      <Script id="gtm" strategy="afterInteractive">
+        {`
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({"gtm.start":new Date().getTime(),event:"gtm.js"});
+              var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!="dataLayer"?"&l="+l:"";
+              j.async=true;
+              j.src="https://www.googletagmanager.com/gtm.js?id="+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,"script","dataLayer","GTM-NTNH983");
           `}
       </Script>
       {cclwAnalyticsAllowed && (
@@ -97,8 +108,9 @@ export const CookieConsent = () => {
         </>
       )}
       {enableAnalytics && (
-        <Script id="hotjar" strategy="afterInteractive">
-          {`
+        <>
+          <Script id="hotjar" strategy="afterInteractive">
+            {`
             (function(h,o,t,j,a,r){
               h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
               h._hjSettings={hjid:3192374,hjsv:6};
@@ -108,7 +120,8 @@ export const CookieConsent = () => {
               a.appendChild(r);
             })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
           `}
-        </Script>
+          </Script>
+        </>
       )}
     </>
   );
