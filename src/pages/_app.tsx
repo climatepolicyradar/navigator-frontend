3c8639a0a081b8bda4027568906434f4fd5c0065
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2023-11-27 15:27:47 +0000

    Add new cclw favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -47,7 +47,7 @@ function MyApp({ Component, pageProps, theme, adobeApiKey }: TProps) {
   const dynamicTheme = theme ?? siteTheme;
   const dynamicAdobeKey = adobeApiKey ?? adobeKey;
 
-  const favicon = dynamicTheme === "cclw" ? "/images/cclw/images/favicon.png" : "/favicon.png";
+  const favicon = dynamicTheme === "cclw" ? "/images/cclw/favicon.png" : "/favicon.png";
 
   return (
     <QueryClientProvider client={queryClient}>
