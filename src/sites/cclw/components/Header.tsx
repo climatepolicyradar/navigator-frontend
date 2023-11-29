0b2f85d645180f13b8051f98202bb48614b3f0b5
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2023-11-29 11:51:32 +0000

    Separate out the cclw logo to fix blurry text

--- src/sites/cclw/components/Header.tsx
+++ src/sites/cclw/components/Header.tsx
@@ -18,8 +18,9 @@ const Header = ({ background = true }) => {
       <div className="container">
         <div className="flex flex-nowrap my-2">
           <div className="items-center flex flex-nowrap flex-1">
-            <LinkWithQuery href={`/`} className="flex" cypress="cclw-logo">
-              <Image src="/images/cclw/cclw-logo-light.png" alt="Climate Change Laws of the World logo" width={233} height={40} />
+            <LinkWithQuery href={`/`} className="flex items-center flex-nowrap gap-1" cypress="cclw-logo">
+              <Image src="/images/cclw/cclw-logo-globe.png" alt="Climate Change Laws of the World logo globe" width={50} height={50} />
+              <Image src="/images/cclw/cclw-logo-text-light.svg" alt="Climate Change Laws of the World logo text" width={275} height={42} />
             </LinkWithQuery>
             {showSearch && (
               <div className="hidden ml-6 md:block md:w-[340px]">
