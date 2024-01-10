590c93fd9428b7310bd352b5dab2330ca6e435d4
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2024-01-10 16:19:55 +0000

    Pass function and variable props to homepage component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,15 @@
-import React, { useEffect, useContext } from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/router";
-import { useTranslation } from "react-i18next";
 import useUpdateCountries from "@hooks/useUpdateCountries";
 import useConfig from "@hooks/useConfig";
-import Layout from "@components/layouts/LandingPage";
 import { QUERY_PARAMS } from "@constants/queryParams";
-
-import CPRLandingPage from "@cpr/pages/landing-page";
-import CCLWLandingPage from "@cclw/pages/landing-page";
-
-import { ThemeContext } from "@context/ThemeContext";
 import { triggerNewSearch } from "@utils/triggerNewSearch";
 
 const { default: Homepage } = await import(`/themes/${process.env.BUILDTIME_TEST}/pages/Homepage`);
 
 const IndexPage = () => {
-  const { t } = useTranslation(["searchStart", "searchResults"]);
   const router = useRouter();
   const { mutate: updateCountries } = useUpdateCountries();
-  const theme = useContext(ThemeContext);
 
   const configQuery = useConfig();
   const { data: { regions = [], countries = [] } = {} } = configQuery;
@@ -42,20 +33,12 @@ const IndexPage = () => {
 
   return (
     <>
-      {/* <Layout title={t("Law and Policy Search")}>
-        {theme === "cpr" && (
-          <CPRLandingPage
-            handleSearchInput={handleSearchInput}
-            handleSearchChange={handleSearchChange}
-            searchInput={(router.query[QUERY_PARAMS.query_string] as string) ?? ""}
-            exactMatch={router.query[QUERY_PARAMS.exact_match] === "true"}
-          />
-        )}
-        {theme === "cclw" && (
-          <CCLWLandingPage handleSearchInput={handleSearchInput} searchInput={(router.query[QUERY_PARAMS.query_string] as string) ?? ""} />
-        )}
-      </Layout> */}
-      <Homepage handleSearchInput={handleSearchInput} searchInput={(router.query[QUERY_PARAMS.query_string] as string) ?? ""} testIgnore="123" />
+      <Homepage
+        handleSearchInput={handleSearchInput}
+        handleSearchChange={handleSearchChange}
+        searchInput={(router.query[QUERY_PARAMS.query_string] as string) ?? ""}
+        exactMatch={router.query[QUERY_PARAMS.exact_match] === "true"}
+      />
     </>
   );
 };
