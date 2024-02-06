e1d43f1f83381aa10322fa89b57034f80f41adb4
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2024-02-06 09:14:16 +0000

    Add cpr theme env check for tests

--- e2e/cypress/e2e/cpr/components/navigation.cy.js
+++ e2e/cypress/e2e/cpr/components/navigation.cy.js
@@ -2,20 +2,22 @@
 
 import { Navigation } from "../../../support/page_objects/NavigationConstructor";
 
-describe.skip("Site navigation", () => {
-  before(() => {
-    cy.visit("/");
-  });
+if (Cypress.env("THEME") === "cpr") {
+  describe.skip("Site navigation", () => {
+    before(() => {
+      cy.visit("/");
+    });
 
-  it("Click each dropdown menu icon", () => {
-    Navigation.aboutUsPage().methodologyPage();
+    it("Click each dropdown menu icon", () => {
+      Navigation.aboutUsPage().methodologyPage();
+    });
+    it("Clicking logo should return to landing page", () => {
+      Navigation.logoLink();
+    });
+    it("Click each footer link", () => {
+      // Close cookie consent popup
+      cy.get('[data-cy="cookie-consent-reject"]').click();
+      Navigation.footerLinkMethodology().footerLinkTerms().footerLinkPrivacy();
+    });
   });
-  it("Clicking logo should return to landing page", () => {
-    Navigation.logoLink();
-  });
-  it("Click each footer link", () => {
-    // Close cookie consent popup
-    cy.get('[data-cy="cookie-consent-reject"]').click();
-    Navigation.footerLinkMethodology().footerLinkTerms().footerLinkPrivacy();
-  });
-});
+}
