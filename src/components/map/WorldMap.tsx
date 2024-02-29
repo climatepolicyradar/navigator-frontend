c5e9d8249c17a9fe438a2050ce938d2a0ca8d9b4
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2024-02-29 17:01:12 +0000

    Configure translate boundaries

--- src/components/map/WorldMap.tsx
+++ src/components/map/WorldMap.tsx
@@ -2,9 +2,17 @@ import React, { useState } from "react";
 import { ComposableMap, Geographies, Geography, Graticule, Marker, Sphere, ZoomableGroup, Point as TPoint } from "react-simple-maps";
 import { GEO_CENTER_POINTS } from "@constants/mapCentres";
 import useConfig from "@hooks/useConfig";
-import { set } from "react-hook-form";
 
 const geoUrl = "/data/map/world-countries-50m.json";
+
+type TGeo = {
+  geometry: { type: string; coordinates: [number, number][] };
+  id: string;
+  properties: { name: string };
+  rsmKey: string;
+  svgPath: string;
+  type: string;
+};
 
 const geoStyle = (isActive: boolean) => {
   return {
@@ -53,15 +61,15 @@ export default function MapChart() {
   const [mapCenter, setMapCenter] = useState<TPoint>([0, 0]);
   const [mapZoom, setMapZoom] = useState(1);
 
-  const handleGeoClick = (geo) => {
-    console.log("handleGeoClick", geo);
-    setActiveGeography(geo?.properties?.name ?? "");
+  const handleGeoClick = (geo: TGeo) => {
+    setActiveGeography(geo.properties.name);
+    const geography = countries.find((country) => country.display_value === geo.properties.name);
+    setMapCenter(GEO_CENTER_POINTS[geography.value]);
+    setMapZoom(3);
   };
 
   const handleMarkerClick = (countryCode: string) => {
-    console.log("handleMarkerClick", countryCode);
     const geography = countries.find((country) => country.value === countryCode);
-    console.log("filtered geography", geography);
     setActiveGeography(geography?.display_value ?? "");
     setMapCenter(GEO_CENTER_POINTS[countryCode]);
     setMapZoom(3);
@@ -76,8 +84,15 @@ export default function MapChart() {
   return (
     <div className="map-container my-8">
       <button onClick={handleResetMapClick}>Reset map</button>
-      <ComposableMap>
-        <ZoomableGroup center={mapCenter} zoom={mapZoom}>
+      <ComposableMap projection="geoMercator" projectionConfig={{ scale: 160 }}>
+        <ZoomableGroup
+          center={mapCenter}
+          zoom={mapZoom}
+          translateExtent={[
+            [-600, -400],
+            [1200, 800],
+          ]}
+        >
           <Sphere id="1" stroke="#E4E5E6" strokeWidth={0.2} fill="none" />
           <Graticule stroke="#E4E5E6" strokeWidth={0.2} />
           <Geographies geography={geoUrl}>
