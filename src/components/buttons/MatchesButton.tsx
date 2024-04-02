6651da1e6d3918d3e12642ff203b777fd8b947ba
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2024-04-02 16:54:27 +0100

    Update matches button to show correct count

--- src/components/buttons/MatchesButton.tsx
+++ src/components/buttons/MatchesButton.tsx
@@ -10,10 +10,13 @@ const MatchesButton = ({ dataAttribute, count, overideText }: TProps) => {
   if (!numberOfMatches && !overideText) return null;
 
   return (
-    <span data-cy="document-matches-button" data-analytics="document-matches-button" data-slug={dataAttribute} className="text-sm text-blue-400 shrink-0">
-      {overideText
-        ? overideText
-        : `View ${numberOfMatches > 10 ? 10 : numberOfMatches}${numberOfMatches > 10 ? "+" : ""} match${numberOfMatches > 1 ? "es" : ""}`}
+    <span
+      data-cy="document-matches-button"
+      data-analytics="document-matches-button"
+      data-slug={dataAttribute}
+      className="text-sm text-blue-400 shrink-0"
+    >
+      {overideText ? overideText : `View ${numberOfMatches} match${numberOfMatches > 1 ? "es" : ""}`}
     </span>
   );
 };
