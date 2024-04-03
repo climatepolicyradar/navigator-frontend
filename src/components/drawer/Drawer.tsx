8ee6675433fece416ace7816cf6281ad3b74f81f
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2024-04-03 12:11:22 +0100

    Update drawer styling and document details

--- src/components/drawer/Drawer.tsx
+++ src/components/drawer/Drawer.tsx
@@ -26,7 +26,7 @@ const Slideout = ({ children, show, setShow }: SlideoutProps) => {
   return (
     <div
       ref={ref}
-      className={`fixed top-0 right-0 w-11/12 h-screen bg-white z-50 transition duration-300 origin-left transform shadow-2xl shadow-black/40 md:w-1/2 xl:w-1/3 ${
+      className={`fixed top-0 right-0 w-11/12 h-screen bg-white z-50 transition duration-300 origin-left transform shadow-2xl shadow-black/40 md:w-2/3 xl:w-5/12 ${
         show ? "translate-x-0" : "translate-x-[110%]"
       }`}
     >
--- src/components/drawer/FamilyMatchesDrawer.tsx
+++ src/components/drawer/FamilyMatchesDrawer.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { TMatchedFamily } from "@types";
 import { FamilyMeta } from "@components/document/FamilyMeta";
@@ -5,18 +6,39 @@ import PassageMatches from "@components/PassageMatches";
 import { LinkWithQuery } from "@components/LinkWithQuery";
 import { matchesCount } from "@utils/matchesCount";
 import { CleanRouterQuery } from "@utils/cleanRouterQuery";
+import { truncateString } from "@helpers/index";
+import { MAX_FAMILY_SUMMARY_LENGTH_BRIEF } from "@constants/document";
+import Button from "@components/buttons/Button";
 
 type TProps = {
   family?: TMatchedFamily;
 };
 
+const pluralise = (count: number, singular: string, plural: string) => {
+  return count === 1 ? singular : plural;
+};
+
 export const FamilyMatchesDrawer = ({ family }: TProps) => {
   const router = useRouter();
+  const [showFullSummary, setShowFullSummary] = useState(false);
+  const [summary, setSummary] = useState("");
+
+  useEffect(() => {
+    if (family.family_description) {
+      const text = family.family_description;
+      if (showFullSummary) {
+        setSummary(text);
+      } else {
+        setSummary(truncateString(text, MAX_FAMILY_SUMMARY_LENGTH_BRIEF));
+      }
+    }
+  }, [family.family_description, showFullSummary]);
+
   if (!family) return null;
   const { family_geography, family_name, family_category, family_date, family_documents } = family;
   const numberOfMatches = matchesCount(family_documents);
   const numberOfDocsWithMatches = family_documents.filter((document) => document.document_passage_matches.length > 0).length;
-  const matchesDescription = `${numberOfMatches} ${numberOfMatches === 1 ? "match" : "matches"} in ${numberOfDocsWithMatches} ${
+  const matchesDescription = `${numberOfMatches} ${pluralise(numberOfMatches, "match", "matches")} in ${numberOfDocsWithMatches} ${
     numberOfDocsWithMatches === 1 ? "document" : "documents"
   }`;
 
@@ -27,26 +49,52 @@ export const FamilyMatchesDrawer = ({ family }: TProps) => {
     router.push({ pathname: `/documents/${document.document_slug}`, query: queryObj });
   };
 
+  const handleViewOtherDocsClick = (e: React.FormEvent<HTMLButtonElement>, documentSlug: string) => {
+    e.preventDefault();
+    if (!documentSlug) return;
+    router.push({ pathname: `/documents/${documentSlug}`, query: router.query });
+  };
+
   return (
     <>
       <div className="h-full flex flex-col">
-        <div className="p-4 pt-10 bg-gray-100">
-          <h4 className="text-base">{family_name}</h4>
-          <div className="flex flex-wrap text-sm gap-1 text-gray-700 mt-2 items-center font-medium">
+        <div className="p-4 pb-0 pr-10 md:p-8 md:pr-12 md:pb-0">
+          <h4 className="text-xl font-bold">{family_name}</h4>
+          <div className="flex flex-wrap text-sm gap-1 text-gray-700 mt-2 items-center font-medium middot-between">
             <FamilyMeta category={family_category} geography={family_geography} date={family_date} />
           </div>
+          <div className="mt-6">
+            <h5 className="text-base mb-2">Summary</h5>
+            <div className="text-content mb-2" dangerouslySetInnerHTML={{ __html: summary }} />
+            <LinkWithQuery href={`/document/${family.family_slug}`} className="text-sm alt">
+              View full summary and timeline
+            </LinkWithQuery>
+          </div>
         </div>
-        <div className="p-4 flex-grow flex flex-col overflow-hidden">
-          <div className="text-sm text-gray-700 mb-4">{matchesDescription}</div>
-          <div className="flex-grow pr-1 overflow-y-scroll scrollbar-thumb-gray-200 scrollbar-thin scrollbar-track-white scrollbar-thumb-rounded-full hover:scrollbar-thumb-gray-500">
+        <div className="p-4 pt-0 flex-grow flex flex-col overflow-hidden md:p-8 md:pt-0">
+          <div className="my-6">
+            <div className="flex justify-between items-baseline">
+              <h5 className="text-base mb-2">Documents</h5>
+              <LinkWithQuery href={`/document/${family.family_slug}`} className="text-sm alt">
+                View document overview
+              </LinkWithQuery>
+            </div>
+            <div className="text-sm">{matchesDescription}</div>
+          </div>
+          <div className="flex-grow pr-1 overflow-y-scroll scrollbar-narrow">
             {family_documents.map((document, docIndex) => (
-              <div key={document.document_slug} className="mb-4">
+              <div key={document.document_slug} className="mb-6">
                 <LinkWithQuery href={`/documents/${document.document_slug}`}>{document.document_title}</LinkWithQuery>
-                <PassageMatches
-                  passages={document.document_passage_matches}
-                  onClick={(index) => onPassageClick(index, docIndex)}
-                  pageColour="gray-600"
-                />
+                <PassageMatches passages={document.document_passage_matches.slice(0, 5)} onClick={(index) => onPassageClick(index, docIndex)} />
+                <Button
+                  color="clear"
+                  data-cy="view-document-button"
+                  onClick={(e) => handleViewOtherDocsClick(e, document.document_slug)}
+                  extraClasses="text-sm text-blue-600"
+                >
+                  View {document.document_passage_matches.length} {pluralise(document.document_passage_matches.length, "match", "matches")}{" "}
+                  highlighted in document
+                </Button>
               </div>
             ))}
           </div>
