c9ef1b344b703b0dcfaa2e5a69f1a3ed23ef1110
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2024-04-11 10:29:27 +0100

    Hide world map on mobile devices

--- themes/cclw/pages/homepage.tsx
+++ themes/cclw/pages/homepage.tsx
@@ -27,7 +27,7 @@ const LandingPage = ({ handleSearchInput, searchInput }: TProps) => {
           <Header background={false} />
           <Hero handleSearchInput={handleSearchInput} searchInput={searchInput} />
         </div>
-        <div className="container">
+        <div className="container hidden md:block">
           <WorldMap />
         </div>
         <div className="container my-12" data-cy="powered-by">
