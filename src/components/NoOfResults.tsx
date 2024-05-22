2d6ebbbbc62eb1665a899530942f913b764c8afd
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2024-05-22 17:08:02 +0100

    Incorporate MAX_RESULTS constant into the no. of results messaging

--- src/components/NoOfResults.tsx
+++ src/components/NoOfResults.tsx
@@ -1,5 +1,7 @@
 import Tooltip from "@components/tooltip";
 
+import { MAX_RESULTS } from "@constants/paging";
+
 type TProps = {
   hits: number;
   queryString: string | string[];
@@ -7,10 +9,10 @@ type TProps = {
 
 export const NoOfResults = ({ hits, queryString }: TProps) => {
   let resultsMsg = `Showing`;
-  if (hits < 100) {
+  if (hits < MAX_RESULTS) {
     resultsMsg += ` ${hits} results`;
   } else {
-    resultsMsg += ` the top 100 results`;
+    resultsMsg += ` the top ${MAX_RESULTS} results`;
   }
   return (
     <>
@@ -18,14 +20,14 @@ export const NoOfResults = ({ hits, queryString }: TProps) => {
       {queryString && (
         <>
           for "<span className="font-bold">{queryString}</span>"
-          {hits >= 100 && (
+          {hits >= MAX_RESULTS && (
             <div className="ml-2 inline-block">
               <Tooltip
                 id="search-results-number"
                 tooltip={
                   <>
-                    We limit the number of search results to 100 so that you get the best performance from our tool. We're working on a way to remove
-                    this limit.
+                    We limit the number of search results to {MAX_RESULTS} so that you get the best performance from our tool. We're working on a way
+                    to remove this limit.
                   </>
                 }
                 icon="i"
