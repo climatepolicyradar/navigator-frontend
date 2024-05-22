67c5a1363928a5eb6ac805c535110d10aa896c06
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2024-05-22 12:52:28 +0100

    Update comment

--- src/utils/getCurrentPage.ts
+++ src/utils/getCurrentPage.ts
@@ -1,6 +1,6 @@
 export const getCurrentPage = (offset: number, perPage: number, pagesPerToken: number, cts: string[], ct: string = "") => {
   let tokenIndex = cts.indexOf(ct);
   // each token represents a number of pages, so we need to calculate the page based on the offset and perPage
-  const tokenPage = Math.ceil(offset / perPage) + 1; // Not zero-indexed
+  const tokenPage = Math.ceil(offset / perPage) + 1; // Pages are not zero-indexed
   return tokenIndex * pagesPerToken + tokenPage;
 };
