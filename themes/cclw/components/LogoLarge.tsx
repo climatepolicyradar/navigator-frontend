42f4532636a5c5a5a0a5efa491db93f043dab8aa
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2024-06-03 11:05:20 +0100

    Avoid next image sclae error due to width

--- themes/cclw/components/LogoLarge.tsx
+++ themes/cclw/components/LogoLarge.tsx
@@ -4,7 +4,7 @@ export const LogoLarge = () => {
   return (
     <div className="flex items-center flex-nowrap gap-6" data-cy="cclw-logo">
       <Image src="/images/cclw/cclw-logo-globe-large.png" alt="Climate Change Laws of the World logo globe" width={109} height={109} />
-      <Image src="/images/cclw/cclw-logo-text-light.svg" alt="Climate Change Laws of the World logo text" width={295} height={45} />
+      <Image src="/images/cclw/cclw-logo-text-light.svg" alt="Climate Change Laws of the World logo text" width={296} height={45} />
     </div>
   );
 };
