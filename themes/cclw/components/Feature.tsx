de4ba84d3b3e777660d9763c565028084453a550
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2024-06-24 12:38:18 +0100

    Fix spacing and font size on all widths

--- themes/cclw/components/Feature.tsx
+++ themes/cclw/components/Feature.tsx
@@ -14,11 +14,11 @@ export const Feature = ({ heading, contentSide = "left", image, imageAlt, childr
     <div className="bg-cclw-dark text-white py-12">
       <div className="container md:flex gap-8">
         <div className={`basis-1/2 ${contentSide === "right" ? "order-last" : ""}`}>
-          {heading && <h3 className="text-3xl md:text-h2 lg:text-h1 lg:leading-[72px] text-white">{heading}</h3>}
+          {heading && <h3 className="text-3xl md:text-h2 xl:text-h1 xl:leading-[72px] text-white">{heading}</h3>}
           {children}
         </div>
         {image && (
-          <div className="mt-8 basis-1/2 rounded-2xl border-[12px] border-gray-300 bg-gray-300 shadow-2xl self-start">
+          <div className="mt-8 basis-1/2 rounded-2xl border-[12px] border-gray-300 bg-gray-300 shadow-2xl self-start md:mt-0">
             <div>
               <Image
                 src={`/images/cclw/images/${image}`}
