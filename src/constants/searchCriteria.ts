9845da55d59bab6db2ddc061956806ae9b748ee9
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2024-07-08 17:41:09 +0100

    Increase the max passages by default

--- src/constants/searchCriteria.ts
+++ src/constants/searchCriteria.ts
@@ -5,7 +5,7 @@ import { TSearchCriteria } from "@types";
 export const initialSearchCriteria: TSearchCriteria = {
   query_string: "",
   exact_match: false,
-  max_passages_per_doc: 5,
+  max_passages_per_doc: 50,
   keyword_filters: {},
   year_range: [minYear, currentYear()],
   sort_field: null,
