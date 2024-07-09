5fcea39ce93286b669bfed04b849f884e648497c
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2024-07-09 16:18:52 +0100

    Use constant in default search criteria

--- src/constants/searchCriteria.ts
+++ src/constants/searchCriteria.ts
@@ -1,11 +1,11 @@
 import { minYear, currentYear } from "@constants/timedate";
-import { RESULTS_PER_PAGE } from "@constants/paging";
+import { RESULTS_PER_PAGE, PASSAGES_PER_DOC } from "@constants/paging";
 import { TSearchCriteria } from "@types";
 
 export const initialSearchCriteria: TSearchCriteria = {
   query_string: "",
   exact_match: false,
-  max_passages_per_doc: 50,
+  max_passages_per_doc: PASSAGES_PER_DOC,
   keyword_filters: {},
   year_range: [minYear, currentYear()],
   sort_field: null,
