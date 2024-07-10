f73c1e67a79289ff9bab08fd461d0f397090c2a3
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2024-07-10 10:07:57 +0100

    Disable screenshots in Cypress on failures

--- e2e/cypress.config.js
+++ e2e/cypress.config.js
@@ -8,6 +8,7 @@ module.exports = defineConfig({
   viewportWidth: 1280,
   viewportHeight: 800,
   chromeWebSecurity: false,
+  screenshotOnRunFailure: true,
   video: false,
   e2e: {
     baseUrl: "http://localhost:3000",
