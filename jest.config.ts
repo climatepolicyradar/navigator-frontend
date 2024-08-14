186b67d632fdbd49be8ae7d84446eb9aee821af3
Osneil Drakes <osneildrakes@Osneils-MacBook-Pro.local>
2024-08-14 15:49:02 +0100

    chore: move jest configs into config file

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  testEnvironment: "jsdom",
+  verbose: true,
+
+  moduleNameMapper: {
+    // Map `@mcf/components` to the correct directory
+    "^@constants/(.*)$": "<rootDir>/src/constants/$1",
+    "^@components/(.*)$": "<rootDir>/src/components/$1",
+    "^@hooks/(.*)$": "<rootDir>/src/hooks/$1",
+    "^@mcf/(.*)$": "<rootDir>/themes/mcf/$1",
+    "^@utils/(.*)$": "<rootDir>/src/utils/$1",
+    // Add other aliases if needed
+  },
+};
+
+export default config;
