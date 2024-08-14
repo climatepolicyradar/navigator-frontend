22b95132836352c68c315605a9dea7b4543659cd
Osneil Drakes <osneildrakes@Osneils-MacBook-Pro.local>
2024-08-14 13:38:58 +0100

    feat: add search component to skeleton app
    - this reuses the component in the cpr app and repurposes it for the mcf
      custom app
    - updates to the data, filtering and a methodology page will be handled
      in a separate PR

--- themes/mcf/pages/homepage.tsx
+++ themes/mcf/pages/homepage.tsx
@@ -1,3 +1,25 @@
-const LandingPage = ({}) => <div> Hello World </div>;
+import React from "react";
+
+// generic layer component
+import Layout from "@components/layouts/LandingPage";
+
+import { Hero } from "@mcf/components";
+
+type TProps = {
+  handleSearchInput: (term: string, filter?: string, filterValue?: string) => void;
+  searchInput: string;
+};
+
+const LandingPage = ({ handleSearchInput, searchInput }: TProps) => {
+  return (
+    <Layout title="MCF">
+      <main id="main" className="relative h-full">
+        <div className="bg-cclw-dark text-white h-full">
+          <Hero handleSearchInput={handleSearchInput} searchInput={searchInput} />
+        </div>
+      </main>
+    </Layout>
+  );
+};
 
 export default LandingPage;
