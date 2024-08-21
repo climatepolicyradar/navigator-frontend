2cc4b03aa7184ec1be2dca4e120b5f997042a8d9
Osneil Drakes <osneildrakes@Osneils-MacBook-Pro.local>
2024-08-21 13:53:20 +0100

    refactor: update custom app to take another variable for the theme name

--- build-scripts/build-custom-app.sh
+++ build-scripts/build-custom-app.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Prompt the user for the name of the new directory
-read -r -p "Enter the name of the new custom app (use camel-case if required): " custom_app_name
+# Prompt the user for the name of the new custom app website
+read -r -p "Enter the name of the new custom app website (use camel-case if required): " custom_app_name
+
+read -r -p "Input the acronym for the custom app theme (e.g. CPR, CCLW, OEP, MCF...): " theme
+theme="$(tr "[:upper:]" "[:lower:]" <<<"${theme}")"
 
 # Define the content for the main.tsx file
 read -r -d '' MAIN_CONTENT <<EOM
@@ -58,7 +61,7 @@ import React from "react";
 // generic layer component
 import Layout from "@components/layouts/LandingPage";
 
-import { Hero } from "@${custom_app_name}/components/Hero";
+import Hero from "@${theme}/components/Hero";
 
 type TProps = {
   handleSearchInput: (term: string, filter?: string, filterValue?: string) => void;
@@ -84,7 +87,7 @@ EOM
 
 # Define the content for the hero.
 
-read -r -d '' HERO_CONTENT <<EOF
+read -r -d '' HERO_CONTENT <<EOM
 
 import LandingSearchForm from "./LandingSearchForm";
 
@@ -94,13 +97,13 @@ type TProps = {
   searchInput: string;
 };
 
-export const Hero = ({ handleSearchInput, searchInput }: TProps) => {
+const Hero = ({ handleSearchInput, searchInput }: TProps) => {
   return (
     <div className="pb-6 text-white pt-[28px] sm:pt-[48px] md:pt-[80px] lg:pt-[100px] xl:pt-[140px]">
       <div className="container">
         <div className="flex flex-col items-center justify-center mb-6">
 		  <p className="font-medium tracking-slight text-lg lg:text-3xl" data-cy="intro-message">
-          ${custom_app_name} custom app
+          ${custom_app_name} Custom App
         </p>
         </div>
         <div className="max-w-screen-md mx-auto mt-6">
@@ -111,8 +114,19 @@ export const Hero = ({ handleSearchInput, searchInput }: TProps) => {
   );
 };
 
+export default Hero;
 
-EOF
+
+EOM
+
+# Define the content for MethodologyLink.tsx
+read -r -d '' METHODOLOGY_LINK_CONTENT <<EOM
+import { LinkWithQuery } from "@components/LinkWithQuery";
+
+const MethodologyLink = () => <LinkWithQuery href="/methodology">our methodology page</LinkWithQuery>;
+
+export default MethodologyLink;
+EOM
 
 # Define the content for the routes.json
 
@@ -150,22 +164,22 @@ if [[ ! -d ${themes_path} ]]; then
 fi
 
 # Define the full path to the new directory
-new_dir="${themes_path}/${custom_app_name}"
+new_dir="${themes_path}/${theme}"
 # Define the path to the subdirectories
 components_dir="${new_dir}/components"
 layouts_dir="${new_dir}/layouts"
 pages_dir="${new_dir}/pages"
 styles_dir="${new_dir}/styles"
 tests_dir="${new_dir}/tests"
-e2e_test_dir="${e2e_path}/cypress/e2e/${custom_app_name}"
+e2e_test_dir="${e2e_path}/cypress/e2e/${theme}"
 
 # Check if the directory already exists
 if [[ -d ${new_dir} ]]; then
-	echo "You are trying to duplicate a Custom App, ${new_dir} already exists."
+	echo "You are trying to duplicate a Custom App, ${new_dir} already exists. Please delete this folder and its contents and try again if you want to create a new custom app at this path"
 else
 	printf "The custom app does not exist, creating it now with the required sub-directories and files..."
 	mkdir -p "${new_dir}" "${components_dir}" "${layouts_dir}" "${pages_dir}" "${styles_dir}" "${tests_dir}" "${e2e_test_dir}"
-	printf " -> Created custom app directory for %s: %s\n\n" "${custom_app_name}" "${new_dir}"
+	printf " -> Created custom app directory for %s: %s\n\n" "${theme}" "${new_dir}"
 
 	# Create the required files
 	touch "${components_dir}/index.ts"
@@ -175,6 +189,9 @@ else
 	echo "${INDEX_TS_CONTENT}" >"${components_dir}/index.ts"
 	cp -r "${themes_path}/cclw/components/LandingSearchForm.tsx" "${components_dir}/LandingSearchForm.tsx"
 	printf "%s" "${HERO_CONTENT}" >"${components_dir}/Hero.tsx"
+
+	touch "${components_dir}/MethodologyLink.tsx"
+	echo "${METHODOLOGY_LINK_CONTENT}" >"${components_dir}/MethodologyLink.tsx"
 
 	touch "${pages_dir}/homepage.tsx"
 	touch "${pages_dir}/methodology.tsx"
@@ -189,7 +206,7 @@ else
 	printf "Copied tailwind.config.js file from %s to %s.\n\n" "${themes_path}/cclw" "${new_dir}"
 
 	# Copy the styles.scss file from the cclw theme directory
-	cp -r "${themes_path}/cpr/styles/styles.scss" "${styles_dir}/styles.scss"
+	cp -r "${themes_path}/cclw/styles/styles.scss" "${styles_dir}/styles.scss"
 	printf "Copied styles.scss file from %s to %s.\n\n" "${themes_path}/cclw" "${styles_dir}"
 
 	# Create the redirects.json file
@@ -206,14 +223,14 @@ else
 	touch "${e2e_test_pages_dir}/homepage.cy.js"
 
 	# Add the new custom app to ci-cd.yml file
-	sed -i.bak "s/theme: \[cpr, cclw, mcf\]/theme: [cpr, cclw, mcf, ${custom_app_name}]/" .github/workflows/ci-cd.yml && rm .github/workflows/ci-cd.yml.bak
-	echo "Added ${custom_app_name} to the theme list in ci-cd.yml"
+	sed -i '' "s/theme: \[\(.*\)\]/theme: [\1, ${theme}]/" .github/workflows/ci-cd.yml
+	echo "Added ${theme} to the theme list in ci-cd.yml"
 
 	# Add the new custom app path to tsconfig.json
 	tsconfig_file="tsconfig.json"
 	temp_tsconfig="temp_tsconfig.json"
 
-	awk -v app="${custom_app_name}" '
+	awk -v app="${theme}" '
 	BEGIN { in_paths = 0; added = 0 }
 	/"paths":/  { in_paths = 1 }
 	in_paths && /}/ && !added {
@@ -223,12 +240,12 @@ else
 	{ print }
 	' "${tsconfig_file}" >"${temp_tsconfig}" && mv "${temp_tsconfig}" "${tsconfig_file}"
 
-	echo "Added @${custom_app_name}/* path to tsconfig.json"
+	echo "Added @${theme}/* path to tsconfig.json"
 
 	# Update TTheme type in types.ts
-	sed -i.bak "s/export type TTheme = /export type TTheme = \"${custom_app_name}\" | /" src/types/types.ts
+	sed -i.bak "s/export type TTheme = /export type TTheme = \"${theme}\" | /" src/types/types.ts
 
-	echo "Added ${custom_app_name} to the TTheme type in types.ts"
+	echo "Added ${theme} to the TTheme type in types.ts"
 
-	printf "\033[1;34mNow that's done, set the environment variable by running : export THEME=%s\n \033[0m\n" "${custom_app_name}"
+	printf "\033[1;34mNow that's done, set the environment variable by running : export THEME=%s\n \033[0m\n" "${theme}"
 fi
