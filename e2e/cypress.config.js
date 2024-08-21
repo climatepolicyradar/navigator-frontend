b4c2c6fc66419f107cb71e55ed110091dea9cb2f
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-08-21 14:06:14 +0100

    Feature/pdct 1275 add the encrypted token into the application factory front (#208)
    * Add APP_CONFIG_TOKEN to example env file
    
    * Turn off screenshots for cypress testing
    
    * Fix typo in README
    
    * Create CPR e2e test for non landing page to see if fails
    
    * Don't test for page selectors in CPR terms of use
    
    * Remove aliasing of APP_CONFIG_TOKEN from next.config.js
    
    * Rename to NEXT_PUBLIC_APP_TOKEN from NEXT_PUBLIC_APP_CONFIG_TOKEN
    
    * Rename from APP_CONFIG_TOKEN to NEXT_PUBLIC_APP_TOKEN
    
    * Remove NEXT_PUBLIC_APP_TOKEN from cypress env
    
    * Set NEXT_PUBLIC_APP_TOKEN in makefile

--- e2e/cypress.config.js
+++ e2e/cypress.config.js
@@ -8,7 +8,7 @@ module.exports = defineConfig({
   viewportWidth: 1280,
   viewportHeight: 800,
   chromeWebSecurity: false,
-  screenshotOnRunFailure: true,
+  screenshotOnRunFailure: false,
   video: false,
   e2e: {
     baseUrl: "http://localhost:3000",
