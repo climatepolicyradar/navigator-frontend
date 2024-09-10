eb98a9c54f85665837eeb4e212f212e6b36fa557
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2024-09-10 14:38:50 +0100

    Remove HTML from the document description

--- src/utils/getFamilyMetaDescription.ts
+++ src/utils/getFamilyMetaDescription.ts
@@ -1,6 +1,10 @@
 import { TCategory } from "@types";
 import { getCategoryName } from "@helpers/getCategoryName";
 
+const stripHtml = (html: string) => {
+  return html.replace(/<[^>]*>?/gm, "");
+};
+
 export const getFamilyMetaDescription = (summary: string, geo: string, category: TCategory) => {
-  return `${geo} | ${getCategoryName(category)} | ${summary.substring(0, 600)}`;
+  return `${geo} | ${getCategoryName(category)} | ${stripHtml(summary).substring(0, 600)}`;
 };
