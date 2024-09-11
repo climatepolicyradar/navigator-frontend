93aed6b2c4aba7831b46d7e4da9a94cd2cdf4100
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2024-09-11 14:40:03 +0100

    Remove unused components

--- src/components/blocks/Error.tsx
+++
@@ -1,9 +0,0 @@
-interface FormFieldErrorProps {
-  message: string;
-}
-
-const FormFieldError = ({ message }: FormFieldErrorProps) => {
-  return <div className="error w-full text-red-600 mt-1">{message}</div>;
-};
-
-export default FormFieldError;
