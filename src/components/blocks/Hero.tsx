a3d4151424515b7ada2f8bdbff44f2ebba9bf1ce
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2024-09-11 14:43:42 +0100

    Remove default hero

--- src/components/blocks/Hero.tsx
+++
@@ -1,9 +0,0 @@
-import { FC, ReactNode } from "react";
-
-type TProps = {
-  children?: ReactNode;
-};
-
-export const Hero: FC<TProps> = ({ children }) => {
-  return <section className="absolute inset-0 z-10 flex flex-col items-center justify-center min-h-[800px]">{children}</section>;
-};
