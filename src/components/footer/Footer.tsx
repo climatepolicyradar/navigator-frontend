99eb7a3ce3caf0814e2a15a42d1917ea86f1f720
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2024-09-17 16:46:30 +0100

    Update footer with new panel, fix spacing on mobile

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,18 +1,19 @@
 import { ExternalLink } from "@components/ExternalLink";
 import FooterLinks from "./FooterLinks";
+import { FullWidth } from "@components/panels/FullWidth";
 
 const Footer = () => {
   return (
-    <footer className="py-12 dark-gradient flex items-center shrink-0">
-      <div className="container">
+    <footer className="py-12 dark-gradient">
+      <FullWidth>
         <p className="font-medium text-lg mb-6 text-white md:text-center">
           To report a problem email us at support@climatepolicyradar.org <br /> Spotted missing or inaccurate data?{" "}
           <ExternalLink url="https://form.jotform.com/233294135296359" className="text-white underline hover:text-white">
             Let us know
           </ExternalLink>
         </p>
-        <FooterLinks landing={true} />
-      </div>
+        <FooterLinks />
+      </FullWidth>
     </footer>
   );
 };
