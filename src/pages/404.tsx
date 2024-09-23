3d07d85883301bc9638bd0524aeb627a4a32e7a9
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2024-09-23 12:31:56 +0100

    Update error pages with panels

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,16 +1,18 @@
-import { ExternalLink } from "@components/ExternalLink";
 import Layout from "../components/layouts/Main";
+import { SiteWidth } from "@components/panels/SiteWidth";
+
+import { ExternalLink } from "@components/ExternalLink";
 
 export default function NotFound() {
   return (
     <Layout title={"Page not found"}>
       <section>
-        <div className="text-content px-4 container mb-12">
+        <SiteWidth extraClasses="text-content px-4 mb-12">
           <h1 className="my-8">Sorry, we can't find that page</h1>
           <p>
             Please <ExternalLink url="mailto:support@climatepolicyradar.org">contact us</ExternalLink> if you would like to report the issue.
           </p>
-        </div>
+        </SiteWidth>
       </section>
     </Layout>
   );
