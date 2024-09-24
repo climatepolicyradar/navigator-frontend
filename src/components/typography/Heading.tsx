1e341a0e1acf2a489418757e6747aa7db5fee33a
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2024-09-24 16:08:29 +0100

    Add new font colours

--- src/components/typography/Heading.tsx
+++ src/components/typography/Heading.tsx
@@ -2,26 +2,28 @@ type THeadingLevels = 1 | 2 | 3 | 4 | 5 | 6;
 
 type TProps = {
   level?: THeadingLevels;
-  as?: THeadingLevels;
   extraClasses?: string;
   children: React.ReactNode;
 };
 
-export const Heading = ({ level, as, extraClasses, children }: TProps) => {
+export const coreClasses = "text-textDark font-medium";
+export const h1Classes = "text-4xl";
+export const h2Classes = "text-2xl";
+export const h3Classes = "text-xl";
+
+export const Heading = ({ level, extraClasses, children }: TProps) => {
   switch (level) {
     case 1:
-      return <h1 className={` ${extraClasses}`}>{children}</h1>;
+      return <h1 className={`${coreClasses} ${h1Classes} ${extraClasses}`}>{children}</h1>;
     case 2:
-      return <h2 className={` ${extraClasses}`}>{children}</h2>;
+      return <h2 className={`${coreClasses} ${extraClasses}`}>{children}</h2>;
     case 3:
-      return <h3 className={` ${extraClasses}`}>{children}</h3>;
+      return <h3 className={`${coreClasses} ${extraClasses}`}>{children}</h3>;
     case 4:
-      return <h4 className={` ${extraClasses}`}>{children}</h4>;
+      return <h4 className={`${coreClasses} ${extraClasses}`}>{children}</h4>;
     case 5:
-      return <h5 className={` ${extraClasses}`}>{children}</h5>;
-    case 6:
-      return <h6 className={` ${extraClasses}`}>{children}</h6>;
+      return <h5 className={`${coreClasses} ${extraClasses}`}>{children}</h5>;
     default:
-      return <h1 className={` ${extraClasses}`}>{children}</h1>;
+      return <h1 className={`${coreClasses} ${h1Classes} ${extraClasses}`}>{children}</h1>;
   }
 };
