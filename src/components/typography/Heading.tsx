30ad3393efca24fdcf369f8092e7c56294b06f78
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2024-09-24 17:37:42 +0100

    Add H2 classes

--- src/components/typography/Heading.tsx
+++ src/components/typography/Heading.tsx
@@ -8,15 +8,15 @@ type TProps = {
 
 export const coreClasses = "text-textDark font-medium";
 export const h1Classes = "text-4xl";
-export const h2Classes = "text-2xl";
+export const h2Classes = "text-2xl mb-5";
 export const h3Classes = "text-xl";
 
-export const Heading = ({ level, extraClasses, children }: TProps) => {
+export const Heading = ({ level, extraClasses = "", children }: TProps) => {
   switch (level) {
     case 1:
       return <h1 className={`${coreClasses} ${h1Classes} ${extraClasses}`}>{children}</h1>;
     case 2:
-      return <h2 className={`${coreClasses} ${extraClasses}`}>{children}</h2>;
+      return <h2 className={`${coreClasses} ${h2Classes} ${extraClasses}`}>{children}</h2>;
     case 3:
       return <h3 className={`${coreClasses} ${extraClasses}`}>{children}</h3>;
     case 4:
