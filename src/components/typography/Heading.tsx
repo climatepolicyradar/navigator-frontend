7234279a63e711de11ca80ad9bf2ceabc59d199d
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2024-09-25 15:39:46 +0100

    Add classes up to H4

--- src/components/typography/Heading.tsx
+++ src/components/typography/Heading.tsx
@@ -10,6 +10,7 @@ export const coreClasses = "text-textDark font-medium";
 export const h1Classes = "text-4xl";
 export const h2Classes = "text-2xl mb-5";
 export const h3Classes = "text-xl";
+export const h4Classes = "text-lg";
 
 export const Heading = ({ level, extraClasses = "", children }: TProps) => {
   switch (level) {
@@ -18,11 +19,9 @@ export const Heading = ({ level, extraClasses = "", children }: TProps) => {
     case 2:
       return <h2 className={`${coreClasses} ${h2Classes} ${extraClasses}`}>{children}</h2>;
     case 3:
-      return <h3 className={`${coreClasses} ${extraClasses}`}>{children}</h3>;
+      return <h3 className={`${coreClasses} ${h3Classes} ${extraClasses}`}>{children}</h3>;
     case 4:
-      return <h4 className={`${coreClasses} ${extraClasses}`}>{children}</h4>;
-    case 5:
-      return <h5 className={`${coreClasses} ${extraClasses}`}>{children}</h5>;
+      return <h4 className={`${coreClasses} ${h4Classes} ${extraClasses}`}>{children}</h4>;
     default:
       return <h1 className={`${coreClasses} ${h1Classes} ${extraClasses}`}>{children}</h1>;
   }
