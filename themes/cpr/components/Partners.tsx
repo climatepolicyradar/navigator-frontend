e881c06b9d83e736b2c3c649a02eb24836ddd9fe
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2024-09-30 10:16:43 +0100

    Update CPR homepage components

--- themes/cpr/components/Partners.tsx
+++ themes/cpr/components/Partners.tsx
@@ -1,6 +1,7 @@
 import Image from "next/legacy/image";
 
 import { SiteWidth } from "@components/panels/SiteWidth";
+import { SingleCol } from "@components/panels/SingleCol";
 
 import { ExternalLink } from "@components/ExternalLink";
 import { Heading } from "@components/typography/Heading";
@@ -11,15 +12,18 @@ const Partners = () => {
   return (
     <div className="py-24">
       <SiteWidth>
-        <h2 className="text-indigo-500 mb-8 text-center">Our Partners</h2>
-        <Heading level={2}>Our partners</Heading>
-        <div className="grid grid-cols-1 gap-8 px-4 mb-8 md:flex md:flex-wrap md:gap-[3%] md:justify-center md:mb-0 xl:px-0">
-          {partners.map((partner) => (
-            <ExternalLink key={partner.link} className="block relative py-12 unset-img md:basis-[30%] md:mb-8" url={partner.link}>
-              <Image src={`/images/partners/${partner.logo}`} alt={partner.name} layout="fill" className="custom-img" />
-            </ExternalLink>
-          ))}
-        </div>
+        <Heading level={2} extraClasses="text-center">
+          Our partners
+        </Heading>
+        <SingleCol>
+          <div className="flex flex-col gap-5 mb-5 md:flex-row md:flex-wrap md:justify-center md:mb-0">
+            {partners.map((partner) => (
+              <ExternalLink key={partner.link} className="block relative py-12 unset-img md:basis-[30%] md:mb-5" url={partner.link}>
+                <Image src={`/images/partners/${partner.logo}`} alt={partner.name} layout="fill" className="custom-img" />
+              </ExternalLink>
+            ))}
+          </div>
+        </SingleCol>
       </SiteWidth>
     </div>
   );
--- themes/cpr/components/Summary.tsx
+++ themes/cpr/components/Summary.tsx
@@ -10,13 +10,11 @@ const Summary = () => {
       <SiteWidth extraClasses="py-24 md:flex items-center">
         <div>
           <Heading level={2}>Read our methodology</Heading>
-
           <p className="text-lg md:mr-8">
             Please refer to this page for information about the scope and structure of our database, our data collection methods and terminology,
             updates to the database and planned future developments.
           </p>
         </div>
-
         <ExternalLink
           url="https://github.com/climatepolicyradar/methodology"
           className="mt-4 md:mt-0 block w-full text-center bg-blue-500 text-white md:grow-0 p-2 rounded-xl md:w-48 transition duration-300 hover:text-white"
