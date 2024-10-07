abe0b8baf8b45c7bb7ea3022153ccca7ab887077
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2024-10-07 11:38:35 +0100

    Ensure checkbox is always a controlled component

--- src/components/forms/Checkbox.tsx
+++ src/components/forms/Checkbox.tsx
@@ -1,8 +1,14 @@
-const CheckBox = ({ checked, onChange }) => {
-  return <input type="checkbox" checked={checked} onChange={onChange} className="w-[24px] h-[24px]" />;
+type TProps = {
+  label?: string;
+  checked: boolean;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-export const InputCheck = ({ label, checked, onChange }) => {
+const CheckBox = ({ checked, onChange }: TProps) => {
+  return <input type="checkbox" checked={checked || false} onChange={onChange} className="w-[24px] h-[24px]" />;
+};
+
+export const InputCheck = ({ label, checked, onChange }: TProps) => {
   return (
     <label className={`flex gap-2 items-center cursor-pointer ${checked ? "font-medium text-textDark" : ""}`}>
       <CheckBox checked={checked} onChange={onChange} />
