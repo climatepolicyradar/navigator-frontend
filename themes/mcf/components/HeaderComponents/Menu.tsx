8a75f4ac29975ec6d1e374cc136975f1b16e8a78
Osneil Drakes <osneildrakes@Osneils-MBP.communityfibre.co.uk>
2024-10-10 10:24:52 +0100

    feat: add header component to mcf landing page

---
+++ themes/mcf/components/HeaderComponents/Menu.tsx
@@ -0,0 +1,38 @@
+import { useState, useRef, useCallback } from "react";
+
+import useOutsideAlerter from "@hooks/useOutsideAlerter";
+import { MenuIcon } from "@components/svg/Icons";
+import DropdownMenuItem from "@components/menus/DropdownMenuItem";
+import DropdownMenuWrapper from "@components/menus/DropdownMenuWrapper";
+
+import menuLinks from "../../constants/menuLinks";
+
+export const Menu = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const menuRef = useRef(null);
+  useOutsideAlerter(menuRef, () => setShowMenu(false));
+
+  const toggleMenu = useCallback((e) => {
+    e.preventDefault();
+    setShowMenu((prev) => !prev);
+  }, []);
+
+  const mainAppColour = "#082C43";
+
+  return (
+    <div ref={menuRef} className="flex items-center relative z-[41]">
+      <button data-cy="menu-icon" onClick={toggleMenu} aria-label="Toggle menu">
+        <MenuIcon color={mainAppColour} width="32" height="32" />
+      </button>
+      {showMenu && (
+        <div className="absolute right-0 top-[100%] z-50">
+          <DropdownMenuWrapper>
+            {menuLinks.map((link, index) => (
+              <DropdownMenuItem key={index} {...link} title={link.text} setShowMenu={setShowMenu} first={index === 0} cy={`navigation-${link.cy}`} />
+            ))}
+          </DropdownMenuWrapper>
+        </div>
+      )}
+    </div>
+  );
+};
---
+++ themes/mcf/constants/menuLinks.ts
@@ -0,0 +1,16 @@
+const menuLinks = [
+  {
+    text: "Home",
+    href: "/",
+    external: false,
+    cy: "home",
+  },
+  {
+    text: "Search",
+    href: "/search",
+    external: false,
+    cy: "search",
+  },
+];
+
+export default menuLinks;
