5583e802b49c82206aadc1d3014b538f7a7e6ab6
Osneil Drakes <osneildrakes@Osneils-MBP.communityfibre.co.uk>
2024-10-10 10:24:06 +0100

    feat: add climate policy banner holder component

---
+++ themes/mcf/components/ClimatePolicyRadarBannerHolder.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ClimatePolicyRadarBannerHolder = () => {
+  return (
+    <div className="bg-[#005EEB] py-36">
+      <h2 className="text-center text-white text-3xl xl:text-4xl">Climate Policy Radar Banner Holder</h2>
+    </div>
+  );
+};
+
+export default ClimatePolicyRadarBannerHolder;
