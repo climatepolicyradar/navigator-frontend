6d58430b292660e9721817e3bf1b4f597ab4ab1e
Osneil Drakes <osneildrakes@Osneils-MacBook-Pro.local>
2024-10-10 15:07:27 +0100

    refactor: update social media content component

--- themes/mcf/components/FooterComponents/SocialMediaContent.tsx
+++ themes/mcf/components/FooterComponents/SocialMediaContent.tsx
@@ -1,46 +1,8 @@
 import React from "react";
 import { ExternalLink } from "@components/ExternalLink";
-import { FacebookIcon, InstagramIcon, LinkedInIcon, TwitterIcon } from "@components/svg/Icons";
+import { multilateralClimateFundSocialMedia } from "./socialMediaData";
 
 const SocialMediaContent = () => {
-  const iconColor = "#0171B8";
-
-  const multilateralClimateFundSocialMedia = {
-    "Adaptation Fund": [
-      { url: "https://twitter.com/adaptationfund", icon: <TwitterIcon color={iconColor} /> },
-      { url: "https://www.facebook.com/adaptationfund", icon: <FacebookIcon color={iconColor} /> },
-      {
-        icon: <LinkedInIcon color={iconColor} />,
-        url: "https://www.linkedin.com/company/adaptation-fund",
-      },
-      {
-        url: "https://www.instagram.com/adaptationfund/?hl=en",
-        icon: <InstagramIcon color={iconColor} />,
-      },
-    ],
-    "Green Climate Fund": [
-      { url: "https://twitter.com/theGCF", icon: <TwitterIcon color={iconColor} /> },
-      { url: "https://facebook.com/GCFOfficial", icon: <FacebookIcon color={iconColor} /> },
-      {
-        url: "https://www.linkedin.com/company/green-climate-fund/",
-        icon: <LinkedInIcon color={iconColor} />,
-      },
-    ],
-    "Climate Investment Funds": [
-      { url: "https://twitter.com/CIF_Action", icon: <TwitterIcon color={iconColor} /> },
-      { url: "https://www.facebook.com/CIFaction/", icon: <FacebookIcon color={iconColor} /> },
-      { url: "https://www.linkedin.com/company/cifaction", icon: <LinkedInIcon color={iconColor} /> },
-    ],
-    "Global Environment Facility": [
-      { url: "https://twitter.com/theGEF", icon: <TwitterIcon color={iconColor} /> },
-      { url: "https://www.facebook.com/TheGEF1/", icon: <FacebookIcon color={iconColor} /> },
-      {
-        url: "https://www.linkedin.com/company/global-environment-facility",
-        icon: <LinkedInIcon color={iconColor} />,
-      },
-    ],
-  };
-
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
       {Object.entries(multilateralClimateFundSocialMedia).map(([fundType, fundSocialMedia]) => (
--- themes/mcf/constants/colors.ts
+++ themes/mcf/constants/colors.ts
@@ -1,3 +1,5 @@
 export const mainAppColour = "#082C43";
 
 export const climatePolicyBannerColour = "#005EEB";
+
+export const iconColor = "#0171B8";
