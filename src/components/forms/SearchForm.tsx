0cd93105fbc623dc528a03a8f2092558cd0909f0
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2024-10-10 18:04:16 +0100

    Update search form with new input component

--- src/components/forms/SearchForm.tsx
+++ src/components/forms/SearchForm.tsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect, useRef } from "react";
-import Close from "../buttons/Close";
-import SearchButton from "../buttons/SearchButton";
-import useWindowResize from "@hooks/useWindowResize";
+
 import { SearchDropdown } from "./SearchDropdown";
+import { TextInput } from "./TextInput";
+import { Search2Icon } from "@components/svg/Icons";
 
 interface SearchFormProps {
   input?: string;
@@ -15,15 +15,9 @@ const SearchForm = ({ input, placeholder, handleSearchInput, handleSuggestion }:
   const [term, setTerm] = useState("");
   const [formFocus, setFormFocus] = useState(false);
   const formRef = useRef(null);
-  const windowSize = useWindowResize();
 
-  const clearSearch = () => {
-    setTerm("");
-    handleSearchInput("");
-  };
-
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setTerm(e.currentTarget.value);
+  const onChange = (value: string) => {
+    setTerm(value);
   };
 
   const handleSuggestionClick = (term: string, filter?: string, filterValue?: string) => {
@@ -64,24 +58,19 @@ const SearchForm = ({ input, placeholder, handleSearchInput, handleSuggestion }:
   return (
     <form data-cy="search-form" ref={formRef} onSubmit={onFormSubmit}>
       <div className="relative z-40">
-        <div className="shadow-md rounded-full bg-white relative z-20">
-          <div className="absolute p-[1px] pr-0 top-0 left-0 h-full flex items-center justify-start z-20">
-            <SearchButton onClick={handleSearchButtonClick} />
-          </div>
-          <input
-            data-analytics="seachPage-searchInput"
-            data-cy="search-input"
-            className="w-full bg-white appearance-none py-2 px-10 z-10 leading-snug rounded-full relative flex-grow border-gray-300 placeholder:text-grey-300 md:pl-12"
-            type="search"
-            placeholder={`${windowSize.width > 767 ? placeholder : ""}`}
-            value={term}
+        <div className="relative z-20">
+          <TextInput
             onChange={onChange}
-          />
-          {term.length > 0 && (
-            <div data-cy="search-clear-button" className="flex items-center shrink-0 px-2 absolute top-0 right-2 h-full z-20">
-              <Close onClick={clearSearch} size="10" />
-            </div>
-          )}
+            data-cy="search-input"
+            data-analytics="seachPage-searchInput"
+            placeholder={placeholder}
+            type="search"
+            size="large"
+          >
+            <button type="submit" className="flex cursor-pointer text-textDark" onClick={() => handleSearchButtonClick()} title="Click to search">
+              <Search2Icon width="20" height="20" />
+            </button>
+          </TextInput>
         </div>
         {handleSuggestion && <SearchDropdown term={term} show={formFocus} handleSearchClick={handleSuggestionClick} />}
       </div>
