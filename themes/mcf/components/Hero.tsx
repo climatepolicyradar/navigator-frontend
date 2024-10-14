5041d710dbdfcfffa45df0aa5a344219958c4e13
Osneil Drakes <osneildrakes@Osneils-MBP.communityfibre.co.uk>
2024-10-14 09:23:55 +0100

    fix: update styling on hero paragraph

--- themes/mcf/components/Hero.tsx
+++ themes/mcf/components/Hero.tsx
@@ -16,7 +16,7 @@ const Hero = ({ handleSearchInput, searchInput }: TProps) => (
   <div className="pb-6 text-white pt-[28px] sm:pt-[48px] md:pt-[80px] lg:pt-[100px] xl:pt-[140px]">
     <SiteWidth>
       <div className="mx-auto text-center">
-        <p className="font-medium tracking-slight text-lg lg:text-3xl text-blue-500" data-cy="intro-message">
+        <p className="font-medium tracking-slight text-lg lg:text-xl text-subheadingText" data-cy="search-all">
           SEARCH ALL
         </p>
         <Heading extraClasses="custom-hero" level={1}>
@@ -27,7 +27,6 @@ const Hero = ({ handleSearchInput, searchInput }: TProps) => (
       <div className="max-w-screen-md mx-auto mt-6">
         <LandingSearchForm handleSearchInput={handleSearchInput} placeholder="Search the full text of any document" input={searchInput} />
       </div>
-      <VerticalSpacing size="xxl" />
       <MultilateralClimateFundLogos />
       <div className="mt-6 mt-[48px] sm:mt-[88px] md:mt-[188px] lg:mt-[208px] xl:mt-[248px]">
         <Instructions />
