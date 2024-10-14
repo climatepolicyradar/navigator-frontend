8332ea851bc0a9c77c10e9b403878faeb191c39f
Osneil Drakes <osneildrakes@Osneils-MBP.communityfibre.co.uk>
2024-10-14 16:56:52 +0100

    refactor: update the mcf logos so that they are in alphabetic order

--- themes/mcf/components/HeroComponents/MultilateralClimateFundsLogos.tsx
+++ themes/mcf/components/HeroComponents/MultilateralClimateFundsLogos.tsx
@@ -16,14 +16,14 @@ export const MultilateralClimateFundLogos = () => {
       imgSrc: "/images/mcf/AF.png",
     },
     {
-      name: "Green Climate Fund",
-      url: "https://www.greenclimate.fund/",
-      imgSrc: "/images/mcf/GCF.png",
-    },
-    {
       name: "Climate Investment Funds",
       url: "https://www.cif.org/",
       imgSrc: "/images/mcf/CIF.png",
+    },
+    {
+      name: "Green Climate Fund",
+      url: "https://www.greenclimate.fund/",
+      imgSrc: "/images/mcf/GCF.png",
     },
     {
       name: "Global Environment Facility",
@@ -35,9 +35,9 @@ export const MultilateralClimateFundLogos = () => {
   return (
     <div className="hidden sm:flex justify-center items-center gap-8 mt-8 mb-4">
       {fundInformation.map((fund) => (
-        <div key={fund.name} className="w-[100px] h-[42px]">
+        <div key={fund.name} className="w-[120px] h-[60px]">
           <ExternalLink url={fund.url} className="block w-full h-full">
-            <Image src={fund.imgSrc} alt={`${fund.name} logo`} width={100} height={42} className="object-contain w-full h-full" />
+            <Image src={fund.imgSrc} alt={`${fund.name} logo`} width={120} height={60} className="object-contain w-full h-full" />
           </ExternalLink>
         </div>
       ))}
