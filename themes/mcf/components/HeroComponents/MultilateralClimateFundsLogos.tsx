bae3476e49244098161e79a95fa8e7fe9189611a
Osneil Drakes <osneildrakes@Osneils-MBP.communityfibre.co.uk>
2024-10-15 09:46:56 +0100

    refactor: define collection data outside of component, so it doesnt redefine on every render

--- themes/mcf/components/HeroComponents/MultilateralClimateFundsLogos.tsx
+++ themes/mcf/components/HeroComponents/MultilateralClimateFundsLogos.tsx
@@ -8,30 +8,30 @@ interface FundData {
   imgSrc: string;
 }
 
-export const MultilateralClimateFundLogos = () => {
-  const fundInformation: FundData[] = [
-    {
-      name: "Adaptation Fund",
-      url: "https://www.adaptation-fund.org/",
-      imgSrc: "/images/mcf/AF.png",
-    },
-    {
-      name: "Climate Investment Funds",
-      url: "https://www.cif.org/",
-      imgSrc: "/images/mcf/CIF.png",
-    },
-    {
-      name: "Green Climate Fund",
-      url: "https://www.greenclimate.fund/",
-      imgSrc: "/images/mcf/GCF.png",
-    },
-    {
-      name: "Global Environment Facility",
-      url: "https://www.thegef.org/",
-      imgSrc: "/images/mcf/GEF.png",
-    },
-  ];
+const fundInformation: FundData[] = [
+  {
+    name: "Adaptation Fund",
+    url: "https://www.adaptation-fund.org/",
+    imgSrc: "/images/mcf/AF.png",
+  },
+  {
+    name: "Climate Investment Funds",
+    url: "https://www.cif.org/",
+    imgSrc: "/images/mcf/CIF.png",
+  },
+  {
+    name: "Green Climate Fund",
+    url: "https://www.greenclimate.fund/",
+    imgSrc: "/images/mcf/GCF.png",
+  },
+  {
+    name: "Global Environment Facility",
+    url: "https://www.thegef.org/",
+    imgSrc: "/images/mcf/GEF.png",
+  },
+];
 
+export const MultilateralClimateFundLogos = () => {
   return (
     <div className="hidden sm:flex justify-center items-center gap-8 mt-8 mb-4">
       {fundInformation.map((fund) => (
