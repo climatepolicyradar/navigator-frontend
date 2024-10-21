c17bb2919ec781c1fd09d854812b411715e76062
Osneil Drakes <osneildrakes@Osneils-MBP.communityfibre.co.uk>
2024-10-21 10:43:16 +0100

    style: formatting

--- themes/mcf/components/HeroComponents/MultilateralClimateFundsLogos.tsx
+++ themes/mcf/components/HeroComponents/MultilateralClimateFundsLogos.tsx
@@ -5,44 +5,39 @@ import { ExternalLink } from "@components/ExternalLink";
 import { AdaptationFundLogo, GlobalEnvironmentFacilityLogo, GreenClimateFundLogo, ClimateInvestmentFundsLogo } from "./logos/logos";
 
 interface FundData {
+  logo: JSX.Element;
   name: string;
   url: string;
-  imgSrc: string;
-  logo: JSX.Element;
 }
 
 const fundInformation: FundData[] = [
   {
+    logo: <AdaptationFundLogo />,
     name: "Adaptation Fund",
     url: "https://www.adaptation-fund.org/",
-    imgSrc: "/images/mcf/AF.png",
-    logo: <AdaptationFundLogo />,
   },
   {
+    logo: <ClimateInvestmentFundsLogo />,
     name: "Climate Investment Funds",
     url: "https://www.cif.org/",
-    imgSrc: "/images/mcf/CIF.png",
-    logo: <ClimateInvestmentFundsLogo />,
   },
   {
+    logo: <GreenClimateFundLogo />,
     name: "Green Climate Fund",
     url: "https://www.greenclimate.fund/",
-    imgSrc: "/images/mcf/GCF.png",
-    logo: <GreenClimateFundLogo />,
   },
   {
+    logo: <GlobalEnvironmentFacilityLogo />,
     name: "Global Environment Facility",
     url: "https://www.thegef.org/",
-    imgSrc: "/images/mcf/GEF.png",
-    logo: <GlobalEnvironmentFacilityLogo />,
   },
 ];
 
 export const MultilateralClimateFundLogos = () => {
   return (
     <div className="hidden sm:flex justify-center items-center gap-8 mt-8 mb-4">
-      {fundInformation.map((fund, index) => (
-        <div key={index} className="w-[120px] h-[60px]">
+      {fundInformation.map((fund) => (
+        <div key={fund.name} className="w-[120px] h-[60px]">
           <ExternalLink url={fund.url} className="block w-full h-full">
             {fund.logo}
           </ExternalLink>
