ee88723e0a5c7b71b99463276981209292e383a5
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2024-10-23 09:38:41 +0100

    Tidy up filters api

--- src/pages/api/filters.ts
+++ src/pages/api/filters.ts
@@ -4,16 +4,9 @@ import path from "path";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
-    // Construct the file path
     const dataFilePath = path.join(process.cwd(), `/themes/${process.env.THEME}/config.json`);
-
-    // Read the JSON file
     const fileContents = await fs.readFile(dataFilePath, "utf8");
-
-    // Parse the JSON data
     const data = JSON.parse(fileContents);
-
-    // Send the data as the API response
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json({ error: "Error reading data", message: error });
