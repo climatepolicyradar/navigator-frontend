c46fcb4b563c5af0e70ffd8fab5142b0e7427d90
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2024-10-23 09:37:46 +0100

    Add missing config to download csv call

--- src/hooks/useDownloadCsv.ts
+++ src/hooks/useDownloadCsv.ts
@@ -1,4 +1,4 @@
-import { ApiClient, getEnvFromServer } from "../api/http-common";
+import { ApiClient, getEnvFromServer, getFilters } from "../api/http-common";
 import buildSearchQuery, { TRouterQuery } from "@utils/buildSearchQuery";
 import { TLoadingStatus, TSearch } from "@types";
 import { useState } from "react";
@@ -34,9 +34,10 @@ async function getDownloadCsv(query: TRouterQuery) {
   };
 
   const { data } = await getEnvFromServer();
+  const { themeConfig } = await getFilters();
   const client = new ApiClient(data?.env?.api_url, data?.env?.app_token);
 
-  const searchQuery = buildSearchQuery(query, "", "", true, 0);
+  const searchQuery = buildSearchQuery(query, themeConfig, "", "", true, 0);
   // Manually set this to 500, overriding the default 10 which is used for pagination
   searchQuery.page_size = 500;
   searchQuery.limit = 500;
