3c9d522783b5d81bcf0b9fb3f76fc8dc5b855d42
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-10-23 15:02:40 +0100

    Fix bug on clickable XAB geo breadcrumb

--- src/components/breadcrumbs/Breadcrumbs.tsx
+++ src/components/breadcrumbs/Breadcrumbs.tsx
@@ -1,4 +1,6 @@
 import { LinkWithQuery } from "@components/LinkWithQuery";
+import { isSystemGeo } from "@utils/isSystemGeo";
+
 const BREADCRUMB_MAXLENGTH = 50;
 
 type TBreadcrumbLink = {
@@ -40,7 +42,7 @@ export const BreadCrumbs = ({ geography = null, category = null, family = null, 
   return (
     <ul className="flex items-baseline flex-wrap gap-2 text-sm" data-cy="breadcrumbs">
       <BreadCrumb label="Home" href="/" cy="home" />
-      {geography && <BreadCrumb label={geography.label} href={geography.label === "No Geography" ? null : geography.href} cy="geography" />}
+      {geography && <BreadCrumb label={geography.label} href={isSystemGeo(geography.label) ? null : geography.href} cy="geography" />}
       {category && <BreadCrumb label={category.label} href={category.href} cy="category" />}
       {family && <BreadCrumb label={family.label} href={family.href} cy="family" />}
       <BreadCrumb label={label} last cy="current" />
