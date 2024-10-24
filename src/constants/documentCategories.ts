804152eda200289f36fc741039859ce8a7952d21
Osneil Drakes <osneildrakes@Osneils-MacBook-Pro.local>
2024-10-24 18:58:42 +0100

    feat: render mcf only docs on mcf geography pages
    - As we want to keep the functionality and look of the geography at this
      stage, we are just updating the way we present the data
    - filter the data in the server side prop only return MCF related data
    - define variable to hold if the app is currently mcf, if so update the
      document categories that we pass to the nav index, as well as teh docs
      that we are rendering in the renderdocs file.
      For the latter we we only parse the current category if there are more
      than one categories/families, in which case is a non mcf app

--- src/constants/documentCategories.ts
+++ src/constants/documentCategories.ts
@@ -1,3 +1,6 @@
 export const DOCUMENT_CATEGORIES = ["All", "Legislation", "Policies", "UNFCCC", "Litigation"];
 
+// For now all individual fund documents are returning as MCF, so we do not need separate categories
+export const MCF_DOCUMENT_CATEGORIES = ["All"];
+
 export type TDocumentCategory = (typeof DOCUMENT_CATEGORIES)[number];
