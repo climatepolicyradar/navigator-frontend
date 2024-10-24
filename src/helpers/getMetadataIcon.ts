f25f64f838ae5233d6adc896822086ae3ed16962
Osneil Drakes <osneildrakes@Osneils-MBP.communityfibre.co.uk>
2024-10-24 11:58:16 +0100

    style: remove lucide package, we already have react icons

--- src/helpers/getMetadataIcon.ts
+++ src/helpers/getMetadataIcon.ts
@@ -1,4 +1,17 @@
-import { Globe, Building2, FileText, DollarSign, Building, Calendar, Scroll, Hourglass, Link, Layers } from "lucide-react";
+// import { Globe, Building2, FileText, DollarSign, Building, Calendar, Scroll, Hourglass, Link, Layers } from "lucide-react";
+
+import {
+  LuBuilding as Building,
+  LuGlobe as Globe,
+  LuBuilding2 as Building2,
+  LuFileText as FileText,
+  LuDollarSign as DollarSign,
+  LuCalendar as Calendar,
+  LuScroll as Scroll,
+  LuHourglass as Hourglass,
+  LuLink as Link,
+  LuLayers as Layers,
+} from "react-icons/lu";
 
 const metadataIcons = {
   Calendar,
