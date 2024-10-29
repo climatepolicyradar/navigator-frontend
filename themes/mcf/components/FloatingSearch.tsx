cf1ab8b8a6cb0d0ebd6184442d1628c6ecef0ec3
Osneil Drakes <osneildrakes@Osneils-MBP.communityfibre.co.uk>
2024-10-29 08:42:20 +0000

    fix: remove unused code

--- themes/mcf/components/FloatingSearch.tsx
+++ themes/mcf/components/FloatingSearch.tsx
@@ -1,32 +1,24 @@
 import { useEffect, useRef, useState } from "react";
 import { useRouter } from "next/router";
-// import { CloseIcon, LightblubIcon, SearchIcon } from "./svg/Icons";
-import { CloseIcon, LightblubIcon, SearchIcon } from "@components/svg/Icons";
-// import { Divider } from "./dividers/Divider";
+import { SearchIcon } from "@components/svg/Icons";
 import { Divider } from "@components/dividers/Divider";
 import { CleanRouterQuery } from "@utils/cleanRouterQuery";
 import { QUERY_PARAMS } from "@constants/queryParams";
-import { getSessionStorage, setSessionStorage } from "@utils/sessionStorage";
 
-const SEARCH_HIGHLIGHT_DELAY = 5000;
 const FEATURED_SEARCHES = ["Adaptation strategy", "Energy prices", "Flood defence", "Fossil fuels"];
-const FEATURED_DOCUMENT_SLUG = "/documents/adaptation-strategy-to-climate-change-in-the-czech-republic_213b";
 
 const FloatingSearch = () => {
   const ref = useRef(null);
   const router = useRouter();
-  const qQueryString = router.query[QUERY_PARAMS.query_string] as string;
+
   const [search, setSearch] = useState("");
-  const [hasOpenedSearch, setHasOpenedSearch] = useState(false);
+
   const [showFloatingSearch, setShowFloatingSearch] = useState(false);
-  const [showSearchHighlight, setShowSearchHighlight] = useState(false);
 
   const inputFocusStyles = "rounded-bl-none rounded-br-none";
 
   const handleInputFocus = () => {
-    setHasOpenedSearch(true);
     setShowFloatingSearch(true);
-    setShowSearchHighlight(false);
   };
 
   // We specifically do not want to erase any filtering
@@ -35,29 +27,6 @@ const FloatingSearch = () => {
     queryObj[QUERY_PARAMS.query_string] = term;
     router.push({ pathname: "/search", query: queryObj });
   };
-
-  const handleTryThisClick = (e: any) => {
-    e.preventDefault();
-    const queryObj = {
-      [QUERY_PARAMS.query_string]: "Flood defence",
-      [QUERY_PARAMS.country]: "czechia",
-    };
-    router.push({ pathname: FEATURED_DOCUMENT_SLUG, query: queryObj });
-    setShowFloatingSearch(false);
-  };
-
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      if (getSessionStorage("showSearchHighlight") || hasOpenedSearch) return;
-      setShowSearchHighlight(true);
-      setSessionStorage("showSearchHighlight", true);
-    }, SEARCH_HIGHLIGHT_DELAY);
-    return () => clearTimeout(timeout);
-  }, [hasOpenedSearch]);
-
-  useEffect(() => {
-    setSearch(qQueryString || "");
-  }, [qQueryString]);
 
   // Clicking outside the floating search will close it
   useEffect(() => {
@@ -83,7 +52,7 @@ const FloatingSearch = () => {
         <input
           type="search"
           value={search}
-          placeholder="Full text of over 5,000 climate laws and policies"
+          placeholder="Full text of over 5,000 multilateral climate fund projects"
           onChange={(e) => setSearch(e.target.value)}
           onFocus={() => handleInputFocus()}
           className={`pl-[34px] rounded-[20px] text-sm w-full bg-white text-gray-800 border-1 focus:ring-0 focus:ring-offset-0 focus:ring-white ${
@@ -113,26 +82,6 @@ const FloatingSearch = () => {
           </div>
         </div>
       )}
-      <div
-        data-cy="search-highlight"
-        className={`absolute left-[40px] top-full mt-3 p-4 bg-blue-800 w-[320px] text-white rounded-lg transition opacity-0 ${
-          showSearchHighlight ? "visible !opacity-100" : "invisible"
-        }`}
-      >
-        <div className="tooltip-up-arrow" />
-        <button onClick={() => setShowSearchHighlight(false)} className="absolute right-0 top-0 p-4">
-          <CloseIcon width="12" height="12" />
-        </button>
-        <p>Find relevant information</p>
-        <ul className="text-sm list-disc pl-5 mt-2">
-          <li>
-            Documents from <b>all languages</b> translated into English
-          </li>
-          <li>
-            Exact matches and related phrases <b>highlighted</b> in the text
-          </li>
-        </ul>
-      </div>
     </div>
   );
 };
