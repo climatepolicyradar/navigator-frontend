3d3994e4facc5bd155aaab659523bcd9a4705355
Osneil Drakes <osneildrakes@Osneils-MBP.communityfibre.co.uk>
2024-11-04 14:52:11 +0000

    feat: geography page only deplays projects in country stats

--- src/components/blocks/CountryHeader.tsx
+++ src/components/blocks/CountryHeader.tsx
@@ -4,15 +4,17 @@ import Tooltip from "@components/tooltip";
 import { ExternalLink } from "@components/ExternalLink";
 import { Heading } from "@components/typography/Heading";
 
-import { TGeographyStats, TGeography } from "@types";
+import { TGeographyStats, TGeography, TTheme } from "@types";
 
 type TProps = {
   country: TGeographyStats;
   targetCount: number;
   onTargetClick: () => void;
+  theme: TTheme;
+  totalProjects: number;
 };
 
-export const CountryHeader = ({ country, targetCount, onTargetClick }: TProps) => {
+export const CountryHeader = ({ country, targetCount, onTargetClick, theme, totalProjects }: TProps) => {
   const configQuery = useConfig();
   const { data: { regions = [], countries = [] } = {} } = configQuery;
 
@@ -27,6 +29,112 @@ export const CountryHeader = ({ country, targetCount, onTargetClick }: TProps) =
   const countryRegion = getCountryRegion();
   const { name, political_groups, federal, federal_details, worldbank_income_group, climate_risk_index, global_emissions_percent } = country;
 
+  const renderHeaderContent = () => {
+    if (theme === "mcf") {
+      return (
+        <div data-cy="total-projects" className="col-span-1 md:col-span-2">
+          <div className="text-sm font-medium text-textDark">Total Projects</div>
+          <div>{totalProjects}</div>
+        </div>
+      );
+    } else {
+      return (
+        <>
+          {federal && (
+            <div className="col-span-2 md:col-span-5">
+              <div className="font-medium text-textDark">Federative {federal_details && <span className="text-sm">({federal_details})</span>}</div>
+            </div>
+          )}
+          <div data-cy="political-group" className="col-span-1 md:col-span-2">
+            {political_groups !== "" && (
+              <>
+                <div className="text-sm font-medium text-textDark">Political Groups</div>
+                <div>{political_groups.split(";").join(", ")}</div>
+              </>
+            )}
+          </div>
+          <div data-cy="global-climate-risk-index" className="col-span-1 md:col-span-2">
+            {climate_risk_index !== null && (
+              <>
+                <div className="text-sm font-medium text-textDark">Global Climate Risk Index</div>
+                <div className="flex items-center">
+                  <div className=" mr-1">{climate_risk_index}</div>{" "}
+                  <Tooltip
+                    id="country-gcri"
+                    tooltip={
+                      <>
+                        <p className="mb-4">
+                          The annually published Global Climate Risk Index analyses to what extent countries have been affected by the impacts of
+                          weather-related loss events (storms, floods, heat waves etc.).
+                        </p>
+                        <p className="mb-4">
+                          This data is from the Global Risk Index 2021 published by{" "}
+                          <ExternalLink url="https://www.germanwatch.org/en/cri">German Watch</ExternalLink>. Numbers marked with an asterisk (*) are
+                          from the Global Risk Index 2020, being the latest available data for that country. This data was last updated on this site
+                          on 18 September 2023.
+                        </p>
+                        See the full report published by German Watch <ExternalLink url="https://www.germanwatch.org/en/19777">here</ExternalLink>.
+                      </>
+                    }
+                    icon="i"
+                    interactableContent
+                  />
+                </div>
+              </>
+            )}
+          </div>
+          <div data-cy="targets" className="order-last col-span-2 md:col-span-1 md:order-none">
+            {targetCount > 0 && (
+              <>
+                <div className="text-sm font-medium text-textDark">Targets</div>
+                <div className="flex items-center">
+                  <button onClick={onTargetClick} className="mr-1 underline hover:text-blue-800">
+                    {targetCount}
+                  </button>
+                </div>
+              </>
+            )}
+          </div>
+          <div data-cy="world-bank-income-group" className="col-span-1 md:col-span-2">
+            {worldbank_income_group !== "" && (
+              <>
+                <div className="text-sm font-medium text-textDark">World Bank Income Group</div>
+                <div>{worldbank_income_group}</div>
+              </>
+            )}
+          </div>
+          <div data-cy="share-of-global-emissions" className="col-span-1 md:col-span-2">
+            {global_emissions_percent !== null && (
+              <>
+                <div className="text-sm font-medium text-textDark">Share of Global Emissions</div>
+                <div className="flex items-center">
+                  <div className=" mr-1">{global_emissions_percent}%</div>{" "}
+                  <Tooltip
+                    id="country-gep"
+                    tooltip={
+                      <>
+                        <p className="mb-4">
+                          The share of global emissions data is from{" "}
+                          <ExternalLink url="https://www.climatewatchdata.org/">Climate Watch</ExternalLink>, managed by the World Resources
+                          Institute.
+                        </p>
+                        <p className="mb-4">
+                          This percentage is based on emissions data from 2020. This data was last updated on this site on 18 September 2023.
+                        </p>
+                      </>
+                    }
+                    icon="i"
+                    interactableContent
+                  />
+                </div>
+              </>
+            )}
+          </div>
+        </>
+      );
+    }
+  };
+
   return (
     <div>
       <Heading level={1}>{name}</Heading>
@@ -38,96 +146,7 @@ export const CountryHeader = ({ country, targetCount, onTargetClick }: TProps) =
                 {countryRegion}
               </div>
             </div>
-            {federal && (
-              <div className="col-span-2 md:col-span-5">
-                <div className="font-medium text-textDark">Federative {federal_details && <span className="text-sm">({federal_details})</span>}</div>
-              </div>
-            )}
-            <div data-cy="political-group" className="col-span-1 md:col-span-2">
-              {political_groups !== "" && (
-                <>
-                  <div className="text-sm font-medium text-textDark">Political Groups</div>
-                  <div>{political_groups.split(";").join(", ")}</div>
-                </>
-              )}
-            </div>
-            <div data-cy="global-climate-risk-index" className="col-span-1 md:col-span-2">
-              {climate_risk_index !== null && (
-                <>
-                  <div className="text-sm font-medium text-textDark">Global Climate Risk Index</div>
-                  <div className="flex items-center">
-                    <div className=" mr-1">{climate_risk_index}</div>{" "}
-                    <Tooltip
-                      id="country-gcri"
-                      tooltip={
-                        <>
-                          <p className="mb-4">
-                            The annually published Global Climate Risk Index analyses to what extent countries have been affected by the impacts of
-                            weather-related loss events (storms, floods, heat waves etc.).
-                          </p>
-                          <p className="mb-4">
-                            This data is from the Global Risk Index 2021 published by{" "}
-                            <ExternalLink url="https://www.germanwatch.org/en/cri">German Watch</ExternalLink>. Numbers marked with an asterisk (*)
-                            are from the Global Risk Index 2020, being the latest available data for that country. This data was last updated on this
-                            site on 18 September 2023.
-                          </p>
-                          See the full report published by German Watch <ExternalLink url="https://www.germanwatch.org/en/19777">here</ExternalLink>.
-                        </>
-                      }
-                      icon="i"
-                      interactableContent
-                    />
-                  </div>
-                </>
-              )}
-            </div>
-            <div data-cy="targets" className="order-last col-span-2 md:col-span-1 md:order-none">
-              {targetCount > 0 && (
-                <>
-                  <div className="text-sm font-medium text-textDark">Targets</div>
-                  <div className="flex items-center">
-                    <button onClick={onTargetClick} className="mr-1 underline hover:text-blue-800">
-                      {targetCount}
-                    </button>
-                  </div>
-                </>
-              )}
-            </div>
-            <div data-cy="world-bank-income-group" className="col-span-1 md:col-span-2">
-              {worldbank_income_group !== "" && (
-                <>
-                  <div className="text-sm font-medium text-textDark">World Bank Income Group</div>
-                  <div>{worldbank_income_group}</div>
-                </>
-              )}
-            </div>
-            <div data-cy="share-of-global-emissions" className="col-span-1 md:col-span-2">
-              {global_emissions_percent !== null && (
-                <>
-                  <div className="text-sm font-medium text-textDark">Share of Global Emissions</div>
-                  <div className="flex items-center">
-                    <div className=" mr-1">{global_emissions_percent}%</div>{" "}
-                    <Tooltip
-                      id="country-gep"
-                      tooltip={
-                        <>
-                          <p className="mb-4">
-                            The share of global emissions data is from{" "}
-                            <ExternalLink url="https://www.climatewatchdata.org/">Climate Watch</ExternalLink>, managed by the World Resources
-                            Institute.
-                          </p>
-                          <p className="mb-4">
-                            This percentage is based on emissions data from 2020. This data was last updated on this site on 18 September 2023.
-                          </p>
-                        </>
-                      }
-                      icon="i"
-                      interactableContent
-                    />
-                  </div>
-                </>
-              )}
-            </div>
+            {renderHeaderContent()}
           </div>
         </div>
         {countryGeography?.value && (
