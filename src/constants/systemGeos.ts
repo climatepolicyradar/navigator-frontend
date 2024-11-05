a71be8241bbcf943e3346a2691659954ed81ffd8
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2024-11-05 10:52:49 +0000

    Do not show "no geography" in the meta detail list of geographies

--- src/constants/systemGeos.ts
+++ src/constants/systemGeos.ts
@@ -1,6 +1,9 @@
+export const SYSTEM_INTERNATIONAL_CODE = "xab";
+export const SYSTEM_INTERNATIONAL_NAME = "international";
+
 export const systemGeoCodes = [
   "xaa", // No Geography
-  "xab", // International
+  SYSTEM_INTERNATIONAL_CODE,
 ];
 
-export const systemGeoNames = ["no-geography", "international"];
+export const systemGeoNames = ["no-geography", SYSTEM_INTERNATIONAL_NAME];
