1919a038e29822c49af07253e422ba385c62549b
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2024-11-12 16:26:34 +0000

    Refactor file reader for filters/config into separate file

--- src/pages/api/filters.ts
+++ src/pages/api/filters.ts
@@ -1,12 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { promises as fs } from "fs";
-import path from "path";
+
+import { readConfigFile } from "@utils/readConfigFile";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
-    const dataFilePath = path.join(process.cwd(), `/themes/${process.env.THEME}/config.json`);
-    const fileContents = await fs.readFile(dataFilePath, "utf8");
-    const data = JSON.parse(fileContents);
+    const data = await readConfigFile(process.env.THEME);
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json({ error: "Error reading data", message: error });
