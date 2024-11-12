fb04e2d14dfe5a7a562082541402a1302c7ebb04
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2024-11-12 14:30:45 +0000

    Add MCF domain into the canonical url logic

--- src/utils/getThemeDomain.ts
+++ src/utils/getThemeDomain.ts
@@ -2,12 +2,16 @@ import { TTheme } from "@types";
 
 const CPR_DOMAIN = "app.climatepolicyradar.org";
 const CCLW_DOMAIN = "climate-laws.org";
+const MCF_DOMAIN = "climateprojectexplorer.org";
 
 export default function getThemeDomain(site: TTheme): string {
   let domain = CPR_DOMAIN;
   switch (site) {
     case "cclw":
       domain = CCLW_DOMAIN;
+      break;
+    case "mcf":
+      domain = MCF_DOMAIN;
       break;
   }
   return domain;
