61696e37a2d42c3b46c1e78c6b6714b8301d4c1f
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2024-12-04 11:21:53 +0000

    Add MCF tooltip

--- src/helpers/getCategoryTooltip.ts
+++ src/helpers/getCategoryTooltip.ts
@@ -4,7 +4,7 @@ export const getCategoryTooltip = (category: TDocumentCategory): string => {
   switch (category) {
     case "All":
       return "";
-    case "Legislation":
+    case "Laws":
       return "For example: Laws, Acts, Constitutions (legislative branch)";
     case "Policies":
       return "For example: Policies, strategies, decrees, action plans (from executive branch)";
@@ -12,6 +12,8 @@ export const getCategoryTooltip = (category: TDocumentCategory): string => {
       return "For example: Court cases and tribunal proceedings";
     case "UNFCCC":
       return "Documents submitted to the UNFCCC (including NDCs)";
+    case "MCF":
+      return "Multilateral climate fund projects and policies";
     default:
       return "";
   }
