e4c926744a7207ef54c2b7867cad5de7845ddc84
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2024-12-10 10:42:34 +0000

    Remove unused query prop on LinkWithQuery

--- src/components/LinkWithQuery.tsx
+++ src/components/LinkWithQuery.tsx
@@ -1,12 +1,11 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { ParsedUrlQuery } from "querystring";
+
 import { CleanRouterQuery } from "@utils/cleanRouterQuery";
 
 type TProps = {
   href: string;
   hash?: string;
-  query?: ParsedUrlQuery;
   children: React.ReactNode;
   className?: string;
   passHref?: boolean;
@@ -14,7 +13,7 @@ type TProps = {
   cypress?: string;
 };
 
-export const LinkWithQuery = ({ href, hash, query, children, cypress, ...props }: TProps) => {
+export const LinkWithQuery = ({ href, hash, children, cypress, ...props }: TProps) => {
   const router = useRouter();
   const queryObj = CleanRouterQuery({ ...router.query });
 
