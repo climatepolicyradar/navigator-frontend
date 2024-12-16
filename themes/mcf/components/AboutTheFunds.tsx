e3fea92c0150e6b404ed61ccc7f0ef378c10dd83
Osneil Drakes <osneildrakes@Osneils-MacBook-Pro.local>
2024-12-16 15:41:25 +0000

    fix: update content relating to MCF from their edits

--- themes/mcf/components/AboutTheFunds.tsx
+++ themes/mcf/components/AboutTheFunds.tsx
@@ -11,19 +11,20 @@ type FundDescription = {
 
 const fundDescriptions: FundDescription[] = [
   {
-    name: "Adaptation Fund",
+    name: "Adaptation Fund ",
     description: "(AF) finances projects and programmes that help vulnerable communities in developing countries adapt to climate change.",
   },
   {
-    name: "Climate Investment Funds",
+    name: "Climate Investment Funds ",
     description: "(CIF) is a major multilateral climate fund established in 2008 to finance climate-smart solutions in developing countries.",
   },
   {
-    name: "Global Environment Facility",
-    description: "(GEF) projects and programmes that help vulnerable communities in developing countries adapt to climate change.",
+    name: "The Global Environment Facility ",
+    description:
+      "(GEF) is a multilateral family of funds dedicated to confronting biodiversity loss, climate change, and pollution, and supporting land and ocean health.",
   },
   {
-    name: "Green Climate Fund",
+    name: "Green Climate Fund ",
     description:
       "(GCF) is the world’s largest dedicated climate fund with a mandate to foster a paradigm shift towards low emission, climate-resilient development pathways in developing countries.",
   },
@@ -35,7 +36,7 @@ const AboutTheFundsContent = () => {
       <div className="flex justify-between gap-20">
         <div className="content-center xl:max-w-screen-md py-16">
           <Heading level={2} extraClasses="custom-header">
-            The Funds
+            Funds
           </Heading>
           {fundDescriptions.map((fund) => (
             <p key={fund.name}>
