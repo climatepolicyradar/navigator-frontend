48be1de5dd2a952da57e8c6fc96a82de8945d1d9
Anna Pokorska <anna@climatepolicyradar.org>
2025-01-13 15:08:21 +0000

    Replace jest with vitest

--- themes/mcf/pages/homepage.test.tsx
+++ themes/mcf/pages/homepage.test.tsx
@@ -1,19 +1,18 @@
 import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom";
 
-import LandingPage from "../../pages/homepage";
+import LandingPage from "./homepage";
 
-const mockHandleSearchInput = jest.fn();
+const mockHandleSearchInput = vi.fn();
 const mockSearchInput = "mockSearchInput";
 
-jest.mock("next/router", () => ({
+vi.mock("next/router", () => ({
   useRouter() {
     return { asPath: "", query: "" };
   },
 }));
 
-jest.mock("react-query", () => ({
-  useQuery: jest.fn(() => ({
+vi.mock("react-query", () => ({
+  useQuery: vi.fn(() => ({
     data: {},
     isLoading: false,
     error: null,
@@ -23,6 +22,6 @@ jest.mock("react-query", () => ({
 describe("Landing Page: ", () => {
   it("should render MCF Landing Page", () => {
     render(<LandingPage handleSearchInput={mockHandleSearchInput} searchInput={mockSearchInput} />);
-    expect(screen.getByText("Multilateral Climate Funds")).toBeInTheDocument();
+    expect(screen.getByText("Multilateral Climate Funds")).toBeDefined();
   });
 });
