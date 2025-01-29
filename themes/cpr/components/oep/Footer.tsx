e2a604bcb408ba9b349c1a552cf6b81b85cedd36
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2025-01-29 13:53:35 +0000

    Re-style footer

--- themes/cpr/components/oep/Footer.tsx
+++ themes/cpr/components/oep/Footer.tsx
@@ -6,30 +6,30 @@ import { LinkWithQuery } from "@components/LinkWithQuery";
 
 export const Footer = () => {
   return (
-    <footer className="py-12 bg-white">
+    <footer className="py-12 bg-white border-t">
       <SiteWidth>
         <LinkWithQuery href="/">
           <Image src="/images/cpr-logo-horizontal-dark.svg" width={228} height={35} alt="Climate Policy Radar logo" data-cy="cpr-logo" />
         </LinkWithQuery>
-        <p className="font-medium text-lg mb-5 text-textDark md:text-center">
+        <p className="text-lg my-9 text-textNormal">
           To report a problem, email us at support@climatepolicyradar.org <br /> Spotted missing or inaccurate data?{" "}
-          <ExternalLink url="https://form.jotform.com/233294135296359" className="text-white underline hover:text-white">
+          <ExternalLink url="https://form.jotform.com/233294135296359" className="text-textDark underline">
             Let us know
           </ExternalLink>
         </p>
         <ul className="flex gap-9">
           <li>
-            <ExternalLink url="https://github.com/climatepolicyradar/methodology" className="text-textNormal underline">
+            <ExternalLink url="https://github.com/climatepolicyradar/methodology" className="text-textNormal underline opacity-60 hover:opacity-100">
               Methodology
             </ExternalLink>
           </li>
           <li>
-            <LinkWithQuery href="/terms-of-use" className="text-textNormal underline">
+            <LinkWithQuery href="/terms-of-use" className="text-textNormal underline opacity-60 hover:opacity-100">
               Terms &amp; conditions
             </LinkWithQuery>
           </li>
           <li>
-            <ExternalLink url="https://www.climatepolicyradar.org/privacy-policy" className="text-textNormal underline">
+            <ExternalLink url="https://www.climatepolicyradar.org/privacy-policy" className="text-textNormal underline opacity-60 hover:opacity-100">
               Privacy policy
             </ExternalLink>
           </li>
