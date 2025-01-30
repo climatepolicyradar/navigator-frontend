5114a55bd95d019a21d79d3ea673c8efb0e16f8e
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2025-01-30 17:59:27 +0000

    Add custom scroll bar for OEP

--- themes/cpr/pages/oep.tsx
+++ themes/cpr/pages/oep.tsx
@@ -1,3 +1,5 @@
+import Head from "next/head";
+
 import Layout from "@components/layouts/LandingPage";
 
 import { Hero } from "@cpr/components/oep/Hero";
@@ -7,6 +9,12 @@ import { Footer } from "@cpr/components/oep/Footer";
 const OceanEnergyPathwayPage = () => {
   return (
     <>
+      <Head>
+        <></>
+        {/* TODO: FIX THIS AND IGNORE */}
+        {/* trunk-ignore(eslint/@next/next/no-css-tags) */}
+        {/* <link rel="stylesheet" href="/css/oep/oep.css" /> */}
+      </Head>
       <Layout title="Ocean Energy Pathway">
         <Header />
         <main id="oep">
