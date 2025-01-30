3e533e9e267d2a89ef337b20636a594660be8264
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2025-01-30 10:31:13 +0000

    Render concepts on document page (#327)

--- src/components/documents/renderers/DocumentMetaRenderer.tsx
+++ src/components/documents/renderers/DocumentMetaRenderer.tsx
@@ -1,10 +1,21 @@
 import { McfFamilyMeta } from "@components/document/McfFamilyMeta";
 import { DocumentMeta } from "../DocumentMeta";
 import { getApprovedYearFromEvents } from "@helpers/getApprovedYearFromEvents";
+import { TConcept } from "@types";
 
 const MULTILATERALCLIMATEFUNDSCATEGORY = "MCF";
 
-export const DocumentMetaRenderer = ({ family, isMain, document }) => {
+export const DocumentMetaRenderer = ({
+  family,
+  isMain,
+  document,
+  concepts,
+}: {
+  family: any;
+  isMain: boolean;
+  document: any;
+  concepts?: (TConcept & { count: number })[];
+}) => {
   const { metadata, organisation, category, geographies } = family || {};
 
   const mcfFamilyMetadata = {
@@ -16,8 +27,8 @@ export const DocumentMetaRenderer = ({ family, isMain, document }) => {
   };
 
   if (category !== MULTILATERALCLIMATEFUNDSCATEGORY) {
-    return <DocumentMeta family={family} isMain={isMain} document={document} />;
+    return <DocumentMeta family={family} isMain={isMain} document={document} concepts={concepts} />;
   }
 
-  return <McfFamilyMeta metadata={mcfFamilyMetadata} />;
+  return <McfFamilyMeta metadata={mcfFamilyMetadata} concepts={concepts} />;
 };
