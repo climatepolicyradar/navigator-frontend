83f1f48326fc32c8bdc935d83b35e3d1abdaba65
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2025-01-30 16:37:47 +0000

    Couple of small adjustments from Conor's feedback

--- themes/cpr/components/oep/Hero.tsx
+++ themes/cpr/components/oep/Hero.tsx
@@ -31,27 +31,32 @@ export const Hero = () => {
     }
   };
 
-  const handleSubmit = () => {
-    router.push({ pathname: "/search", query: { [QUERY_PARAMS.query_string]: term, [QUERY_PARAMS.category]: "Reports" } });
+  const handleSuggestionClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    handleSubmit(e.currentTarget.textContent);
+  };
+
+  const handleSubmit = (query?: string) => {
+    router.push({ pathname: "/search", query: { [QUERY_PARAMS.query_string]: query ?? term, [QUERY_PARAMS.category]: "Reports" } });
   };
 
   return (
     <section>
-      <div className="h-[800px] relative z-1 overflow-hidden">
+      <div className="h-[600px] relative z-1 overflow-hidden md:h-[800px]">
         <div className="oep-hero-left oep-hero-background"></div>
         <div className="oep-hero-bg oep-hero-background"></div>
         <div className="oep-hero-right oep-hero-background"></div>
         <SiteWidth>
           <SingleCol>
-            <div className="relative z-10 pt-[218px] pb-[100px]">
+            <div className="relative z-10 pb-[100px] pt-[100px] md:pt-[218px]">
               <div className="mb-6">
                 <Image src="/images/oep/oep-logo-small.png" width={155} height={54} alt="Ocean Energy Pathway logo" data-cy="oep-logo" />
               </div>
-              <h1 className="font-['tenez'] font-bold italic text-oep-royal-blue text-8xl tracking-[-0.96px] leading-[80%]">
+              <h1 className="font-['tenez'] font-bold italic text-oep-royal-blue tracking-[-0.96px] leading-[80%] text-7xl md:text-8xl">
                 <span className="not-italic">POWER</span> library
               </h1>
-              <p className="my-9 text-2xl text-textDark">Helping the offshore wind sector design effective strategies</p>
-              <div className="relative">
+              <p className="my-6 text-xl text-textDark md:text-2xl">Helping the offshore wind sector design effective strategies</p>
+              <div className="relative z-1 mb-4">
                 <button className="h-full absolute left-0 px-4" onClick={() => handleSubmit()} aria-label="Search">
                   <span className="block">
                     <SearchIcon />
@@ -62,13 +67,33 @@ export const Hero = () => {
                   data-analytics="oep-searchInput"
                   data-cy="search-input"
                   type="search"
-                  className="w-full text-textDark py-5 pl-[52px] text-normal bg-white rounded-xl shadow-oep focus:border-white focus:ring-0 placeholder:text-textNormal"
+                  className="w-full text-textDark py-5 pl-[52px] text-normal bg-white rounded-xl shadow-oep border-[#f1f1f1] focus:border-white focus:ring-0 placeholder:text-textNormal"
                   value={term}
                   onChange={(e) => setTerm(e.currentTarget.value)}
                   onKeyDown={handleKeydown}
                   placeholder="Search..."
                   aria-label="Search"
                 />
+              </div>
+              <div className="flex gap-4 relative z-2 text-sm">
+                <p className="font-medium text-textDark">Suggestions:</p>
+                <ul className="flex flex-col md:flex-row gap-2 md:gap-4">
+                  <li>
+                    <a href="" onClick={handleSuggestionClick} className="text-textDark opacity-60 hover:opacity-100">
+                      Offshore wind
+                    </a>
+                  </li>
+                  <li>
+                    <a href="" onClick={handleSuggestionClick} className="text-textDark opacity-60 hover:opacity-100">
+                      Energy security
+                    </a>
+                  </li>
+                  <li>
+                    <a href="" onClick={handleSuggestionClick} className="text-textDark opacity-60 hover:opacity-100">
+                      Renewables
+                    </a>
+                  </li>
+                </ul>
               </div>
             </div>
           </SingleCol>
