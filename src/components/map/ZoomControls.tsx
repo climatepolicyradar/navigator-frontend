028d8c7601fa9a177e1c918ea3310bf128981325
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2025-02-17 14:20:10 +0000

    Adjust border color in map

--- src/components/map/ZoomControls.tsx
+++ src/components/map/ZoomControls.tsx
@@ -10,18 +10,18 @@ type TProps = {
 export const ZoomControls = ({ mapZoom, minZoom, maxZoom, handleZoomIn, handleZoomOut, handleReset }: TProps) => {
   return (
     <div className="absolute bottom-0 right-0 p-4 flex gap-2">
-      <button className="text-xs bg-white text-gray-500 rounded-full border h-[40px] px-4" onClick={handleReset}>
+      <button className="text-xs bg-white text-gray-500 rounded-full border border-gray-300 h-[40px] px-4" onClick={handleReset}>
         Reset
       </button>
       <button
-        className="bg-white text-gray-500 rounded-full border w-[40px] h-[40px] disabled:opacity-50"
+        className="bg-white text-gray-500 rounded-full border w-[40px] h-[40px] border-gray-300 disabled:opacity-50"
         disabled={mapZoom === maxZoom}
         onClick={handleZoomIn}
       >
         +
       </button>
       <button
-        className="bg-white text-gray-500 rounded-full border w-[40px] h-[40px] disabled:opacity-50"
+        className="bg-white text-gray-500 rounded-full border w-[40px] h-[40px] border-gray-300 disabled:opacity-50"
         disabled={mapZoom === minZoom}
         onClick={handleZoomOut}
       >
