2866755f97a907f244f25d9ffcd39482942428b7
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2025-02-19 11:39:43 +0000

    Add MCF google tag manager script

--- themes/mcf/components/Analytics.tsx
+++ themes/mcf/components/Analytics.tsx
@@ -1,9 +1,31 @@
+import Script from "next/script";
+
 type TProps = {
   enableAnalytics: boolean;
 };
 
+const MCF_GA_ID = "G-4SHFJ172GL";
+
 const Analytics = ({ enableAnalytics }: TProps) => {
-  return null;
+  return (
+    <>
+      {enableAnalytics && (
+        <>
+          <Script async src={`https://www.googletagmanager.com/gtag/js?id=${MCF_GA_ID}`} />
+          <Script id="mcf-google-analytics" strategy="afterInteractive">
+            {`
+              (function(w,l,i) {
+                w[l] = w[l] || [];
+                function gtag(){w[l].push(arguments);}
+                gtag('js', new Date());
+                gtag('config', i);
+              })(window, "dataLayer", "${MCF_GA_ID}");
+            `}
+          </Script>
+        </>
+      )}
+    </>
+  );
 };
 
 export default Analytics;
