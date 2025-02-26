53157e1576637fa0a1c5d38c62782cf4baad6580
Jessica Kelsall <jessica.kelsall@climatepolicyradar.org>
2025-02-26 16:30:40 +0000

    Icons editable in Storybook

--- src/components/icon/Icon.stories.tsx
+++ src/components/icon/Icon.stories.tsx
@@ -8,6 +8,9 @@ const meta = {
     layout: "centered",
   },
   argTypes: {
+    color: { control: "color" },
+    height: { control: "text" },
+    width: { control: "text" },
     name: {
       control: "select",
       options: iconNames,
@@ -19,14 +22,19 @@ type Story = StoryObj<typeof Icon>;
 export default meta;
 
 export const AllIcons: Story = {
+  args: {
+    color: "#505050",
+    height: "40",
+    width: "40",
+  },
   argTypes: {
     name: { control: false },
   },
-  render: () => (
+  render: ({ color, height, width }) => (
     <div className="grid grid-cols-6 gap-y-10">
       {iconNames.map((name) => (
         <div key={name} className="flex flex-col items-center gap-1">
-          <Icon name={name} height="40" width="40" />
+          <Icon color={color} name={name} height={height} width={width} />
           <span>{name}</span>
         </div>
       ))}
@@ -36,6 +44,9 @@ export const AllIcons: Story = {
 
 export const Primary: Story = {
   args: {
+    color: "#505050",
     name: "close",
+    height: "16",
+    width: "16",
   },
 };
