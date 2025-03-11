ea1bf0759804e50d0377a029a67e34560bbf854f
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2025-03-11 10:55:27 +0000

    Add missing react imports

--- src/components/FloatingSearch.tsx
+++ src/components/FloatingSearch.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import { useEffect, useRef, useState } from "react";
 
 import { QUERY_PARAMS } from "@/constants/queryParams";
 import { CleanRouterQuery } from "@/utils/cleanRouterQuery";
