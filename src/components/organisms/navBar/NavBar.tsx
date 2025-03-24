79452ef6bac49aff95b6a603c18d880cdfffd191
Jessica Kelsall <jessica.kelsall@climatepolicyradar.org>
2025-03-24 11:01:51 +0000

    Sticky header

--- src/components/organisms/navBar/NavBar.tsx
+++ src/components/organisms/navBar/NavBar.tsx
@@ -9,7 +9,10 @@ export const NavBar = () => {
 
   return (
     <>
-      <header data-cy="header" className="w-full sm:px-4 banner flex justify-between items-center flex-wrap sm:flex-nowrap gap-y-1 sm:gap-y-0">
+      <header
+        data-cy="header"
+        className="w-full sm:px-4 banner flex justify-between items-center flex-wrap sm:flex-nowrap gap-y-1 sm:gap-y-0 sticky top-0 z-100"
+      >
         <div className="lg:flex-1 pl-4 pt-4 sm:pl-0 sm:pt-0">
           <LinkWithQuery href="/">
             <Image src="/images/cpr-logo-horizontal.png" width={228} height={35} alt="Climate Policy Radar logo" data-cy="cpr-logo" />
@@ -22,12 +25,6 @@ export const NavBar = () => {
           <MainMenu />
         </div>
       </header>
-      <div>
-        <span className="sm:hidden">sm</span>
-        <span className="md:hidden">md</span>
-        <span className="lg:hidden">lg</span>
-        <span className="lg:hidden">xl</span>
-      </div>
     </>
   );
 };
