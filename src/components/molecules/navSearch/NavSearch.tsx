6ee281594c3177d722bedd9425f01bb8c4c59850
Jessica Kelsall <jessica.kelsall@climatepolicyradar.org>
2025-04-01 11:10:52 +0100

    NavSearch: show pop-around when focused + input empty

--- src/components/molecules/navSearch/NavSearch.tsx
+++ src/components/molecules/navSearch/NavSearch.tsx
@@ -162,7 +162,7 @@ export const NavSearch = () => {
 
       {/* Results */}
       {isFocused && (
-        <div className="absolute top-0 left-0 right-0 outline -outline-offset-1 outline-border-lighter rounded-xl bg-surface-light shadow-[0px_4px_48px_0px_rgba(0,0,0,0.08)]">
+        <div className="absolute top-0 left-0 right-0 min-h-[56px] outline -outline-offset-1 outline-border-lighter rounded-xl bg-surface-light shadow-[0px_4px_48px_0px_rgba(0,0,0,0.08)]">
           {showResults && (
             <div className="flex flex-col gap-3 p-2 pt-[56px]">
               {/* Geographies */}
