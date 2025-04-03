881472d3a8bdfdef3936e470b3b2975079d4da7d
Jessica Kelsall <jessica.kelsall@climatepolicyradar.org>
2025-04-03 14:43:00 +0100

    APP-375: Concepts on a document page (#441)
    * Add v1 of slide out on search page
    
    * Set up slide out story
    
    * Add v1 concepts picker
    
    * Add animation to slide out
    
    * Update story
    
    * Add focus border to inputs
    
    * Add few UI updates from Conor
    
    * Add nice scroll to top of search area when a new search kicks off
    
    * Comment out crap sort picker
    
    * Concept results capitalised first letter
    
    * move concept grouping into reusable utility
    
    * Add z index to slide out
    
    * Add sorting into the concept picker
    
    * Add custom select box
    
    * inherit width from anchor, set styling of items
    
    * Add sort label to the sort picker
    
    * Add scroll bar colours to theme
    
    * Update UI to be more consistent with existing select design
    
    * Update inputs to use new colors
    
    * Capitalise concept in applied filters
    
    * Update focus colours
    
    * Hide concept picker on mobile device (for now)
    
    * Move all external icons over to Lucide due to build
    
    * Unecessary check in className
    
    * Up size limit to 375kb
    
    * Speed up animation
    
    * Pass in an optional custom container to the select component
    
    * Handle clicking outside slideOut to close
    
    * Fix alignment of accordian icons
    
    * Update alignment on the mobile view for no. of results and filters
    
    * Move the data attribute key to a constant
    
    * Strongly type slide out context content options
    
    * Export slide out context content type
    
    * Sort options derive type from collection, expand select box to accept a futureproof shape
    
    * Clean up change handler for concepts
    
    * Use trendy new conditional checker
    
    * Use existing border colours
    
    * ConceptsDocumentViewer sidebar changes
    
    * ConceptsDocumentViewer concept picker
    
    * ConceptPicker merge conflict fixes
    
    ---------
    
    Co-authored-by: Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>

--- src/components/panels/SideCol.tsx
+++ src/components/panels/SideCol.tsx
@@ -1,9 +1,8 @@
 type TProps = {
   extraClasses?: string;
-  children?: React.ReactNode;
 };
 
-export function SideCol({ extraClasses = "", children, ...props }: TProps) {
+export function SideCol({ extraClasses = "", children, ...props }: TProps & React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div className={`w-maxSidebar grow-0 shrink-0 ${extraClasses}`} {...props}>
       {children}
