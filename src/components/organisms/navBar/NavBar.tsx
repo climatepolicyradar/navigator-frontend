f7bf198a0212664d8298529293272a72109fe944
Jessica Kelsall <jessica.kelsall@climatepolicyradar.org>
2025-04-07 14:09:15 +0100

    Various NavBar fixes

--- src/components/organisms/navBar/NavBar.tsx
+++ src/components/organisms/navBar/NavBar.tsx
@@ -10,17 +10,16 @@ interface NavBarProps {
 
 export const NavBar = ({ headerClasses = "", logo, menu, showLogo = true, showSearch = true }: NavBarProps) => {
   return (
-    <header
-      data-cy="header"
-      className={`w-full h-[128px] md:h-[72px] sm:px-4 pt-2 sm:pb-2 flex justify-between items-center flex-wrap sm:flex-nowrap gap-y-1 sm:gap-y-0 ${headerClasses}`}
-    >
-      {showLogo && <div className="lg:flex-1 shrink-4 md:max-w-[30%] px-4 sm:pl-0 sm:pt-0">{logo}</div>}
-      {showSearch && (
-        <div className="flex-[1_1_100%] sm:flex-initial order-1 sm:order-0 lg:min-w-[550px]">
-          <NavSearch />
-        </div>
-      )}
-      <div className="lg:flex-1 flex justify-end pt-4 pr-4 sm:pt-0 sm:pr-0">{menu}</div>
+    <header data-cy="header" className={`w-full h-[128px] sm:h-[72px] ${headerClasses}`}>
+      <div className="max-w-maxSiteWidth sm:px-4 pt-2 sm:pb-2 mx-auto flex justify-between items-center flex-wrap sm:flex-nowrap gap-y-1 sm:gap-y-0">
+        {showLogo && <div className="lg:flex-1 shrink-4 md:max-w-[30%] px-4 sm:pl-0 sm:pt-0">{logo}</div>}
+        {showSearch && (
+          <div className="flex-[1_1_100%] sm:flex-initial order-1 sm:order-0 lg:min-w-[550px]">
+            <NavSearch />
+          </div>
+        )}
+        <div className="lg:flex-1 flex justify-end pt-4 pr-4 sm:pt-0 sm:pr-0">{menu}</div>
+      </div>
     </header>
   );
 };
