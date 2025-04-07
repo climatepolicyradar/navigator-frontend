c0f18f676644055611542bc485a087d649c53c5a
Anna Pokorska <anna@climatepolicyradar.org>
2025-04-07 14:58:42 +0100

    Move all unit tests to the tests folder

--- tests/unit/pages/geographies.test.tsx
+++ tests/unit/pages/geographies.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import CountryPage from "./[id]";
+import CountryPage from "../../../src/pages/geographies/[id]";
 
 vi.mock("next/router", () => ({
   useRouter: () => {
