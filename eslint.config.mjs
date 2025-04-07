0a125659e75e0970d2e5d7c82b6a9017641da236
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2025-04-07 17:44:34 +0100

    Add missing rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import globals from "globals";
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import prettier from "eslint-config-prettier";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
@@ -10,4 +11,13 @@ export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  prettier,
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-html-link-for-pages": "off",
+      eqeqeq: "warn",
+      "no-console": "warn",
+    },
+  },
 ]);
