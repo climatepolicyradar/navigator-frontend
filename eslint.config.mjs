0a56bc7ed4a76724e9aeadf2a63b8ebeee01d3b7
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2025-04-08 12:25:20 +0100

    Migrate eslint to flat config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,15 +4,47 @@ import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import prettier from "eslint-config-prettier";
+import tsParser from "@typescript-eslint/parser";
+import next from "@next/eslint-plugin-next";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  prettier,
   {
+    ignores: [
+      "**/dist",
+      "**/build",
+      "**/node_modules",
+      "**/*.config.js",
+      "**/*.config.mjs",
+      "**/eslint.config.mjs",
+      "**/vite.config.{js,mts}",
+      "tailwind.config.js",
+      "postcss.config.js",
+      ".storybook/preview.ts",
+      ".storybook/main.ts",
+    ],
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    plugins: {
+      js: js,
+      "@typescript-eslint": tseslint.configs.strict,
+      prettier: prettier,
+      "react-hooks": pluginReact.configs.flat.recommended,
+      "@next/next": next,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.es2025,
+      },
+      parser: tsParser,
+      parserOptions: {
+        project: "./tsconfig.json",
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
     rules: {
       "react/no-unescaped-entities": "off",
       "@next/next/no-html-link-for-pages": "off",
