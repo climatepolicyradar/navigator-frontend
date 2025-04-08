947640c1208fd67a290508ab7c04dd8106e12355
Jessica Kelsall <jessica.kelsall@climatepolicyradar.org>
2025-04-08 09:33:53 +0100

    APP-456: Not displaying passages when searching with concept but no search (#452)
    * Fixed passage results not updating on query clear
    
    * Various NavBar fixes
    
    * Fix NavSearch results z-index
    
    * Fix SearchSettings position on documents page

--- themes/cpr/layouts/main.tsx
+++ themes/cpr/layouts/main.tsx
@@ -18,7 +18,7 @@ type TProps = {
 const Main: FC<TProps> = ({ children }) => (
   <>
     <NavBar headerClasses="banner" logo={CPRLogo} menu={<MainMenu />} />
-    <div className="h-[calc(100vh-128px)] md:h-[calc(100vh-72px)] flex flex-col justify-between overflow-y-auto">
+    <div className="h-[calc(100vh-128px)] sm:h-[calc(100vh-72px)] flex flex-col justify-between overflow-y-auto">
       <main className="flex flex-col flex-1">{children}</main>
       <Footer />
     </div>
