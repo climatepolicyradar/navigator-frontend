60f2f3cfafda979a0b967bb82eab00837b253711
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2025-04-09 10:34:16 +0100

    App 448: concept picker mobile support (#456)

--- src/components/atoms/select/Select.tsx
+++ src/components/atoms/select/Select.tsx
@@ -21,7 +21,7 @@ export function Select({ defaultValue, value, options, onValueChange, container 
   };
 
   return (
-    <BaseSelect.Root defaultValue={defaultValue} onValueChange={handleValueChange} value={value}>
+    <BaseSelect.Root defaultValue={defaultValue} onValueChange={handleValueChange} value={value} alignItemToTrigger={false}>
       <BaseSelect.Trigger className="flex items-center justify-between gap-2 p-2 border border-border-light bg-surface-ui b-1 rounded-sm text-sm m-0 outline-0 select-none cursor-default hover:border-inputSelected active:bg-surface-ui data-popup-open:bg-surface-ui focus:border-inputSelected">
         <BaseSelect.Value placeholder="" />
         <BaseSelect.Icon className="flex">
