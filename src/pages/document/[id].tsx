12a2d418923617742511464e5a826f3816d58d73
James Gorrie <jamesgorrie@users.noreply.github.com>
2025-04-10 14:11:00 +0100

    fix: remove NEXT_PUBLIC_APP_TOKEN env var that isn't public (#462)

--- src/pages/document/[id].tsx
+++ src/pages/document/[id].tsx
@@ -444,7 +444,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 
   const theme = process.env.THEME;
   const id = context.params.id;
-  const client = new ApiClient(process.env.API_URL);
+  const client = new ApiClient(process.env.BACKEND_API_URL);
 
   let familyData: TFamilyPage;
   let vespaFamilyData: TSearchResponse;
