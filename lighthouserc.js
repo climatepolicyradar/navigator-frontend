344f61443ddf4f8702602bd4de492dd7f309714a
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2025-04-14 17:30:59 +0100

    Remove assertions and add lighthouse token for reporting

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,8 +1,5 @@
 module.exports = {
   ci: {
-    assert: {
-      preset: "lighthouse:recommended",
-    },
     collect: {
       url: ["http://localhost:3000/"],
       startServerCommand: "npm run start",
