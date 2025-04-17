e5e671e07fc646ac48a3d0e680b71b7f1b5cd464
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2025-04-17 11:13:47 +0100

    Add seperate jobs for mobile and desktop, try adding search page

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,7 @@
 module.exports = {
   ci: {
     collect: {
-      url: ["http://localhost:3000/"],
+      url: ["http://localhost:3000/", "http://localhost:3000/search"],
       startServerCommand: "npm run start",
     },
     upload: {
