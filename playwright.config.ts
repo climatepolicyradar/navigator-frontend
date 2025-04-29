da3847f29c303d071d650710aca35cb18e9f0cfe
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2025-04-29 09:16:56 +0100

    Force trace on first fail and no retries

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   // reporter: [["junit", { outputFile: "playright.xml" }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -38,7 +38,7 @@ export default defineConfig({
     baseURL: envConfig.baseURL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    trace: "on",
   },
 
   /* Configure projects for major browsers */
