cc7d5ac5c1fe11ed4e020ab40656f6501ff30115
Jessica Kelsall <jessica.kelsall@climatepolicyradar.org>
2025-04-29 14:20:36 +0100

    Collection page without calling family API endpoint

--- src/pages/collection/[id].tsx
+++ src/pages/collection/[id].tsx
@@ -1,8 +1,15 @@
 import { ApiClient } from "@/api/http-common";
+import { BreadCrumbs } from "@/components/breadcrumbs/Breadcrumbs";
+import Layout from "@/components/layouts/Main";
+import { SubNav } from "@/components/nav/SubNav";
+import { MultiCol } from "@/components/panels/MultiCol";
+import { SingleCol } from "@/components/panels/SingleCol";
+import { Heading } from "@/components/typography/Heading";
 import { withEnvConfig } from "@/context/EnvConfig";
-import { TTheme } from "@/types";
+import { TCollection, TTheme } from "@/types";
 import { getFeatureFlags } from "@/utils/featureFlags";
 import { GetServerSideProps, InferGetStaticPropsType } from "next";
+import { LuFiles } from "react-icons/lu";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   context.res.setHeader("Cache-Control", "public, max-age=3600, immutable");
@@ -12,36 +19,80 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   const id = context.params.id;
   const client = new ApiClient(process.env.BACKEND_API_URL);
 
-  // if (!featureFlags["litigation"]) {
-  //   return { notFound: true };
-  // }
+  if (!featureFlags["litigation"]) {
+    return { notFound: true };
+  }
+
+  let collection: TCollection;
 
   try {
     const { data: returnedData } = await client.get(`/collections/${id}`);
-    console.log({ returnedData });
+    collection = returnedData;
   } catch (error) {
     // TODO: handle error more elegantly
   }
 
+  if (!collection) {
+    return { notFound: true };
+  }
+
   return {
     props: withEnvConfig({
-      id: `${id}`, // TODO remove
+      collection,
       theme,
     }),
   };
 };
 
 type TProps = {
-  id: string; // TODO remove
+  collection: TCollection;
   theme: TTheme;
 };
 
-const CollectionPage: InferGetStaticPropsType<typeof getServerSideProps> = ({ id, theme }: TProps) => (
-  <div>
-    <h1>Collection</h1>
-    <p>ID: {id}</p>
-    <p>Theme: {theme}</p>
-  </div>
-);
+const CollectionPage: InferGetStaticPropsType<typeof getServerSideProps> = ({ collection, theme }: TProps) => {
+  const { families } = collection;
+  const breadcrumbCategory = { label: "Search results", href: "/search" };
+
+  return (
+    <Layout title={collection.title} description={collection.description} theme={theme}>
+      <section className="mb-8">
+        <SubNav>
+          <BreadCrumbs category={breadcrumbCategory} label={collection.title} />
+        </SubNav>
+        <MultiCol>
+          <SingleCol extraClasses={`mt-8 px-5 w-full`}>
+            <Heading level={1}>{collection.title}</Heading>
+            <div className="flex flex-wrap text-sm gap-1 mt-2 items-center middot-between" data-cy="family-metadata">
+              Collection
+            </div>
+            <section className="mt-6">
+              <div className="text-content">{collection.description}</div>
+            </section>
+
+            {families.length > 0 && (
+              <section className="mt-10">
+                <Heading level={2}>Documents</Heading>
+                {families.map((family) => (
+                  <a
+                    key={family.slug}
+                    href={`/document/${family.slug}`}
+                    className="family-document group mt-4 p-4 rounded-lg border bg-gray-25 border-gray-100 shadow-xs transition duration-300 flex flex-no-wrap cursor-pointer hover:border-blue-100 hover:bg-gray-50"
+                  >
+                    <div className="flex-0 mr-2 hidden md:block">
+                      <LuFiles size="20" className="mt-0.5 text-text-brand" />
+                    </div>
+                    <div className="flex-1">
+                      <span dangerouslySetInnerHTML={{ __html: family.title }} />
+                    </div>
+                  </a>
+                ))}
+              </section>
+            )}
+          </SingleCol>
+        </MultiCol>
+      </section>
+    </Layout>
+  );
+};
 
 export default CollectionPage;
