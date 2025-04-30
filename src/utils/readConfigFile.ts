1a3bc627a05a1a650efe2eaa3e88aab09b5aa5b1
Jessica Kelsall <jessica.kelsall@climatepolicyradar.org>
2025-04-30 14:04:03 +0100

    No Litigation category without feature flag

--- src/utils/readConfigFile.ts
+++ src/utils/readConfigFile.ts
@@ -1,9 +1,8 @@
-import { TThemeConfig } from "@/types";
+import { TTheme, TThemeConfig } from "@/types";
 import { promises as fs } from "fs";
 import path from "path";
 import { TFeatureFlags } from "./featureFlags";
 
-// Modifies config by reference
 const addCorporaToCategories = (config: TThemeConfig, categorySlugs: string[], corpusIds: string[]): void => {
   config.categories.options.forEach((category) => {
     if (categorySlugs.includes(category.slug)) {
@@ -12,13 +11,23 @@ const addCorporaToCategories = (config: TThemeConfig, categorySlugs: string[], c
   });
 };
 
+const removeCategories = (config: TThemeConfig, categorySlugs: string[]): void => {
+  config.categories.options.forEach((category, categoryIndex) => {
+    if (categorySlugs.includes(category.slug)) {
+      config.categories.options.splice(categoryIndex, 1);
+    }
+  });
+};
+
 export const readConfigFile = async (theme: string, featureFlags: TFeatureFlags): Promise<TThemeConfig> => {
   const dataFilePath = path.join(process.cwd(), `/themes/${theme}/config.json`);
   const fileContents = await fs.readFile(dataFilePath, "utf8");
-  const config = JSON.parse(fileContents);
+  const config: TThemeConfig = JSON.parse(fileContents);
 
   if (featureFlags.litigation) {
     addCorporaToCategories(config, ["All"], ["Academic.corpus.Litigation.n0000"]);
+  } else {
+    removeCategories(config, ["Litigation"]);
   }
 
   return config;
