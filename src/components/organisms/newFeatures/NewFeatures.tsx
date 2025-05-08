b90311aab81a4bf0275d54dbbdc119c48e71d69d
Jessica Kelsall <jessica.kelsall@climatepolicyradar.org>
2025-05-08 11:26:37 +0100

    Design round 2

--- src/components/organisms/newFeatures/NewFeatures.tsx
+++ src/components/organisms/newFeatures/NewFeatures.tsx
@@ -4,6 +4,7 @@ import { COOKIE_FEATURES_NAME } from "@/constants/cookies";
 import { LATEST_FEATURE } from "@/constants/newFeatures";
 import { getCookie, setCookie } from "@/utils/cookies";
 import getDomain from "@/utils/getDomain";
+import Image from "next/image";
 import { useEffect, useState } from "react";
 import { LuX } from "react-icons/lu";
 
@@ -11,7 +12,7 @@ export const NewFeatures = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [latestUpdateIsNew, setLatestUpdateIsNew] = useState(false);
 
-  const { featureValue, modalContent, modalTitle, popupCTA, popupDescription, popupTitle } = LATEST_FEATURE;
+  const { featureValue, modalContent, modalTitle, popupCTA, popupDescription, popupImage, popupTitle } = LATEST_FEATURE;
 
   // Check if there is an update to show the user
   useEffect(() => {
@@ -38,24 +39,27 @@ export const NewFeatures = () => {
       {/* Call to action */}
       {latestUpdateIsNew && (
         <div className="fixed inset-0 z-90 p-4 flex flex-col justify-end pointer-events-none">
-          <Card className="max-w-[300px] !p-4 flex items-start gap-2 pointer-events-auto select-none">
-            <div>
-              {popupTitle && <h4 className="font-bold">{popupTitle}</h4>}
-              <p className="my-3">{popupDescription}</p>
-              <a href="#" onClick={() => setModalIsOpen(true)} className="underline">
-                {popupCTA}
-              </a>
-            </div>
-            <button onClick={() => markUpdateSeen()}>
-              <LuX height="16" width="16" />
-            </button>
-          </Card>
+          <div className="w-[300px]">
+            {popupImage && <Image src={popupImage.src} alt={popupImage.alt} height={200} width={300} className="w-full h-auto rounded-t-md" />}
+            <Card className="max-w-[300px] !p-4 flex items-start gap-2 pointer-events-auto select-none !rounded-none !rounded-b-md">
+              <div>
+                {popupTitle && <h4 className="font-bold">{popupTitle}</h4>}
+                <p className="my-3">{popupDescription}</p>
+                <a href="#" onClick={() => setModalIsOpen(true)} className="underline">
+                  {popupCTA}
+                </a>
+              </div>
+              <button onClick={() => markUpdateSeen()}>
+                <LuX height="16" width="16" />
+              </button>
+            </Card>
+          </div>
         </div>
       )}
 
       {/* Modal */}
       <Modal isOpen={modalIsOpen} onClose={onModalClose} cardClasses="">
-        <div className="h-full flex flex-col gap-4">
+        <div className="h-full flex flex-col gap-4 w-[600px]">
           <h1 className="text-2xl font-medium">{modalTitle}</h1>
           <div className="h-full pr-2 overflow-y-auto">
             <div className="flex flex-col items-center gap-6 overflow-y-auto">{modalContent}</div>
--- src/constants/newFeatures.tsx
+++ src/constants/newFeatures.tsx
@@ -1,8 +1,13 @@
 import _ from "lodash";
+import Image from "next/image";
 
 type NewFeature = {
   featureValue: number;
   popupTitle?: string;
+  popupImage?: {
+    alt: string;
+    src: string;
+  };
   popupDescription: string;
   popupCTA: string;
   modalTitle: string;
@@ -13,6 +18,10 @@ export const NEW_FEATURES: NewFeature[] = [
   {
     featureValue: 1,
     popupTitle: "New Improvements",
+    popupImage: {
+      alt: "TODO",
+      src: "/images/onboarding/TEMP_300x200.svg",
+    },
     popupDescription: "You can now find what you're looking for faster.",
     popupCTA: "More Info",
     modalTitle: "New Improvements",
@@ -22,18 +31,14 @@ export const NEW_FEATURES: NewFeature[] = [
           Climate Policy Radar have introduced a new layer of structure to the data, automatically identifying mentions of key climate concepts in
           documents.
         </p>
-        <div className="bg-text-tertiary w-[500px] h-[300px] flex items-center justify-center text-white">(image: knowledge graph)</div>
+        <Image src="/images/onboarding/TEMP_600x400.svg" alt="TEMP" height={400} width={600} />
         <p className="w-full">
           Moving beyond simple search + browse, this feature will help you quickly find where important topics (i.e. economic sectors, targets, and
           climate finance instruments) appear across the database.
         </p>
-        <div className="bg-text-tertiary w-[500px] h-[300px] flex items-center justify-center text-white">
-          (image: block with specific concept search or whatever is most useful)
-        </div>
+        <Image src="/images/onboarding/TEMP_600x400.svg" alt="TEMP" height={400} width={600} />
         <p className="w-full">You will now be able to identify the primary focuses of each document faster too.</p>
-        <div className="bg-text-tertiary w-[500px] h-[300px] flex items-center justify-center text-white">
-          (shot of the count of key concepts within each document)
-        </div>
+        <Image src="/images/onboarding/TEMP_600x400.svg" alt="TEMP" height={400} width={600} />
       </>
     ),
   },
