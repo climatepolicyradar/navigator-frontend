2317d4781f9e9dd620b7b4245ac60f4944b3c86f
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2025-05-22 12:01:34 +0100

    Fix feature flagged FAQs

--- themes/ccc/pages/faq.tsx
+++ themes/ccc/pages/faq.tsx
@@ -1,86 +1,48 @@
-import { Fragment } from "react";
-
+import FaqSection from "@/components/FaqSection";
+import { BreadCrumbs } from "@/components/breadcrumbs/Breadcrumbs";
 import Layout from "@/components/layouts/Main";
 import { SiteWidth } from "@/components/panels/SiteWidth";
-import { SingleCol } from "@/components/panels/SingleCol";
 import { SubNav } from "@/components/nav/SubNav";
-
-import { BreadCrumbs } from "@/components/breadcrumbs/Breadcrumbs";
-import { AccordianItem } from "@/ccc/components/AccordianItem";
-
-import { Heading } from "@/components/typography/Heading";
-
-import { FAQS } from "@/ccc/constants/faqs";
 import { CONCEPTS_FAQS } from "@/constants/conceptsFaqs";
+import { FAQS, PLATFORM_FAQS } from "@/ccc/constants/faqs";
 import { getFeatureFlags } from "@/utils/featureFlags";
-import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import { useState } from "react";
+import { getAllCookies } from "@/utils/cookies";
+import { useEffect } from "react";
 
-interface IProps {
-  featureFlags: Record<string, string | boolean>;
-}
+const FAQ: React.FC = () => {
+  /*
+    The FAQs page is read in not by using Next.JS, but by our CPR specific page reading logic.
+    This means that we cannot fetch the feature flags directly by using the page context.
+    This function provides a means of working around this so we can conditionally display the
+    concept FAQs.
 
-const FAQ: InferGetServerSidePropsType<typeof getServerSideProps> = ({ featureFlags = {} }: IProps) => {
+    TODO: Remove this once we have hard launched concepts in product.
+  */
+  const [featureFlags, setFeatureFlags] = useState({});
+  async function getFeatureFlag() {
+    const featureFlagsCookie = getAllCookies();
+    const featureFlagsFromServer = await getFeatureFlags(featureFlagsCookie);
+    setFeatureFlags(featureFlagsFromServer);
+  }
+
+  // TODO: Remove this once we have hard launched concepts in product.
+  useEffect(() => {
+    getFeatureFlag();
+  }, []);
   return (
     <Layout
       title="FAQ"
-      description="Find quick tips for how you can use this resource to explore national-level climate change laws and policies from across the world."
+      description="Find quick tips for how you can use this resource to explore national-level climate change projects from across the world."
     >
       <SubNav>
         <BreadCrumbs label={"Frequently asked questions"} />
       </SubNav>
-      <section>
+      <section className="pt-8">
         <SiteWidth>
-          <SingleCol>
-            <div className="text-content mb-12">
-              <Heading level={1} extraClasses="my-5">
-                How to use this resource
-              </Heading>
-              <p>You can use this resource to:</p>
-              <ul>
-                <li>Search the full text of over 5000 laws, policies and UNFCCC submissions from every country.</li>
-                <li>See exact matches and related phrases highlighted in the text.</li>
-                <li>Find documents from all languages translated to English to increase accessibility.</li>
-              </ul>
-              <p>Watch the video below to learn how to use this resource</p>
-              <iframe
-                height="315"
-                src="https://www.youtube-nocookie.com/embed/QvM2RybWhMM"
-                title="Learn how to use Climate Laws of the World"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                allowFullScreen
-                className="w-full"
-              ></iframe>
-            </div>
-
-            <div className="text-content mb-12">
-              <Heading level={2}>FAQs</Heading>
-              {FAQS.map((faq, i) => (
-                <Fragment key={faq.title}>
-                  <AccordianItem id={faq.id} title={faq.title} startOpen={i === 0}>
-                    {faq.content}
-                  </AccordianItem>
-                  <hr />
-                </Fragment>
-              ))}
-            </div>
-          </SingleCol>
-
-          {featureFlags["concepts-v1"] === true && (
-            <SingleCol>
-              <div className="text-content mb-12">
-                <Heading level={2}>Concepts FAQs</Heading>
-                {CONCEPTS_FAQS.map((faq, i) => (
-                  <Fragment key={faq.title}>
-                    <AccordianItem id={faq.id} title={faq.title} headContent={faq.headContent ?? null} startOpen={i === 0}>
-                      {faq.content}
-                    </AccordianItem>
-                    <hr />
-                  </Fragment>
-                ))}
-              </div>
-            </SingleCol>
-          )}
+          <FaqSection title="FAQs" faqs={FAQS} />
+          <FaqSection title="Platform FAQs" faqs={PLATFORM_FAQS} />
+          {featureFlags["concepts-v1"] === true && <FaqSection title="Concepts FAQs" faqs={CONCEPTS_FAQS} />}
         </SiteWidth>
       </section>
       <script id="feature-flags" type="text/json" dangerouslySetInnerHTML={{ __html: JSON.stringify(featureFlags) }} />
@@ -88,13 +50,3 @@ const FAQ: InferGetServerSidePropsType<typeof getServerSideProps> = ({ featureFl
   );
 };
 export default FAQ;
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const featureFlags = await getFeatureFlags(context.req.cookies);
-
-  return {
-    props: {
-      featureFlags: featureFlags || {},
-    },
-  };
-};
--- themes/cpr/pages/faq.tsx
+++ themes/cpr/pages/faq.tsx
@@ -1,5 +1,3 @@
-import { GetServerSideProps, InferGetServerSidePropsType } from "next";
-
 import FaqSection from "@/components/FaqSection";
 import { BreadCrumbs } from "@/components/breadcrumbs/Breadcrumbs";
 import Layout from "@/components/layouts/Main";
@@ -7,13 +5,31 @@ import { SiteWidth } from "@/components/panels/SiteWidth";
 import { SubNav } from "@/components/nav/SubNav";
 import { CONCEPTS_FAQS } from "@/constants/conceptsFaqs";
 import { FAQS, PLATFORM_FAQS } from "@/cpr/constants/faqs";
+import { useEffect, useState } from "react";
 import { getFeatureFlags } from "@/utils/featureFlags";
+import { getAllCookies } from "@/utils/cookies";
 
-interface IProps {
-  featureFlags: Record<string, string | boolean>;
-}
+const FAQ: React.FC = () => {
+  /*
+  The FAQs page is read in not by using Next.JS, but by our CPR specific page reading logic.
+  This means that we cannot fetch the feature flags directly by using the page context.
+  This function provides a means of working around this so we can conditionally display the
+  concept FAQs.
 
-const FAQ: InferGetServerSidePropsType<typeof getServerSideProps> = ({ featureFlags = {} }: IProps) => {
+  TODO: Remove this once we have hard launched concepts in product.
+  */
+  const [featureFlags, setFeatureFlags] = useState({});
+  async function getFeatureFlag() {
+    const featureFlagsCookie = getAllCookies();
+    const featureFlagsFromServer = await getFeatureFlags(featureFlagsCookie);
+    setFeatureFlags(featureFlagsFromServer);
+  }
+
+  // TODO: Remove this once we have hard launched concepts in product.
+  useEffect(() => {
+    getFeatureFlag();
+  }, []);
+
   return (
     <Layout
       title="FAQ"
@@ -26,22 +42,11 @@ const FAQ: InferGetServerSidePropsType<typeof getServerSideProps> = ({ featureFl
         <SiteWidth>
           <FaqSection title="FAQs" faqs={FAQS} />
           <FaqSection title="Platform FAQs" faqs={PLATFORM_FAQS} />
-          {featureFlags["concepts-v1"] === true && <FaqSection title="Concepts FAQs" faqs={CONCEPTS_FAQS} />}
+          {featureFlags["concepts-v1"] && <FaqSection title="Concepts FAQs" faqs={CONCEPTS_FAQS} />}
         </SiteWidth>
       </section>
-      <script id="feature-flags" type="text/json" dangerouslySetInnerHTML={{ __html: JSON.stringify(featureFlags) }} />
     </Layout>
   );
 };
 
 export default FAQ;
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const featureFlags = await getFeatureFlags(context.req.cookies);
-
-  return {
-    props: {
-      featureFlags: featureFlags || {},
-    },
-  };
-};
