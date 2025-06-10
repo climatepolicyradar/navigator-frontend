903d9a31c1e2edabf99138cd819fa0064a5dbeff
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2025-06-10 15:38:02 +0100

    Add hyperlink to global search topics

--- src/constants/newFeatures.tsx
+++ src/constants/newFeatures.tsx
@@ -22,6 +22,11 @@ export const NEW_FEATURES: INewFeature[] = [
     modalContent: (
       <>
         <p>We have introduced a new layer of structure to the data, automatically identifying mentions of key climate topics in documents.</p>
+        <p>
+          <ExternalLink url="/search?openConceptsPicker=true" className="underline">
+            Find topics in documents
+          </ExternalLink>
+        </p>
         <Image src="/images/features/knowledge-graph.jpg" alt="A screenshot showing a list of topics" width={792} height={446} />
         <p>
           Moving beyond simple search + browse, this feature will help you quickly find where important topics (i.e. economic sectors, targets, and
