849ed8eae20ff602a3963a9b641d1a2f55f32b81
Jessica Kelsall <jessica.kelsall@climatepolicyradar.org>
2025-06-11 18:56:30 +0100

    🔵 (#577)

--- src/components/search/SearchResult.tsx
+++ src/components/search/SearchResult.tsx
@@ -20,7 +20,7 @@ const SearchResult = ({ family, active, onClick }: IProps) => {
   const matchesText = `View ${matchesNumber} text ${total_passage_hits === 1 ? "passage" : "passages"} matching your search`;
 
   const titleClasses = joinTailwindClasses(
-    hasFamilyDocuments ? "text-text-primary" : "text-text-brand",
+    hasFamilyDocuments ? "text-text-[#0041A3]" : "text-text-brand",
     active ? "!underline" : "!no-underline hover:!underline"
   );
 
