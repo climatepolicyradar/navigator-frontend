f59b41c090dd2fe045ad77aef4f5df3bd9d6116b
Jessica Kelsall <jessica.kelsall@climatepolicyradar.org>
2025-06-16 15:04:02 +0100

    APP-685: Iterate the Topics panel on the right hand side of the family page (#584)

--- src/components/molecules/conceptLink/ConceptLink.tsx
+++ src/components/molecules/conceptLink/ConceptLink.tsx
@@ -1,32 +1,42 @@
-import startCase from "lodash/startCase";
 import { useState } from "react";
 
 import { Popover } from "@/components/atoms/popover/Popover";
 import { TConcept } from "@/types";
 import { getConceptStoreLink } from "@/utils/getConceptStoreLink";
 import { joinTailwindClasses } from "@/utils/tailwind";
+import { firstCase } from "@/utils/text";
 
 interface IProps {
   concept: TConcept;
+  onClick?: (concept: TConcept) => void;
   triggerClasses?: string;
 }
 
-export const ConceptLink = ({ concept, triggerClasses = "" }: IProps) => {
+export const ConceptLink = ({ concept, onClick, triggerClasses = "" }: IProps) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const allTriggerClasses = joinTailwindClasses(
-    "inline capitalize underline underline-offset-2 decoration-dotted cursor-help",
+    "inline capitalize underline underline-offset-2 decoration-dotted",
     isOpen ? "decoration-text-primary" : "decoration-text-quaternary",
+    onClick ? "" : "cursor-help",
     triggerClasses
   );
 
-  const title = startCase(concept.preferred_label);
+  const title = firstCase(concept.preferred_label);
+
+  const trigger = onClick ? (
+    <button className={allTriggerClasses} onClick={() => onClick(concept)}>
+      {title}
+    </button>
+  ) : (
+    <span className={allTriggerClasses}>{title}</span>
+  );
 
   return (
     <Popover
       openOnHover
       onOpenChange={setIsOpen}
-      trigger={<span className={allTriggerClasses}>{title}</span>}
+      trigger={trigger}
       title={title}
       description={concept.description}
       link={{
