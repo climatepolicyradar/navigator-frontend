1d096e47a568757143a6ec5991d826f4c08e887c
Anna Pokorska <anna@climatepolicyradar.org>
2025-06-19 15:26:46 +0100

    Add cache options to refresh countries once a week

--- src/hooks/useCountries.ts
+++ src/hooks/useCountries.ts
@@ -7,11 +7,19 @@ import { TCountry } from "@/types";
 export default function useCountries() {
   const { CONCEPTS_API_URL } = useEnvConfig();
 
-  return useQuery("geographies", async () => {
-    const client = new ApiClient();
-    const geographiesUrl = `${CONCEPTS_API_URL}/geographies`;
-    const query_response = await client.get(geographiesUrl);
-    const countries: TCountry[] = query_response.data;
-    return countries;
-  });
+  return useQuery(
+    "geographies",
+    async () => {
+      const client = new ApiClient();
+      const geographiesUrl = `${CONCEPTS_API_URL}/geographies`;
+      const query_response = await client.get(geographiesUrl);
+      const countries: TCountry[] = query_response.data;
+      return countries;
+    },
+    {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      cacheTime: 1000 * 60 * 60 * 24 * 7,
+    }
+  );
 }
