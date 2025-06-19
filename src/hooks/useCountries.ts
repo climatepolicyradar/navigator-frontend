91ff3c12817bea013a285194fb340c598dcd1fdb
Anna Pokorska <anna@climatepolicyradar.org>
2025-06-19 14:03:31 +0100

    Remove duplicated type from useGepgraphies hook and import from types instead

--- src/hooks/useCountries.ts
+++ src/hooks/useCountries.ts
@@ -2,15 +2,7 @@ import { useQuery } from "react-query";
 
 import { ApiClient } from "@/api/http-common";
 import { useEnvConfig } from "@/context/EnvConfig";
-
-export type TCountry = {
-  alpha2: string;
-  alpha3: string;
-  name: string;
-  official_name: string;
-  numeric: string;
-  flag: string;
-};
+import { TCountry } from "@/types";
 
 export default function useCountries() {
   const { CONCEPTS_API_URL } = useEnvConfig();
