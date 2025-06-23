d109e043d7e2cab1331ac2426ae616d767d6b767
Jessica Kelsall <jessica.kelsall@climatepolicyradar.org>
2025-06-23 15:57:35 +0100

    Add comments

--- src/components/molecules/showMore/ShowMore.tsx
+++ src/components/molecules/showMore/ShowMore.tsx
@@ -29,12 +29,14 @@ export const ShowMore = ({ children, containerClasses, maxHeight }: IProps) => {
     <div className="relative mb-4">
       <div className={allContainerClasses} style={{ maxHeight: isOpen ? undefined : maxHeight }}>
         <div className={isOpen ? "pb-4" : ""}>
+          {/* DOM element to measure height of */}
           <div ref={contentRef}>{children}</div>
         </div>
         {contentIsOverflowing && !isOpen && (
           <div
             className="absolute bottom-0 inset-x-0 h-24"
             style={{
+              // Gradiated blur and white-out top-to-bottom
               background: "linear-gradient(transparent, white)",
               mask: "linear-gradient(transparent, black, black)",
               backdropFilter: "blur(4px)",
@@ -42,6 +44,7 @@ export const ShowMore = ({ children, containerClasses, maxHeight }: IProps) => {
           />
         )}
       </div>
+      {/* Bottom button */}
       {contentIsOverflowing && (
         <div className="absolute -bottom-4 inset-x-0 flex justify-center">
           <Button color="mono" rounded size="small" className="px-3 font-semibold" onClick={onToggle}>
