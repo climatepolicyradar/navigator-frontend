837807ef242baadd5c52973aaca024a8e49c26d2
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2025-06-24 12:43:08 +0100

    Revert CCLW KG switch on (#602)
    * Update deploy-staging.yml
    
    * Turn KG off for CCLW

--- themes/cclw/config.ts
+++ themes/cclw/config.ts
@@ -162,7 +162,7 @@ const config: TThemeConfig = {
   ],
   documentCategories: ["All", "Laws", "Policies", "UNFCCC", "Litigation"],
   features: {
-    knowledgeGraph: true,
+    knowledgeGraph: false,
     searchFamilySummary: true,
   },
 };
