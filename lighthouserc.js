e4f7029c00d0760bcab171adc4fb7612d4343f8e
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2025-06-26 14:30:34 +0100

    Update lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -12,7 +12,7 @@ module.exports = {
     assert: {
       assertions: {
         // Lighthouse runs 3 times by default, so this checks each time the score is above the threshold.
-        // If any of the runs are below the threshold, the build will fail [error].
+        // If the highest result of the 3 runs is below the threshold, the build will fail [error].
         "categories:performance": ["error", { minScore: 0.45 }],
         "categories:accessibility": ["error", { minScore: 0.75 }],
         "categories:best-practices": ["error", { minScore: 0.75 }],
