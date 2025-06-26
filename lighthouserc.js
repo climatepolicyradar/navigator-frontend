a3721c3faae09cb582df2ac305a433307a4274e6
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2025-06-26 14:30:09 +0100

    Update performance threshold to 45%

--- lighthouserc.js
+++ lighthouserc.js
@@ -13,7 +13,7 @@ module.exports = {
       assertions: {
         // Lighthouse runs 3 times by default, so this checks each time the score is above the threshold.
         // If any of the runs are below the threshold, the build will fail [error].
-        "categories:performance": ["error", { minScore: 0.6 }],
+        "categories:performance": ["error", { minScore: 0.45 }],
         "categories:accessibility": ["error", { minScore: 0.75 }],
         "categories:best-practices": ["error", { minScore: 0.75 }],
         "categories:seo": ["error", { minScore: 0.6 }],
