e965adc543c23c588386afc153e38d5d137dad2c
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2025-06-26 14:13:37 +0100

    Add minimum scores for LHCI

--- lighthouserc.js
+++ lighthouserc.js
@@ -9,5 +9,13 @@ module.exports = {
       serverBaseUrl: "http://ec2-54-217-16-2.eu-west-1.compute.amazonaws.com:9001/",
       token: "b68259a3-808c-4c71-84c0-ae91a43e0d9d",
     },
+    assert: {
+      assertions: {
+        "categories:performance": ["warn", { minScore: 0.6 }],
+        "categories:accessibility": ["warn", { minScore: 0.9 }],
+        "categories:best-practices": ["warn", { minScore: 0.9 }],
+        "categories:seo": ["warn", { minScore: 0.6 }],
+      },
+    },
   },
 };
