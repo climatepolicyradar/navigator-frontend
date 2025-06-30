a5fed4da3ebb0a030575ce9fbbf9216ca60af37f
Anna Pokorska <anna@climatepolicyradar.org>
2025-06-30 15:48:48 +0100

    Update test render function with missing Context wrappers

---
+++ tests/mocks/renderWithContext.tsx
@@ -0,0 +1,25 @@
+import { render } from "@testing-library/react";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+import { EnvConfigContext } from "@/context/EnvConfig";
+import { ThemeContext } from "@/context/ThemeContext";
+
+export const renderWithContext = (Component: React.ComponentType<any>, pageProps: any) => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: false,
+      },
+    },
+  });
+
+  render(
+    <QueryClientProvider client={queryClient}>
+      <ThemeContext.Provider value={{ theme: pageProps?.theme, themeConfig: pageProps?.themeConfig }}>
+        <EnvConfigContext.Provider value={pageProps?.envConfig}>
+          <Component {...pageProps} />
+        </EnvConfigContext.Provider>
+      </ThemeContext.Provider>
+    </QueryClientProvider>
+  );
+};
