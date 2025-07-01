06d41b98af95408f0c5f47c96b739a09e5bca830
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2025-07-01 10:25:25 +0100

    Fixed typo

--- lighthouserc.js
+++ lighthouserc.js
@@ -11,7 +11,7 @@ module.exports = {
     },
     assert: {
       assertions: {
-        // Lighthouse runs 3 times by default, so this checks each time the score is above the threshold.
+        // Lighthouse runs 3 times by default, so this checks each time whether the score is above the threshold.
         // If the highest result of the 3 runs is below the threshold, the build will fail [error].
         "categories:performance": ["error", { minScore: 0.4 }],
         "categories:accessibility": ["error", { minScore: 0.7 }],
