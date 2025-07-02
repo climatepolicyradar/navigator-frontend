ad2f142cda4fe252bc9d2ecf04b7bbf10150d5d2
Anna Pokorska <anna@climatepolicyradar.org>
2025-07-02 16:44:06 +0100

    Failing test for filtering search by region

--- tests/mocks/api/geographiesHandlers.ts
+++ tests/mocks/api/geographiesHandlers.ts
@@ -4,12 +4,28 @@ export const geographiesHandlers = [
   http.get("*/geographies/", () => {
     return HttpResponse.json([
       {
+        alpha_2: "AR",
+        alpha_3: "ARG",
+        name: "Argentina",
+        official_name: "Argentine Republic",
+        numeric: "032",
+        flag: "🇦🇷",
+      },
+      {
         alpha_3: "BLZ",
         display_value: "Belize",
         name: "Belize",
         slug: "belize",
         type: "country",
         value: "Belize",
+      },
+      {
+        alpha_2: "CU",
+        alpha_3: "CUB",
+        name: "Cuba",
+        official_name: "Republic of Cuba",
+        numeric: "192",
+        flag: "🇨🇺",
       },
     ]);
   }),
--- tests/mocks/repository.ts
+++ tests/mocks/repository.ts
@@ -43,6 +43,53 @@ const families = [
     continuation_token: null,
     prev_continuation_token: null,
   },
+  {
+    family_slug: "cuba-national-inventory-report-nir-2024_7430",
+    family_name: "Cuba National Inventory Report (NIR). 2024",
+    family_description: "<p>Cuba National Inventory Report (NIR). 2024</p>",
+    family_category: "UNFCCC",
+    family_date: "2025-03-31T00:00:00+00:00",
+    family_last_updated_date: "2025-04-01T00:00:00+00:00",
+    family_source: "UNFCCC",
+    corpus_import_id: "UNFCCC.corpus.i00000001.n0000",
+    corpus_type_name: "Intl. agreements",
+    family_geographies: ["CUB"],
+    family_metadata: {
+      author: ["Cuba"],
+      author_type: ["Party"],
+    },
+    family_title_match: true,
+    family_description_match: true,
+    total_passage_hits: 1,
+    family_documents: [],
+    continuation_token: null,
+    prev_continuation_token: null,
+  },
+  {
+    family_slug:
+      "technical-analysis-of-the-first-biennial-update-report-of-afghanistan-submitted-on-13-october-2019-summary-report-by-the-team-of-technical-experts_2bb5",
+    family_name:
+      "Technical analysis of the first biennial update report of Afghanistan submitted on 13 October 2019. Summary report by the team of technical experts",
+    family_description:
+      "Technical analysis of the first biennial update report of Afghanistan submitted on 13 October 2019. Summary report by the team of technical experts, Technical Analysis Summary Report from UNFCCC Secretariat in 2020",
+    family_category: "UNFCCC",
+    family_date: "2020-11-13T00:00:00+00:00",
+    family_last_updated_date: "2020-11-13T00:00:00+00:00",
+    family_source: "UNFCCC",
+    corpus_import_id: "UNFCCC.corpus.i00000001.n0000",
+    corpus_type_name: "Intl. agreements",
+    family_geographies: ["AFG"],
+    family_metadata: {
+      author: ["UNFCCC Secretariat"],
+      author_type: ["Non-Party"],
+    },
+    family_title_match: true,
+    family_description_match: true,
+    total_passage_hits: 1,
+    family_documents: [],
+    continuation_token: null,
+    prev_continuation_token: null,
+  },
 ];
 
 const getFilteredFamilies = (filters: any) => {
