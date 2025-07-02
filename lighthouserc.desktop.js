0867b9449b8c42521a28d8083069f498f3ddeb5b
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2025-07-02 15:26:19 +0100

    Update lighthouserc.desktop.js

--- lighthouserc.desktop.js
+++ lighthouserc.desktop.js
@@ -14,7 +14,7 @@ module.exports = {
         // Desktop typically has better performance, so we can set higher thresholds
         // Lighthouse runs 3 times by default, so this checks each time whether the score is above the threshold.
         // If the highest result of the 3 runs is below the threshold, the build will fail [error].
-        "categories:performance": ["error", { minScore: 0.85 }],
+        "categories:performance": ["error", { minScore: 0.6 }],
         "categories:accessibility": ["error", { minScore: 0.75 }],
         "categories:best-practices": ["error", { minScore: 0.9 }],
         "categories:seo": ["error", { minScore: 0.6 }],
