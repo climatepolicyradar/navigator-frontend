e5eafb5d29aef98dfa64e5cd01d5611ba1eb3027
Anna Pokorska <anna@climatepolicyradar.org>
2025-07-02 10:46:02 +0100

    Import individual handlers in server.js and delete extra handlers file

--- tests/mocks/handlers.ts
+++
@@ -1,5 +0,0 @@
-import { configHandlers } from "./api/configHandlers";
-import { geographiesHandlers } from "./api/geographiesHandlers";
-import { searchHandlers } from "./api/searchHandlers";
-
-export const handlers = [...configHandlers, ...geographiesHandlers, ...searchHandlers];
--- tests/mocks/server.ts
+++ tests/mocks/server.ts
@@ -1,6 +1,8 @@
 import { setupServer } from "msw/node";
 
-import { handlers } from "./handlers";
+import { configHandlers } from "./api/configHandlers";
+import { geographiesHandlers } from "./api/geographiesHandlers";
+import { searchHandlers } from "./api/searchHandlers";
 
 // This configures a request mocking server with the given request handlers.
-export const server = setupServer(...handlers);
+export const server = setupServer(...configHandlers, ...geographiesHandlers, ...searchHandlers);
