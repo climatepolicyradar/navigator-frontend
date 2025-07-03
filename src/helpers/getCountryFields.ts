aac1251527e5e391b028b4fdb3082b9b9c679edc
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2025-07-03 16:04:31 +0100

    Show 'new' countries as pills in applied filters

--- src/helpers/getCountryFields.ts
+++ src/helpers/getCountryFields.ts
@@ -1,21 +1,27 @@
-import { TGeography } from "@/types";
+import { TCountry } from "@/types";
 
-const findCountryObject = (countrySearch: string, dataSet: TGeography[]) => {
-  let country = dataSet.find((c) => c.value.toLowerCase() === countrySearch.toLowerCase());
-  if (!country) country = dataSet.find((c) => c.display_value.toLowerCase() === countrySearch.toLowerCase());
-  if (!country) country = dataSet.find((c) => c.slug.toLowerCase() === countrySearch.toLowerCase());
+const findCountryObject = (countrySearch: string, dataSet: TCountry[]) => {
+  if (dataSet.length === 0) return null;
+  if (!dataSet[0].alpha_3) return null; // FIXME Remove
+
+  let country = dataSet.find((c) => c.alpha_3.toLowerCase() === countrySearch.toLowerCase());
+  if (!country) country = dataSet.find((c) => c.alpha_2.toLowerCase() === countrySearch.toLowerCase());
+  if (!country) country = dataSet.find((c) => c.name.toLowerCase() === countrySearch.toLowerCase());
+  if (!country) country = dataSet.find((c) => c.official_name.toLowerCase() === countrySearch.toLowerCase());
   if (!country) return null;
   return country;
 };
 
-export const getCountrySlug = (countrySearch: string, dataSet: TGeography[]) => {
-  return findCountryObject(countrySearch, dataSet)?.slug;
+export const getCountrySlug = (countrySearch: string, dataSet: TCountry[]) => {
+  // FIXME: Remove references.
+  return findCountryObject(countrySearch, dataSet)?.alpha_3;
 };
 
-export const getCountryName = (countrySearch: string, dataSet: TGeography[]) => {
-  return findCountryObject(countrySearch, dataSet)?.display_value;
+export const getCountryName = (countrySearch: string, dataSet: TCountry[]) => {
+  // Official name is sometimes null.
+  return findCountryObject(countrySearch, dataSet)?.name;
 };
 
-export const getCountryCode = (countrySearch: string, dataSet: TGeography[]) => {
-  return findCountryObject(countrySearch, dataSet)?.value;
+export const getCountryCode = (countrySearch: string, dataSet: TCountry[]) => {
+  return findCountryObject(countrySearch, dataSet)?.alpha_3;
 };
