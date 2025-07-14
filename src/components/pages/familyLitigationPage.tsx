3aad3482cf7e7556d2f6b56a3c3644d361d2423f
Jessica Kelsall <jessica.kelsall@climatepolicyradar.org>
2025-07-14 13:27:42 +0100

    Display raw family data on family page

--- src/components/pages/familyLitigationPage.tsx
+++ src/components/pages/familyLitigationPage.tsx
@@ -18,18 +18,7 @@ export const FamilyLitigationPage = ({ family, theme, themeConfig }: IProps) => 
       <Columns>
         <ContentsSideBar items={FAMILY_PAGE_SIDE_BAR_ITEMS_SORTED} stickyClasses="!top-[72px] pt-3 cols-2:pt-6 cols-3:pt-8" />
         <main className="py-3 cols-2:py-6 cols-3:py-8 cols-3:col-span-2 cols-4:col-span-3">
-          <div className="mb-19">
-            <h1 className="text-4xl leading-tight font-[640] text-text-primary">{family.title}</h1>
-          </div>
-          <div className="flex flex-col gap-24">
-            {/* Each section should eventually be its own component */}
-            {FAMILY_PAGE_SIDE_BAR_ITEMS_SORTED.map((item) => (
-              <section key={item.id} id={item.id} className="scroll-m-21 cols-2:scroll-m-24 cols-3:scroll-m-26">
-                <h2 className="text-2xl leading-tight font-[640] text-text-primary mb-4">{item.display}</h2>
-                <div className="h-[500px] bg-[rgb(230,230,230)] rounded-xl" />
-              </section>
-            ))}
-          </div>
+          <pre className="w-full max-h-[700px] bg-surface-ui text-sm text-text-tertiary overflow-scroll">{JSON.stringify(family, null, 2)}</pre>
         </main>
       </Columns>
     </Layout>
