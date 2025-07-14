8e0f8c26a5be35a3b61fd66f2e4dfb507954dd81
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2025-07-14 10:22:41 +0100

    Fix geographies handler

--- tests/mocks/api/geographiesHandlers.ts
+++ tests/mocks/api/geographiesHandlers.ts
@@ -5,11 +5,45 @@ export const geographiesHandlers = [
     return HttpResponse.json([
       {
         alpha_3: "BLZ",
-        display_value: "Belize",
+        official_name: null,
         name: "Belize",
-        slug: "belize",
-        type: "country",
-        value: "Belize",
+        alpha_2: "BZ",
+      },
+      {
+        alpha_3: "FRA",
+        official_name: "French Republic",
+        name: "France",
+        alpha_2: "FR",
+      },
+      {
+        alpha_3: "ESP",
+        official_name: "Kingdom of Spain",
+        name: "Spain",
+        alpha_2: "ES",
+      },
+      {
+        alpha_3: "ZAF",
+        official_name: "Republic of South Africa",
+        name: "South Africa",
+        alpha_2: "ZA",
+      },
+      {
+        alpha_3: "CAF",
+        official_name: null,
+        name: "Central African Republic",
+        alpha_2: "CF",
+      },
+      {
+        alpha_3: "SDN",
+        official_name: "Republic of Sudan",
+        name: "Sudan",
+        alpha_2: "SD",
+      },
+      {
+        alpha_3: "SSD",
+        official_name: "Republic of South Sudan",
+        name: "South Sudan",
+        alpha_2: "SS",
       },
     ]);
   }),
