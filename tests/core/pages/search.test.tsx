d350272599ec26043e6215b137429218ae1fdd1d
Anna Pokorska <anna@climatepolicyradar.org>
2025-07-14 13:11:22 +0100

    Add test for topics slideout

--- tests/core/pages/search.test.tsx
+++ tests/core/pages/search.test.tsx
@@ -98,4 +98,40 @@ describe("SearchPage", async () => {
       )
     ).not.toBeInTheDocument();
   });
+
+  it("shows topics filters when topics slideout is opened", async () => {
+    const search_props = {
+      envConfig: {
+        BACKEND_API_URL: process.env.BACKEND_API_URL,
+        CONCEPTS_API_URL: process.env.CONCEPTS_API_URL,
+      },
+      theme: "cpr",
+      themeConfig: {
+        documentCategories: ["All"],
+        features: { knowledgeGraph: true, searchFamilySummary: false },
+        metadata: [
+          {
+            key: "search",
+            title: "Law and Policy Search",
+          },
+        ],
+      },
+      conceptsData: [{}],
+    };
+    // @ts-ignore
+    renderWithAppContext(Search, search_props);
+
+    expect(await screen.findByRole("heading", { level: 2, name: "Search results" })).toBeInTheDocument();
+
+    const topicsFilterControl = await screen.findByText(/Topics/);
+
+    expect(topicsFilterControl).toBeInTheDocument();
+    // We have to wrap our user interactions in act() here due to some async updates that happen in the component,
+    // like animations that were causing warnings in the console.
+    await act(async () => {
+      await userEvent.click(topicsFilterControl);
+    });
+
+    expect(await screen.findByText(/Find mentions of topics/i));
+  });
 });
