85ed05e114ec7bf11e49a0ea7ffa81a57fb4dd68
James Gorrie <james.gorrie@climatepolicyradar.org>
2025-07-15 11:15:34 +0100

    wip

--- src/components/layouts/Main.tsx
+++ src/components/layouts/Main.tsx
@@ -10,7 +10,7 @@ import { getCanonicalUrl } from "@/utils/getCanonicalUrl";
 import { getPageDescription } from "@/utils/getPageDescription";
 import { getPageTitle } from "@/utils/getPageTitle";
 
-const Wrapper = dynamic<{ children: ReactNode }>(() => import(`../../themes/${process.env.THEME}/layouts/main.tsx`));
+const Wrapper = dynamic<{ children: ReactNode }>(() => import(`../../../themes/${process.env.THEME}/layouts/main.tsx`));
 
 interface IProps {
   title?: string;
