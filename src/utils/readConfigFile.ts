574823cb4ddfc5ed5ef14e83a180bd4640dc7f6b
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2025-07-15 10:56:15 +0100

    Make relative imports

--- src/utils/readConfigFile.ts
+++ src/utils/readConfigFile.ts
@@ -1,6 +1,6 @@
 import { TThemeConfig } from "@/types";
 
 export const readConfigFile = async (theme: string): Promise<TThemeConfig> => {
-  const configModule = await import(`/themes/${theme}/config.ts`);
+  const configModule = await import(`../../themes/${theme}/config.ts`);
   return configModule.default;
 };
