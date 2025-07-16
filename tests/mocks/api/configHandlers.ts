c72a453e635c779121472bfd13079543de783ff9
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2025-07-16 15:19:02 +0100

    fix formatting

--- tests/mocks/api/configHandlers.ts
+++ tests/mocks/api/configHandlers.ts
@@ -193,7 +193,7 @@ export const configHandlers = [
                 parent_id: 12,
               },
               // Subdivisions
-              children: [], 
+              children: [],
             },
           ],
         },
