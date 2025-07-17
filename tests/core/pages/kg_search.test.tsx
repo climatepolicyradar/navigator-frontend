9d953ffa2e5728fc2dea980d9fbe12b9ea3db9cd
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2025-07-17 17:09:29 +0100

    Squashed commit of the following:
    commit 3783ad504f19c5c5d6f63e84c765b9f71e5b1325
    Merge: 1e6a767 8ac5345
    Author: Katy Baulch <46493669+katybaulch@users.noreply.github.com>
    Date:   Thu Jul 17 17:07:50 2025 +0100
    
        Merge branch 'main' into update-search-tests
    
    commit 1e6a767dbd07af1eb3399b4a322322c4d55ae468
    Author: Katy Baulch <46493669+katybaulch@users.noreply.github.com>
    Date:   Thu Jul 17 16:49:38 2025 +0100
    
        Add sort and search option unit tests
    
    commit 8ac5345be7982d1cdc6dccf6317d1d00103290df
    Author: James Gorrie <jamesgorrie@users.noreply.github.com>
    Date:   Thu Jul 17 14:59:37 2025 +0100
    
        feat: add principal laws and jurisdictions to litigation filter slideouts (#660)
    
    commit b906f2f90037444a9f56e3ee97bfe7ca6565a210
    Author: Katy Baulch <46493669+katybaulch@users.noreply.github.com>
    Date:   Thu Jul 17 14:53:23 2025 +0100
    
        Split up KG search tests from core search
    
    commit f2e10281744bf5002ab3a73aca793f09e0433dbe
    Author: Katy Baulch <46493669+katybaulch@users.noreply.github.com>
    Date:   Thu Jul 17 12:21:29 2025 +0100
    
        Only show topics filter if KG FF on

---
+++ tests/core/pages/kg_search.test.tsx
@@ -0,0 +1,90 @@
+import { act, screen, within } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import { renderWithAppContext } from "@/mocks/renderWithAppContext";
+import Search from "@/pages/search";
+import { mockFeatureFlagsWithConcepts } from "@/mocks/featureFlags";
+
+const baseSearchProps = {
+  envConfig: {
+    BACKEND_API_URL: process.env.BACKEND_API_URL,
+    CONCEPTS_API_URL: process.env.CONCEPTS_API_URL,
+  },
+  theme: "cpr",
+  themeConfig: {
+    documentCategories: ["All"],
+    features: { knowledgeGraph: true, searchFamilySummary: false },
+    metadata: [
+      {
+        key: "search",
+        title: "Law and Policy Search",
+      },
+    ],
+  },
+  featureFlags: mockFeatureFlagsWithConcepts,
+};
+
+describe("SearchPage", async () => {
+  it("shows search onboarding info when no filters applied", async () => {
+    const search_props = { ...baseSearchProps };
+    // @ts-ignore
+    renderWithAppContext(Search, search_props);
+
+    expect(await screen.findByText("Get better results")).toBeInTheDocument();
+    expect(screen.getByText(/You are currently viewing all of the documents in our database/)).toBeInTheDocument();
+    expect(screen.getByText(/Topics filter/)).toBeInTheDocument();
+  });
+
+  it("hides search onboarding info when filters are applied", async () => {
+    const search_props = { ...baseSearchProps, searchParams: { q: "climate policy" } };
+    // @ts-ignore
+    renderWithAppContext(Search, search_props);
+
+    expect(screen.queryByText("Get better results")).not.toBeInTheDocument();
+    expect(screen.queryByText(/You are currently viewing all of the documents in our database/)).not.toBeInTheDocument();
+  });
+
+  it("filters search results by topic", async () => {
+    const search_props = {
+      ...baseSearchProps,
+      conceptsData: [
+        {
+          alternative_labels: [],
+          description: "test concept 1",
+          has_subconcept: [],
+          negative_labels: [],
+          preferred_label: "child topic 1",
+          recursive_subconcept_of: [],
+          related_concepts: [],
+          subconcept_of: [],
+          wikibase_id: "1",
+        },
+      ],
+    };
+    // @ts-ignore
+    renderWithAppContext(Search, search_props);
+
+    expect(await screen.findByRole("heading", { level: 2, name: "Search results" })).toBeInTheDocument();
+
+    const topicsFilterControl = await screen.findByRole("button", { name: /Topics/ });
+
+    expect(topicsFilterControl).toBeInTheDocument();
+    // We have to wrap our user interactions in act() here due to some async updates that happen in the component,
+    // like animations that were causing warnings in the console.
+    await act(async () => {
+      await userEvent.click(topicsFilterControl);
+    });
+
+    expect(await screen.findByText(/Find mentions of topics/i));
+
+    expect(screen.getByText(/Parent topic/)).toBeInTheDocument();
+    const topic = screen.getByRole("checkbox", { name: "Child topic 1" });
+
+    await act(async () => {
+      await userEvent.click(topic);
+    });
+
+    expect(screen.getByRole("link", { name: "Family with topic 1" })).toBeInTheDocument();
+    expect(screen.queryByRole("link", { name: "Family with topic 2" })).not.toBeInTheDocument();
+  });
+});
