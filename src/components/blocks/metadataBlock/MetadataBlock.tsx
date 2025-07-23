3c2666e83a45b2091bad7e6f676993e0b8530467
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2025-07-23 15:40:48 +0100

    Render props, update story

--- src/components/blocks/metadataBlock/MetadataBlock.tsx
+++ src/components/blocks/metadataBlock/MetadataBlock.tsx
@@ -1,45 +1,28 @@
+import { ReactNode } from "react";
+
 import { Section } from "@/components/molecules/section/Section";
 
-export const MetadataBlock = () => {
+interface IMetaData {
+  label: ReactNode;
+  value: ReactNode;
+}
+
+interface IProps {
+  metadata: IMetaData[];
+}
+
+export const MetadataBlock = ({ metadata }: IProps) => {
   return (
-    <Section title="About this case">
+    <Section title="About this case" id="metadata-block">
       <div className="rounded border border-border-light p-12">
         <div className="grid gap-3">
-          <div className="grid md:gap-3 md:grid-cols-(--info-grid)">
-            <div className="text-text-primary font-semibold">Filing year</div>
-            <div>2025</div>
-          </div>
-          <div className="grid md:gap-3 md:grid-cols-(--info-grid)">
-            <div className="text-text-primary font-semibold">Geography</div>
-            <div>
-              <a href="" className="underline">
-                United States
-              </a>
+          {metadata.length === 0 && <div className="text-text-secondary">Sorry, there is no data available at this time.</div>}
+          {metadata.map((item, index) => (
+            <div key={index} className="grid md:gap-3 md:grid-cols-(--info-grid)">
+              <div className="text-text-primary font-semibold">{item.label}</div>
+              <div>{item.value}</div>
             </div>
-          </div>
-          <div className="grid md:gap-3 md:grid-cols-(--info-grid)">
-            <div className="text-text-primary font-semibold">Principal law</div>
-            <div>
-              <div className="grid">
-                <span>Article I (U.S. Constitution)</span>
-                <span>Administrative Procedure Act (APA)</span>
-                <span>State Law—Conversion</span>
-                <span>Take Care Clause</span>
-                <span>Separation of Powers Doctrine</span>
-                <span>Antideficiency Act</span>
-                <span>Inflation Reduction Act of 2022</span>
-                <span>Spending Clause</span>
-                <span>Appropriations Clause</span>
-                <span>Replevin</span>
-                <span>Appointments Clause</span>
-                <span>Impoundment Control Act</span>
-                <span>Uniform Grant Guidance Regulations</span>
-                <span>Legislative Vesting Clause</span>
-                <span>Fifth Amendment—Due Process</span>
-                <span>Contract Law</span>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </Section>
