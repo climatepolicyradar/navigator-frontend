f4501e29489f7a36b3ee848b86a9be14f73f0e87
Jessica Kelsall <jessica.kelsall@climatepolicyradar.org>
2025-07-23 11:59:19 +0100

    APP-908: Text block component (#664)
    * HTML rendered from strings only has margins between paragraphs
    
    * Simplified Section component
    
    * Delete ShowMore component
    
    * Add TextBlock component
    
    * Add to collection & family pages
    
    * Quick PR comment fixes

---
+++ src/components/blocks/textBlock/TextBlock.tsx
@@ -0,0 +1,44 @@
+import { ReactNode, useEffect, useRef, useState } from "react";
+
+import { Section } from "@/components/molecules/section/Section";
+import { joinTailwindClasses } from "@/utils/tailwind";
+
+interface IProps {
+  children: ReactNode;
+  maxHeight?: number;
+}
+
+export const TextBlock = ({ children, maxHeight = 150 }: IProps) => {
+  const contentRef = useRef(null);
+  const [contentHeight, setContentHeight] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    setContentHeight(contentRef.current.clientHeight);
+  }, [setContentHeight]);
+
+  const onToggle = () => {
+    setIsOpen((currentIsOpen) => !currentIsOpen);
+  };
+
+  const contentIsOverflowing = contentHeight > maxHeight;
+  const containerClasses = joinTailwindClasses("relative", isOpen ? "overflow-visible" : "overflow-hidden");
+
+  return (
+    <Section id="section-summary" title="Summary">
+      <div className="relative">
+        <div className={containerClasses} style={{ maxHeight: isOpen ? undefined : maxHeight }}>
+          {/* DOM element to measure height of */}
+          <div ref={contentRef}>{children}</div>
+        </div>
+        {contentIsOverflowing && (
+          <div className="pt-4">
+            <button type="button" onClick={onToggle} className="text-sm text-text-tertiary hover:underline">
+              {isOpen ? "- View less" : "+ View more"}
+            </button>
+          </div>
+        )}
+      </div>
+    </Section>
+  );
+};
