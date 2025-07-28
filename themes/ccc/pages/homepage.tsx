7b9645c048bcb63896461be6c706a9b80753ac96
Anna Pokorska <anna@climatepolicyradar.org>
2025-07-28 17:06:09 +0100

    Add world map to CCC homepage

--- themes/ccc/pages/homepage.tsx
+++ themes/ccc/pages/homepage.tsx
@@ -1,8 +1,15 @@
+import dynamic from "next/dynamic";
+
 import Header from "@/ccc/components/Header";
 import { Hero } from "@/ccc/components/Hero";
 import { APP_NAME, PAGE_DESCRIPTION } from "@/ccc/constants/pageMetadata";
 import Layout from "@/components/layouts/LandingPage";
+import { FullWidth } from "@/components/panels/FullWidth";
 
+const WorldMap = dynamic(() => import("@/components/map/WorldMap"), {
+  loading: () => <p>Loading world map...</p>,
+  ssr: false,
+});
 interface IProps {
   handleSearchInput: (term: string, filter?: string, filterValue?: string) => void;
   searchInput: string;
@@ -17,6 +24,9 @@ const LandingPage = ({ handleSearchInput, searchInput }: IProps) => {
           <Hero handleSearchInput={handleSearchInput} searchInput={searchInput} />
         </div>
       </main>
+      <FullWidth extraClasses="hidden my-6 md:block">
+        <WorldMap showLitigation />
+      </FullWidth>
     </Layout>
   );
 };
