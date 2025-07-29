d8248eaf7fe9c150c55d6e5907fd595760a20372
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2025-07-29 15:49:50 +0100

    Display correct date and geo link

--- src/utils/getFamilyMetadata.tsx
+++ src/utils/getFamilyMetadata.tsx
@@ -1,27 +1,44 @@
-import { ReactNode } from "react";
+import { Fragment } from "react";
 
-import { TFamilyPage, IMetadata } from "@/types";
+import { LinkWithQuery } from "@/components/LinkWithQuery";
+import { getCountryName, getCountrySlug } from "@/helpers/getCountryFields";
+import { TFamilyPage, IMetadata, TGeography } from "@/types";
 
 // Format the family metadata into a shape suitable for the MetadataBlock component
-export const getFamilyMetadata = (family: TFamilyPage): IMetadata[] => {
+export const getFamilyMetadata = (family: TFamilyPage, countries: TGeography[]): IMetadata[] => {
   const familyMetadata = [];
 
   // TODO: handle more categories and their specific metadata later
-  if (family.category === "Litigation") {
-    familyMetadata.push(...getLitigationMetaData(family));
+  if (family.category.toLowerCase() === "litigation") {
+    familyMetadata.push(...getLitigationMetaData(family, countries));
   }
 
   return familyMetadata;
 };
 
-function getLitigationMetaData(family: TFamilyPage): IMetadata[] {
+function getLitigationMetaData(family: TFamilyPage, countries: TGeography[]): IMetadata[] {
   const metadata = [];
 
-  if (family.published_date) {
-    const year = new Date(family.published_date).getFullYear();
+  const filingYearEvent = family.events.find((event) => event.event_type === "Filing Year For Action");
+  if (filingYearEvent) {
+    const year = new Date(filingYearEvent.date).getFullYear();
     metadata.push({
       label: "Filing year",
       value: year,
+    });
+  }
+
+  if (family.geographies.length > 0) {
+    metadata.push({
+      label: "Geography",
+      value: family.geographies.map((geo, index) => (
+        <Fragment key={geo}>
+          {index > 0 && getCountrySlug(geo, countries) && " → "}
+          <LinkWithQuery key={geo} href={`/geographies/${getCountrySlug(geo, countries)}`} className="underline">
+            {getCountryName(geo, countries)}
+          </LinkWithQuery>
+        </Fragment>
+      )),
     });
   }
 
@@ -46,14 +63,7 @@ function getLitigationMetaData(family: TFamilyPage): IMetadata[] {
     });
   }
 
-  if (family.metadata.core_object) {
-    metadata.push({
-      label: "Core object",
-      value: <div className="grid">{family.metadata.core_object?.map((label) => <span key={label}>{label}</span>) || "N/A"}</div>,
-    });
-  }
-
-  if (family.metadata.original_case_name) {
+  if (family.metadata.original_case_name.length > 0) {
     metadata.push({
       label: "Original case name",
       value: <div className="grid">{family.metadata.original_case_name?.map((label) => <span key={label}>{label}</span>) || "N/A"}</div>,
