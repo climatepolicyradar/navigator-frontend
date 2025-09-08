8c3daf6171b53f1b8a13d8d2233fbcd5c368b9d5
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2025-09-08 12:19:08 +0100

    Make checkbox light and dark

--- src/components/filters/ExactMatch.tsx
+++ src/components/filters/ExactMatch.tsx
@@ -1,7 +1,7 @@
-import { ChangeEvent } from "react";
+import { ChangeEvent, useContext } from "react";
 
 import { QUERY_PARAMS } from "@/constants/queryParams";
-import useGetTheme from "@/hooks/useGetTheme";
+import { ThemeContext } from "@/context/ThemeContext";
 
 const ExactMatch = ({ id, handleSearchChange, checked, landing = false }) => {
   const handleClick = (e: ChangeEvent<HTMLInputElement>) => {
@@ -9,9 +9,8 @@ const ExactMatch = ({ id, handleSearchChange, checked, landing = false }) => {
     handleSearchChange(QUERY_PARAMS.exact_match, isChecked);
   };
 
-  const { theme } = useGetTheme();
-  const themeClass = theme === "ccc" ? "checkbox-dark" : "checkbox-light";
-
+  const { theme } = useContext(ThemeContext);
+  const themeClass = landing ? (theme === "ccc" ? "checkbox-dark" : "checkbox-light") : "checkbox-dark";
   return (
     <div className={`${landing ? "landing" : ""}`}>
       <label className={`${themeClass} flex items-center cursor-pointer`} htmlFor={id}>
@@ -22,7 +21,9 @@ const ExactMatch = ({ id, handleSearchChange, checked, landing = false }) => {
           checked={checked}
           onChange={handleClick}
         />
-        <span className={`${landing ? "text-lg" : "text-sm"} ${theme === "ccc" ? "text-text-primary" : "text-text-light"} pl-2 leading-none`}>
+        <span
+          className={`${landing ? "text-lg" : "text-sm"} ${landing ? (theme === "ccc" ? "text-text-primary" : "text-white") : "text-text-primary"} pl-2 leading-none`}
+        >
           Only show exact matches
         </span>
       </label>
