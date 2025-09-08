22044d4ae929e198a85540530f944aeae9875d72
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2025-09-08 14:03:26 +0100

    Bump storybook to v9

--- src/components/molecules/newFeatures/NewFeatureCard.stories.tsx
+++ src/components/molecules/newFeatures/NewFeatureCard.stories.tsx
@@ -1,4 +1,4 @@
-import { Meta, StoryObj } from "@storybook/react";
+import { Meta, StoryObj } from "@storybook/nextjs";
 
 import { NEW_FEATURES } from "@/constants/newFeatures";
 import { NewFeatureContext } from "@/context/NewFeatureContext";
--- src/components/organisms/contentsSideBar/ContentsSideBar.stories.tsx
+++ src/components/organisms/contentsSideBar/ContentsSideBar.stories.tsx
@@ -1,4 +1,4 @@
-import { Meta, StoryObj } from "@storybook/react";
+import { Meta, StoryObj } from "@storybook/nextjs";
 
 import { Columns } from "@/components/atoms/columns/Columns";
 import { FAMILY_PAGE_SIDE_BAR_ITEMS } from "@/constants/sideBarItems";
