d057080f62c6e86509b4f54e39bcc68d1cf38098
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2025-09-08 12:13:21 +0100

    Fix CCC exact match styling

--- src/components/filters/ExactMatch.tsx
+++ src/components/filters/ExactMatch.tsx
@@ -1,6 +1,7 @@
 import { ChangeEvent } from "react";
 
 import { QUERY_PARAMS } from "@/constants/queryParams";
+import useGetTheme from "@/hooks/useGetTheme";
 
 const ExactMatch = ({ id, handleSearchChange, checked, landing = false }) => {
   const handleClick = (e: ChangeEvent<HTMLInputElement>) => {
@@ -8,17 +9,22 @@ const ExactMatch = ({ id, handleSearchChange, checked, landing = false }) => {
     handleSearchChange(QUERY_PARAMS.exact_match, isChecked);
   };
 
+  const { theme } = useGetTheme();
+  const themeClass = theme === "ccc" ? "checkbox-dark" : "checkbox-light";
+
   return (
     <div className={`${landing ? "landing" : ""}`}>
-      <label className="checkbox-input flex items-center cursor-pointer" htmlFor={id}>
+      <label className={`${themeClass} flex items-center cursor-pointer`} htmlFor={id}>
         <input
-          className={`${landing ? "text-indigo-600/0" : "text-white"} border-white rounded`}
+          className={`${landing ? "text-indigo-600/0" : "text-text-light"} ${themeClass} rounded`}
           id={id}
           type="checkbox"
           checked={checked}
           onChange={handleClick}
         />
-        <span className={`${landing ? "text-lg text-white" : "text-sm"} pl-2 leading-none`}>Only show exact matches</span>
+        <span className={`${landing ? "text-lg" : "text-sm"} ${theme === "ccc" ? "text-text-primary" : "text-text-light"} pl-2 leading-none`}>
+          Only show exact matches
+        </span>
       </label>
     </div>
   );
