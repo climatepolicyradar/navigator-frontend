72c75fdd508255fa5f35040c6e62232fd4c4188e
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2025-09-10 09:56:12 +0100

    Add title text to SVG

--- src/components/atoms/icon/Icon.tsx
+++ src/components/atoms/icon/Icon.tsx
@@ -382,6 +382,7 @@ const allIcons: Record<TIconName, (IconProps) => JSX.Element> = {
       stroke-linejoin="round"
       style={{ width: `${width}px`, height: `${height}px` }}
     >
+      <title>House Icon</title>
       <path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" />
       <path d="M3 10a2 2 0 0 1 .709-1.528l7-6a2 2 0 0 1 2.582 0l7 6A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
     </svg>
