dbf1ca5fd02773605d602aa9bfb0b3672c2450e6
Jessica Kelsall <jessica.kelsall@climatepolicyradar.org>
2025-09-11 15:10:49 +0100

    Show placeholder text in DocumentsBlock if there are no document events

--- src/components/blocks/documentsBlock/DocumentsBlock.tsx
+++ src/components/blocks/documentsBlock/DocumentsBlock.tsx
@@ -45,35 +45,40 @@ export const DocumentsBlock = ({ family, matchesFamily, matchesStatus, showMatch
     [category, family]
   );
 
+  const hasDocumentsToDisplay = tableRows.length > 0;
+
   const onToggleChange = (toggleValue: string[]) => {
     setView(toggleValue[0]);
   };
 
   return (
     <Section block="documents" title="Documents">
-      <Card variant="outlined" className="flex flex-col rounded-lg !p-5">
-        {/* Controls */}
-        <div className="pb-6">
-          <ToggleGroup value={[view]} onValueChange={onToggleChange}>
-            <Toggle value="table">Table</Toggle>
-            <Toggle value="cards">Cards</Toggle>
-          </ToggleGroup>
-        </div>
-
-        {/* Cards */}
-        {view === "cards" && (
-          <div className="flex gap-5 items-stretch overflow-x-auto pb-2">
-            {cards.map((card) => (
-              <EntityCard key={card.href} {...card} />
-            ))}
+      {hasDocumentsToDisplay && (
+        <Card variant="outlined" className="flex flex-col rounded-lg !p-5">
+          {/* Controls */}
+          <div className="pb-6">
+            <ToggleGroup value={[view]} onValueChange={onToggleChange}>
+              <Toggle value="table">Table</Toggle>
+              <Toggle value="cards">Cards</Toggle>
+            </ToggleGroup>
           </div>
-        )}
 
-        {/* Table */}
-        {view === "table" && (
-          <InteractiveTable<TEventTableColumnId> columns={tableColumns} rows={tableRows} defaultSort={{ column: "date", ascending: false }} />
-        )}
-      </Card>
+          {/* Cards */}
+          {view === "cards" && (
+            <div className="flex gap-5 items-stretch overflow-x-auto pb-2">
+              {cards.map((card) => (
+                <EntityCard key={card.href} {...card} />
+              ))}
+            </div>
+          )}
+
+          {/* Table */}
+          {view === "table" && (
+            <InteractiveTable<TEventTableColumnId> columns={tableColumns} rows={tableRows} defaultSort={{ column: "date", ascending: false }} />
+          )}
+        </Card>
+      )}
+      {!hasDocumentsToDisplay && <p className="italic">There are no documents to display yet. Check back later.</p>}
     </Section>
   );
 };
