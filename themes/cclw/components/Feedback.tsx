582d69ca6cb1c52ce0d92fe6d194078fb1b8ed9e
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2025-09-16 15:59:25 +0100

    Revert "Delete Feedback.tsx"
    This reverts commit ed4448b820440e5d9ee56a4dafaf2d629595d159.

---
+++ themes/cclw/components/Feedback.tsx
@@ -0,0 +1,23 @@
+import { ExternalLink } from "@/components/ExternalLink";
+
+export const Feedback = () => {
+  return (
+    <div data-cy="feedback" className="bg-cclw-light rounded-3xl p-4">
+      <div className="font-medium text-lg">Feedback</div>
+      <ul className="list-disc pl-4">
+        <li className="mb-2">
+          To report a problem, email us at{" "}
+          <ExternalLink url="mailto:support@climatepolicyradar.org" className="underline text-blue-600 hover:text-blue-800">
+            support@climatepolicyradar.org
+          </ExternalLink>
+        </li>
+        <li className="mb-2">
+          Spotted missing or inaccurate data?{" "}
+          <ExternalLink url="https://form.jotform.com/233294135296359" className="underline text-blue-600 hover:text-blue-800">
+            Let us know
+          </ExternalLink>
+        </li>
+      </ul>
+    </div>
+  );
+};
