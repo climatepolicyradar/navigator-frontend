1d8b3e6e7a4be3dd6c88d6f36337f000516f0b2b
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2025-09-23 12:22:26 +0100

    Adjust padding on smaller screens

--- themes/ccc/components/Feature.tsx
+++ themes/ccc/components/Feature.tsx
@@ -19,7 +19,7 @@ export const Feature = ({ heading, subheading, image, imageAlt, children }: IPro
       heading={heading || ""}
       extraClasses="h-full w-full"
       headingClasses="text-text-brand-darker text-2xl font-bold"
-      paddingClasses="py-11 px-11"
+      paddingClasses="p-4 md:p-6 lg:p-10"
     >
       {subheading && (
         <Heading level={3} extraClasses="!text-2xl !text-text-tertiary !-mt-2 !mb-6 !font-bold">
