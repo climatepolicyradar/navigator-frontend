76ca4d6736814229fc2f6021a55290b096965716
Jessica Kelsall <jessica.kelsall@climatepolicyradar.org>
2025-10-06 11:49:20 +0100

    Refactor sitemap to exclude invalid geography pages

--- src/pages/sitemap.txt.ts
+++ src/pages/sitemap.txt.ts
@@ -1,37 +1,39 @@
 import { ApiClient } from "@/api/http-common";
+import { EXCLUDED_ISO_CODES, INCLUDED_GEO_TYPES } from "@/constants/geography";
 import { TDataNode, TGeography } from "@/types";
 
-function Sitemap() {}
+// Determines which geographies to include/exclude
+const geographyFilter = (geography: TDataNode<TGeography>): boolean => {
+  if (EXCLUDED_ISO_CODES.includes(geography.node.value)) return false;
+  if (!INCLUDED_GEO_TYPES.includes(geography.node.type)) return false;
+  return true;
+};
 
-function extractGeographySlugs(config: TDataNode<TGeography>): string[] {
-  const children_slugs: string[] = config.children.flatMap((node): string[] => extractGeographySlugs(node));
-  return [config.node.slug].concat(children_slugs);
-}
-
-async function fetchGeographies(): Promise<TDataNode<TGeography>[]> {
-  const client = new ApiClient();
-  const { data: data } = await client.getConfig();
-  return data.geographies;
-}
-
-async function getGeographyIds(): Promise<string[]> {
-  const geographyData: TDataNode<TGeography>[] = await fetchGeographies();
-  return geographyData.flatMap((item: TDataNode<TGeography>) => extractGeographySlugs(item));
-}
-
-async function getGeographyPages(res: any, hostname: string): Promise<string[]> {
-  const slug_list: string[] = await getGeographyIds();
-  return slug_list.map((geo_slug: string) => `${hostname ?? ""}/geographies/${geo_slug}`);
-}
+// Recursively transform node structure into flat list of geo slugs
+const extractGeographySlugs = (config: TDataNode<TGeography>): string[] => {
+  const childrenSlugs: string[] = config.children.flatMap((node): string[] => extractGeographySlugs(node));
+  return (geographyFilter(config) ? [config.node.slug] : []).concat(childrenSlugs);
+};
 
 export async function getServerSideProps({ res }) {
-  res.setHeader("Content-Type", "text/plain");
-  res.write((await getGeographyPages(res, process.env.HOSTNAME)).join("\n"));
+  const client = new ApiClient();
+  const {
+    data: { geographies: geographiesData },
+  } = await client.getConfig();
+
+  const geographySlugs = geographiesData.flatMap((item) => extractGeographySlugs(item));
+  const urls = geographySlugs.map((slug) => `${process.env.HOSTNAME ?? ""}/geographies/${slug}`);
+
+  res.setHeader("Content-Type", "application/JSON");
+  res.write(urls.join("\n"));
   res.end();
 
   return {
     props: {},
   };
 }
+
+// Needed so Next.js recognises this as a page
+const Sitemap = () => {};
 
 export default Sitemap;
