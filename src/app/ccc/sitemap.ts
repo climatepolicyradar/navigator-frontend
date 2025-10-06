2731fc9c3a6c81bedf4a90c6d39c340a3fab0f63
Jessica Kelsall <jessica.kelsall@climatepolicyradar.org>
2025-10-06 13:33:04 +0100

    Remove old sitemap

--- src/app/ccc/sitemap.ts
+++ src/app/ccc/sitemap.ts
@@ -1,7 +1,19 @@
 import type { MetadataRoute } from "next";
 
 import { ApiClient } from "@/api/http-common";
-import { extractGeographySlugs } from "@/pages/sitemap.txt";
+import { EXCLUDED_ISO_CODES, INCLUDED_GEO_TYPES } from "@/constants/geography";
+import { TDataNode, TGeography } from "@/types";
+
+// Recursively transform node structure into flat list of geo slugs
+export const extractGeographySlugs = (config: TDataNode<TGeography>): string[] => {
+  const childrenSlugs: string[] = config.children.flatMap((node): string[] => extractGeographySlugs(node));
+
+  if (EXCLUDED_ISO_CODES.includes(config.node.value) || !INCLUDED_GEO_TYPES.includes(config.node.type)) {
+    return childrenSlugs;
+  } else {
+    return [config.node.slug, ...childrenSlugs];
+  }
+};
 
 export const dynamic = "force-dynamic";
 
