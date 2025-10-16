3e73d6191e366ea5306b823d5df20c559eeea149
Jessica Kelsall <jessica.kelsall@climatepolicyradar.org>
2025-10-16 14:06:55 +0100

    Breadcrumbs refactor & design pass

--- src/components/breadcrumbs/Breadcrumbs.tsx
+++ src/components/breadcrumbs/Breadcrumbs.tsx
@@ -1,13 +1,11 @@
 import Link from "next/link";
-import React, { useContext } from "react";
+import React, { ReactNode, useContext } from "react";
 
 import { LinkWithQuery } from "@/components/LinkWithQuery";
-import { Icon } from "@/components/atoms/icon/Icon";
+import { Columns } from "@/components/atoms/columns/Columns";
 import { ThemeContext } from "@/context/ThemeContext";
-import { TTheme } from "@/types";
+import { useText } from "@/hooks/useText";
 import { isSystemGeo } from "@/utils/isSystemGeo";
-
-const BREADCRUMB_MAXLENGTH = 50;
 
 type TBreadcrumbLink = {
   label: string | React.ReactNode;
@@ -27,47 +25,31 @@ interface IProps {
 
 interface IBreadCrumb extends TBreadcrumbLink {
   isHome?: boolean;
-  theme?: TTheme;
 }
 
-const BreadCrumb = ({ last = false, label = null, href = null, cy = "", isHome = false, theme }: IBreadCrumb) => {
-  // Don't render if label is empty, null, or undefined (unless it's the home icon)
+const BreadCrumb = ({ last = false, label = null, href = null, cy = "", isHome = false }: IBreadCrumb) => {
+  const { getText } = useText();
+
+  // Don't render if label is empty, null, or undefined (unless it's the home item)
   if (!isHome && (!label || (typeof label === "string" && label.trim() === ""))) {
     return null;
   }
 
   const LinkComponent = isHome ? Link : LinkWithQuery;
 
-  const labelShort =
-    typeof label === "string" && label.toString().length > BREADCRUMB_MAXLENGTH ? `${label.toString().substring(0, BREADCRUMB_MAXLENGTH)}...` : label;
-
   return (
     <>
-      <li data-cy={`breadcrumb ${cy}`}>
+      <li className="max-w-85 overflow-hidden whitespace-nowrap overflow-ellipsis" data-cy={`breadcrumb ${cy}`}>
         {href ? (
-          <LinkComponent
-            className={`${isHome && theme === "ccc" ? "font-semibold text-text-primary hover:underline" : "underline hover:text-blue-800 text-textNormal flex items-baseline"}`}
-            href={href}
-          >
-            {isHome && (
-              <>
-                {theme === "ccc" ? ( // TODO: get this from config dictionary (coming soon)
-                  <>Climate Litigation Database</>
-                ) : (
-                  <span className="mr-1 translate-y-0.5">
-                    <Icon name="house" />
-                  </span>
-                )}
-              </>
-            )}
-            {labelShort}
+          <LinkComponent className="hover:underline" href={href}>
+            {isHome ? getText("breadcrumbRoot") : label}
           </LinkComponent>
         ) : (
-          <span className="flex items-baseline">{labelShort}</span>
+          <>{label}</>
         )}
       </li>
 
-      {!last && <li>/</li>}
+      {!last && <li className="text-gray-400">/</li>}
     </>
   );
 };
@@ -81,50 +63,44 @@ export const BreadCrumbs = ({ geography = null, parentGeography = null, isSubdiv
   const isGeographyPage = !isSearchPage && !category && !family && geography && !label;
   const isCollectionPage = !isSearchPage && !category && !family && !geography;
 
+  const breadCrumbs: ReactNode[] = [
+    <BreadCrumb key="home" label="" href="/" cy="home" isHome />,
+    <BreadCrumb key="search" label="Search" href="/search" cy="search" />,
+  ];
+
   if (isGeographyPage) {
     const breadcrumbGeography = isSubdivision && parentGeography && !isSystemGeo(String(parentGeography.label)) ? parentGeography : null;
     const finalGeography = geography && !isSystemGeo(String(geography.label)) ? geography : null;
 
-    return (
-      <ul className="flex items-baseline flex-wrap gap-2 text-sm px-3 cols-2:px-6 cols-3:px-8 py-4 border-b border-gray-300" data-cy="breadcrumbs">
-        <BreadCrumb label="" href="/" cy="home" isHome theme={theme} />
-        <BreadCrumb label="Search" href="/search" cy="search" />
-        {breadcrumbGeography && <BreadCrumb label={breadcrumbGeography.label} href={breadcrumbGeography.href} cy="geography" />}
-        {finalGeography && <BreadCrumb label={finalGeography.label} last cy="current" />}
-      </ul>
-    );
-  }
+    if (breadcrumbGeography) breadCrumbs.push(<BreadCrumb label={breadcrumbGeography.label} href={breadcrumbGeography.href} cy="geography" />);
+    if (finalGeography) breadCrumbs.push(<BreadCrumb label={finalGeography.label} last cy="current" />);
+  } else if (isCollectionPage) {
+    if (label) breadCrumbs.push(<BreadCrumb key="collection" label={label} last cy="current" />);
+  } else {
+    const breadcrumbGeography =
+      isSubdivision && parentGeography && !isSystemGeo(String(parentGeography.label))
+        ? parentGeography
+        : geography && !isSystemGeo(String(geography.label))
+          ? geography
+          : null;
+    const breadcrumbSubGeography = isSubdivision && geography && !isSystemGeo(String(geography.label)) ? geography : null;
 
-  if (isCollectionPage) {
-    return (
-      <ul className="flex items-baseline flex-wrap gap-2 text-sm px-3 cols-2:px-6 cols-3:px-8 py-4 border-b border-gray-300" data-cy="breadcrumbs">
-        <BreadCrumb label="" href="/" cy="home" isHome theme={theme} />
-        <BreadCrumb label="Search" href="/search" cy="search" />
-        {label && <BreadCrumb label={label} last cy="current" />}
-      </ul>
-    );
+    if (breadcrumbGeography) breadCrumbs.push(<BreadCrumb label={breadcrumbGeography.label} href={breadcrumbGeography.href} cy="geography" />);
+    if (breadcrumbSubGeography)
+      breadCrumbs.push(<BreadCrumb label={breadcrumbSubGeography.label} href={breadcrumbSubGeography.href} cy="sub-geography" />);
+    if (category) breadCrumbs.push(<BreadCrumb label={category.label} href={category.href} cy="category" />);
+    if (family) breadCrumbs.push(<BreadCrumb label={family.label} href={family.href} cy="family" />);
+    if (label) breadCrumbs.push(<BreadCrumb label={label} last cy="current" />);
   }
-
-  const breadcrumbGeography =
-    isSubdivision && parentGeography && !isSystemGeo(String(parentGeography.label))
-      ? parentGeography
-      : geography && !isSystemGeo(String(geography.label))
-        ? geography
-        : null;
-  const breadcrumbSubGeography = isSubdivision && geography && !isSystemGeo(String(geography.label)) ? geography : null;
 
   return (
-    <ul
-      className={`flex items-baseline flex-wrap gap-2 text-sm px-3 cols-2:px-6 cols-3:px-8 py-4 ${isSearchPage ? "" : "border-b border-gray-300"}`}
-      data-cy="breadcrumbs"
-    >
-      <BreadCrumb label="" href="/" cy="home" isHome theme={theme} />
-      <BreadCrumb label="Search" href="/search" cy="search" />
-      {breadcrumbGeography && <BreadCrumb label={breadcrumbGeography.label} href={breadcrumbGeography.href} cy="geography" />}
-      {breadcrumbSubGeography && <BreadCrumb label={breadcrumbSubGeography.label} href={breadcrumbSubGeography.href} cy="sub-geography" />}
-      {category && <BreadCrumb label={category.label} href={category.href} cy="category" />}
-      {family && <BreadCrumb label={family.label} href={family.href} cy="family" />}
-      {label && <BreadCrumb label={label} last cy="current" />}
-    </ul>
+    <Columns>
+      <ul
+        className="cols-2:col-span-2 cols-3:col-span-3 cols-4:col-span-4 flex flex-wrap items-baseline gap-2 py-3 text-sm text-gray-700 leading-tight select-none"
+        data-cy="breadcrumbs"
+      >
+        {breadCrumbs}
+      </ul>
+    </Columns>
   );
 };
