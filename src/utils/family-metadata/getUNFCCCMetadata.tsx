cf4ae5d0be25fa9a4b7eb97739ef4e1851f6f14d
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2025-10-22 16:53:16 +0100

    Add UNFCCC metdata getter

---
+++ src/utils/family-metadata/getUNFCCCMetadata.tsx
@@ -0,0 +1,66 @@
+import sortBy from "lodash/sortBy";
+import { Fragment } from "react";
+
+import { LinkWithQuery } from "@/components/LinkWithQuery";
+import { ARROW_RIGHT, EN_DASH } from "@/constants/chars";
+import { getCountryName, getCountrySlug } from "@/helpers/getCountryFields";
+import { IMetadata, TFamilyPublic, TGeography } from "@/types";
+import { isSystemGeo } from "@/utils/isSystemGeo";
+import { convertDate } from "@/utils/timedate";
+
+export function getUNFCCCMetadata(family: TFamilyPublic, countries: TGeography[]): IMetadata[] {
+  const metadata = [];
+
+  const geosOrdered = sortBy(family.geographies, [(geo) => geo.length !== 3, (geo) => geo.toLowerCase()]);
+  const [year] = convertDate(family.published_date);
+  const document_type = family.documents && family.documents.length > 0 ? family.documents[0].document_type : undefined;
+
+  /* Year */
+  metadata.push({
+    label: "Year",
+    value: year || EN_DASH,
+  });
+
+  /* Geography */
+  if (family.geographies.length > 0) {
+    metadata.push({
+      label: "Geography",
+      value: family.geographies.map((geo, index) => {
+        const geoSlug = getCountrySlug(geo, countries);
+        const geoName = getCountryName(geo, countries);
+        return (
+          <Fragment key={geo}>
+            {index > 0 && ", "}
+            {!isSystemGeo(geoName) ? (
+              <LinkWithQuery href={`/geographies/${geoSlug || geo.toLowerCase()}`} className="underline">
+                {geoName}
+              </LinkWithQuery>
+            ) : (
+              <span>{geoName}</span>
+            )}
+          </Fragment>
+        );
+      }),
+    });
+  }
+
+  /* Document Type */
+  metadata.push({
+    label: "Type",
+    value: document_type || EN_DASH,
+  });
+
+  family?.metadata?.author_type &&
+    metadata.push({
+      label: "Author Type",
+      value: family.metadata?.author_type.join(", ") || EN_DASH,
+    });
+
+  family?.metadata?.author &&
+    metadata.push({
+      label: "Author",
+      value: family.metadata?.author.join(", ") || EN_DASH,
+    });
+
+  return metadata;
+}
