797b8216d0e98760bd126dadf05771993cebaf6f
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2025-10-22 16:26:48 +0100

    Use labels explcitly and not a mapper right now, we should delete that later

--- src/utils/family-metadata/getMCFMetadata.tsx
+++ src/utils/family-metadata/getMCFMetadata.tsx
@@ -1,10 +1,8 @@
-import sortBy from "lodash/sortBy";
-import { Fragment, ReactNode } from "react";
+import { Fragment } from "react";
 
 import { ExternalLink } from "@/components/ExternalLink";
 import { LinkWithQuery } from "@/components/LinkWithQuery";
-import { ARROW_RIGHT, EN_DASH } from "@/constants/chars";
-import { metadataLabelMappings } from "@/constants/familyMetadataMappings";
+import { EN_DASH } from "@/constants/chars";
 import { getApprovedYearFromEvents } from "@/helpers/getApprovedYearFromEvents";
 import { getSubCategoryName } from "@/helpers/getCategoryName";
 import { getCountryName, getCountrySlug } from "@/helpers/getCountryFields";
@@ -16,33 +14,14 @@ export function getMCFMetadata(family: TFamilyPublic, countries: TGeography[], s
   const metadata = [];
 
   metadata.push({
-    label: metadataLabelMappings.approval_date.label,
+    label: "Approval FY",
     value: getApprovedYearFromEvents(family.events) || EN_DASH,
   });
-
-  metadata.push({
-    label: metadataLabelMappings.organisation.label,
-    value: getSubCategoryName(family.organisation as TCorpusTypeSubCategory) || EN_DASH,
-  });
-
-  family.metadata?.project_value_fund_spend?.length > 0 &&
-    family.metadata?.project_value_fund_spend[0] !== "0" &&
-    metadata.push({
-      label: metadataLabelMappings.project_value_fund_spend.label,
-      value: getSumUSD(family.metadata?.project_value_fund_spend),
-    });
-
-  family.metadata?.project_value_co_financing?.length > 0 &&
-    family.metadata?.project_value_co_financing[0] !== "0" &&
-    metadata.push({
-      label: metadataLabelMappings.project_value_co_financing.label,
-      value: getSumUSD(family.metadata?.project_value_co_financing),
-    });
 
   /* Geography */
   if (family.geographies.length > 0) {
     metadata.push({
-      label: metadataLabelMappings.geographies.label,
+      label: "Geography",
       value: family.geographies.map((geo, index) => {
         const geoSlug = getCountrySlug(geo, countries);
         const geoName = getCountryName(geo, countries);
@@ -62,41 +41,60 @@ export function getMCFMetadata(family: TFamilyPublic, countries: TGeography[], s
     });
   }
 
+  metadata.push({
+    label: "Fund",
+    value: getSubCategoryName(family.organisation as TCorpusTypeSubCategory) || EN_DASH,
+  });
+
+  family.metadata?.project_value_fund_spend?.length > 0 &&
+    family.metadata?.project_value_fund_spend[0] !== "0" &&
+    metadata.push({
+      label: "Fund Spend",
+      value: getSumUSD(family.metadata?.project_value_fund_spend),
+    });
+
+  family.metadata?.project_value_co_financing?.length > 0 &&
+    family.metadata?.project_value_co_financing[0] !== "0" &&
+    metadata.push({
+      label: "Co-Financing",
+      value: getSumUSD(family.metadata?.project_value_co_financing),
+    });
+
   /* Metadata */
   family.metadata?.status?.length > 0 &&
     metadata.push({
-      label: metadataLabelMappings.status.label,
+      label: "Status",
       value: family.metadata.status.join(", "),
     });
   family.metadata?.theme?.length &&
     metadata.push({
-      label: metadataLabelMappings.theme.label,
+      label: "Theme",
       value: family.metadata.theme.join(", "),
     });
   family.metadata?.implementing_agency?.length > 0 &&
     metadata.push({
-      label: metadataLabelMappings.implementing_agency.label,
+      label: "Implementing Agency",
       value: family.metadata.implementing_agency.join(", "),
     });
   family.metadata?.sector?.length &&
     metadata.push({
-      label: metadataLabelMappings.sector.label,
+      label: "Sector",
       value: family.metadata.sector.join(", "),
     });
   family.metadata?.focal_area?.length > 0 &&
     metadata.push({
-      label: metadataLabelMappings.focal_area.label,
+      label: "Focal Area",
       value: family.metadata.focal_area.join(", "),
     });
   family.metadata?.result_area?.length > 0 &&
     metadata.push({
-      label: metadataLabelMappings.result_area.label,
+      label: "Result Area",
       value: family.metadata.result_area.join(", "),
     });
   family.metadata?.project_url?.length > 0 &&
     family.metadata?.project_url[0] !== "" &&
     metadata.push({
-      label: metadataLabelMappings.project_url.label,
+      label: "Source",
       value: (
         <ExternalLink url={family.metadata.project_url[0]} className="underline">
           Visit project page
