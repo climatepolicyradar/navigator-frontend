9b308666a34939a2d669d13eeb4b189b9d8277bd
Jessica Kelsall <jessica.kelsall@climatepolicyradar.org>
2025-10-22 14:48:21 +0100

    Update CPR home page logo

--- themes/cpr/pages/homepage.tsx
+++ themes/cpr/pages/homepage.tsx
@@ -1,7 +1,8 @@
+import Image from "next/image";
+
 import Banner from "@/components/banner/FullHeight";
 import Footer from "@/components/footer/Footer";
 import Layout from "@/components/layouts/LandingPage";
-import AlphaLogo from "@/components/logo/AlphaLogo";
 import { SiteWidth } from "@/components/panels/SiteWidth";
 import { Header } from "@/cpr/components/Header";
 import LandingPageLinks from "@/cpr/components/LandingPageLinks";
@@ -37,12 +38,12 @@ const LandingPage = ({ handleSearchInput, handleSearchChange, searchInput, exact
         <main className="relative h-full">
           <Banner />
           <section className="absolute inset-0 z-10 flex flex-col items-center justify-center min-h-[800px]">
-            <AlphaLogo />
+            <div data-cy="cpr-logo" className="text-white flex">
+              <Image src="/images/cpr-logo-homepage.png" width={596} height={34} alt="Climate Policy Radar" className="px-5" />
+            </div>
             <SiteWidth extraClasses="mt-24 md:mt-48 max-w-screen-lg mx-auto">
               <LandingSearchForm handleSearchInput={handleSearchInput} input={searchInput} />
-              <div className="mt-12">
-                <LandingPageLinks />
-              </div>
+              <LandingPageLinks />
             </SiteWidth>
           </section>
         </main>
