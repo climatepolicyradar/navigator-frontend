4e8c7b650b29fb2ccd074372bfcfcfece01c475b
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2025-11-03 17:55:53 +0000

    Add logic for the table rows

--- src/components/blocks/topicsBlock/TopicsBlock.tsx
+++ src/components/blocks/topicsBlock/TopicsBlock.tsx
@@ -1,17 +1,11 @@
-import startCase from "lodash/startCase";
 import { useEffect, useState } from "react";
 
-import { ConceptLink } from "@/components/molecules/conceptLink/ConceptLink";
-import { Info } from "@/components/molecules/info/Info";
 import { Section } from "@/components/molecules/section/Section";
 import { InteractiveTable } from "@/components/organisms/interactiveTable/InteractiveTable";
-import { Heading } from "@/components/typography/Heading";
 import { TConcept } from "@/types";
 import { groupByRootConcept } from "@/utils/conceptsGroupedbyRootConcept";
-import { getConceptStoreLink } from "@/utils/getConceptStoreLink";
 import { fetchAndProcessConcepts } from "@/utils/processConcepts";
-import { TopicTableColumns, TTopicTableColumnId } from "@/utils/tables/topic/topicTable";
-import { firstCase } from "@/utils/text";
+import { getTopicTableRows, TopicTableColumns, TTopicTableColumnId } from "@/utils/tables/topic/topicTable";
 
 type TProps = {
   topicIds: string[];
@@ -22,45 +16,22 @@ export const TopicsBlock = ({ topicIds }: TProps) => {
   const [conceptsGrouped, setConceptsGrouped] = useState<{
     [rootConceptId: string]: TConcept[];
   }>({});
-  const [filteredConcepts, setFilteredConcepts] = useState<TConcept[]>([]);
 
   useEffect(() => {
     const conceptIds = topicIds.map((topic) => topic);
     fetchAndProcessConcepts(conceptIds).then(({ rootConcepts, concepts }) => {
       setRootConcepts(rootConcepts);
-      setFilteredConcepts(concepts);
       setConceptsGrouped(groupByRootConcept(concepts, rootConcepts));
     });
   }, [topicIds]);
 
   return (
     <Section block="topics" title="Topics explorer">
-      <InteractiveTable<TTopicTableColumnId> columns={TopicTableColumns} rows={[]} defaultSort={{ column: "group", order: "desc" }} />
-      {rootConcepts.map((rootConcept) => {
-        const hasConcepts = conceptsGrouped[rootConcept.wikibase_id]?.length > 0;
-        if (!hasConcepts) return null;
-        return (
-          <div key={rootConcept.wikibase_id} className="relative group">
-            <div className="flex items-center gap-2">
-              <Heading level={3} className="text-[15px] leading-tight font-medium text-text-primary">
-                {firstCase(rootConcept.preferred_label)}
-              </Heading>
-              <Info
-                title={startCase(rootConcept.preferred_label)}
-                description={rootConcept.description}
-                link={{ href: getConceptStoreLink(rootConcept.wikibase_id), text: "Source", external: true }}
-              />
-            </div>
-            <ul className="">
-              {conceptsGrouped[rootConcept.wikibase_id].map((concept) => (
-                <li key={concept.wikibase_id} className="">
-                  <ConceptLink concept={concept} />
-                </li>
-              ))}
-            </ul>
-          </div>
-        );
-      })}
+      <InteractiveTable<TTopicTableColumnId>
+        columns={TopicTableColumns}
+        rows={getTopicTableRows({ rootConcepts, conceptsGrouped })}
+        defaultSort={{ column: "group", order: "desc" }}
+      />
     </Section>
   );
 };
--- src/utils/tables/topic/topicTable.tsx
+++ src/utils/tables/topic/topicTable.tsx
@@ -1,4 +1,5 @@
-import { TTableColumn } from "@/types";
+import { TConcept, TTableColumn, TTableRow } from "@/types";
+import { firstCase } from "@/utils/text";
 
 export type TTopicTableColumnId = "group" | "concepts";
 
@@ -6,3 +7,55 @@ export const TopicTableColumns: TTableColumn<TTopicTableColumnId>[] = [
   { id: "group", name: "Group", fraction: 2 },
   { id: "concepts", name: "Concepts", fraction: 5 },
 ];
+
+export type TTopicTableRow = TTableRow<TTopicTableColumnId>;
+
+export const getTopicTableRows = ({
+  rootConcepts,
+  conceptsGrouped,
+}: {
+  rootConcepts: TConcept[];
+  conceptsGrouped: {
+    [rootConceptId: string]: TConcept[];
+  };
+}): TTopicTableRow[] => {
+  const rows: TTopicTableRow[] = [];
+
+  rootConcepts.forEach((rootConcept) => {
+    const hasConcepts = conceptsGrouped[rootConcept.wikibase_id]?.length > 0;
+    if (!hasConcepts) return null;
+
+    rows.push({
+      id: rootConcept.wikibase_id,
+      cells: {
+        group: firstCase(rootConcept.preferred_label),
+        concepts: {
+          label: conceptsGrouped[rootConcept.wikibase_id].map((concept) => concept.preferred_label).join(", "),
+          value: conceptsGrouped[rootConcept.wikibase_id].map((concept) => concept.wikibase_id).join(", "),
+        },
+      },
+    });
+
+    //       return (
+    // <div key={rootConcept.wikibase_id} className="relative group">
+    //   <div className="flex items-center gap-2">
+    //     <Heading level={3} className="text-[15px] leading-tight font-medium text-text-primary">
+    //       {firstCase(rootConcept.preferred_label)}
+    //     </Heading>
+    //     <Info
+    //       title={startCase(rootConcept.preferred_label)}
+    //       description={rootConcept.description}
+    //       link={{ href: getConceptStoreLink(rootConcept.wikibase_id), text: "Source", external: true }}
+    //     />
+    //   </div>
+    //   <ul className="">
+    //     {conceptsGrouped[rootConcept.wikibase_id].map((concept) => (
+    //       <li key={concept.wikibase_id} className="">
+    //         <ConceptLink concept={concept} />
+    //       </li>
+    //     ))}
+    //   </ul>
+    // </div>
+  });
+  return rows;
+};
