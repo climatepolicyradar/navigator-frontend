ce76085e9e26072d42d773afb0b8ab1f8f13266a
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2025-11-04 20:39:21 +0000

    Add correct counts to the popover cotent and link to document view

--- src/utils/tables/topic/topicTable.tsx
+++ src/utils/tables/topic/topicTable.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 
 import { ConceptLink } from "@/components/molecules/conceptLink/ConceptLink";
+import { QUERY_PARAMS } from "@/constants/queryParams";
 import { IFamilyDocumentTopics, TTableColumn, TTableRow } from "@/types";
 import { firstCase } from "@/utils/text";
 
@@ -41,20 +42,25 @@ export const getTopicTableRows = (familyTopics: IFamilyDocumentTopics): TTopicTa
                     >
                       <div>
                         <h6 className="mb-2 font-bold">{firstCase(concept.preferred_label)}</h6>
-                        <span>{concept.description}</span>
+                        <span className="block mb-2">{concept.description}</span>
                         <span className="text-xs text-gray-500">
                           This concept is mentioned in the following documents, click on the document to view the specific passages:
                         </span>
                         <div className="flex flex-col gap-1">
                           {familyTopics.documents
                             .filter((doc) => Object.keys(doc.conceptCounts).some((key) => key.split(":")[0] === concept.wikibase_id)) // TODO: make this nicer
-                            .map((doc, i) => {
-                              // const topicCount = Object.keys(familyTopics.conceptCounts).some((key) => key === concept.wikibase_id.split(":")[0])
-                              //   ? doc.conceptCounts[`${concept.wikibase_id}:`]
-                              //   : 0;
+                            .map((doc) => {
+                              const topicCount = doc.conceptCounts ? doc.conceptCounts[`${concept.wikibase_id}:${concept.preferred_label}`] || 0 : 0;
                               return (
-                                <Link className="font-medium underline block" key={doc.importId} href={`/documents/${doc.slug}`}>
-                                  {doc.title} ({doc.conceptCounts[concept.wikibase_id] || 0})
+                                <Link
+                                  className="font-medium block hover:text-blue-800 transition underline-offset-2 hover:underline"
+                                  key={doc.importId}
+                                  href={{
+                                    pathname: `/documents/${doc.slug}`,
+                                    query: { [QUERY_PARAMS.concept_name]: concept.preferred_label },
+                                  }}
+                                >
+                                  {doc.title} ({topicCount})
                                 </Link>
                               );
                             })}
