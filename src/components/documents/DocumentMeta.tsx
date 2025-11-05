540c4332426426f83b14f94000822b45bc780794
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2025-11-05 15:23:26 +0000

    Show category name on document page

--- src/components/documents/DocumentMeta.tsx
+++ src/components/documents/DocumentMeta.tsx
@@ -1,4 +1,5 @@
 import { CountryLinks } from "@/components/CountryLinks";
+import { getCategoryName } from "@/helpers/getCategoryName";
 import { getLanguage } from "@/helpers/getLanguage";
 import useConfig from "@/hooks/useConfig";
 import { convertDate } from "@/utils/timedate";
@@ -18,7 +19,9 @@ export const DocumentMeta = ({ family, isMain, document, document_type }: { fami
           {family.category === "Reports" && family.metadata.author && family.metadata.author.length > 0 && (
             <span className="capitalize">{family.metadata.author.join(", ")}</span>
           )}
-          {family.category && <span className="capitalize">{family.category}</span>}
+          {family.category && (
+            <span className="capitalize">{getCategoryName(family.category, family.corpus_type_name, family.source, family.corpus_id)}</span>
+          )}
           {document_type && (
             <span className="capitalize" data-cy="family-metadata-document_type">
               {document_type}
