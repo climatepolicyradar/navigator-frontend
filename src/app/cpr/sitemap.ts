6562e596d99c9cff4d3642c2ba84a86184948c12
James Gorrie <james.gorrie@climatepolicyradar.org>
2025-11-06 17:13:04 +0000

    feat: add UN Submissions to sitemaps and bump number of families in sitemap

--- src/app/cpr/sitemap.ts
+++ src/app/cpr/sitemap.ts
@@ -4,17 +4,25 @@ import { ApiClient } from "@/api/http-common";
 import CPRthemeConfig from "@/cpr/config";
 import { extractGeographySlugs } from "@/utils/geography";
 
-export const dynamic = "force-dynamic";
+/* Regenerate sitemap every 6 hours (21600 seconds) */
+export const revalidate = 21600;
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const allCorpusIds = CPRthemeConfig.categories?.options.flatMap((option) => option.value) || [];
   const allCorpusIdsSearchParams = allCorpusIds.map((corpusId) => ["corpus.import_id", corpusId]);
   const urlSearchParams = new URLSearchParams(allCorpusIdsSearchParams);
 
-  /* Families */
-
-  const familiesData = await fetch(`https://api.climatepolicyradar.org/families/?${urlSearchParams.toString()}`).then((resp) => resp.json());
-  const familiesSiteMap = familiesData.data.map((family) => {
+  /**
+   * UN Submissions
+   * These are here while COP in Rio is on to true and get as much Google juice as possible
+   */
+  const UNSubmissionsCorpusIds = ["UN.corpus.UNCBD.n0000", "UN.corpus.UNCCD.n0000"];
+  const UNSubmissionsSearchParams = UNSubmissionsCorpusIds.map((corpusId) => ["corpus.import_id", corpusId]);
+  const UNSubmissionsUrlSearchParams = new URLSearchParams(UNSubmissionsSearchParams);
+  const UNSubmissionsData = await fetch(`https://api.climatepolicyradar.org/families/?${UNSubmissionsUrlSearchParams.toString()}`).then((resp) =>
+    resp.json()
+  );
+  const UNSubmissionsSiteMap = UNSubmissionsData.data.map((family) => {
     return {
       url: `https://app.climatepolicyradar.org/document/${family.slug}`,
       lastModified: family.last_updated_date,
@@ -23,8 +31,25 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     };
   });
 
-  /* Geographies */
+  /* Families */
+  const familiesSiteMap = [];
+  for (let page = 1; page <= 100; page++) {
+    const familiesData = await fetch(`https://api.climatepolicyradar.org/families/?${urlSearchParams.toString()}&page=${page}`).then((resp) =>
+      resp.json()
+    );
+    familiesSiteMap.push(
+      ...familiesData.data.map((family) => {
+        return {
+          url: `https://app.climatepolicyradar.org/document/${family.slug}`,
+          lastModified: family.last_updated_date,
+          changeFrequency: "daily",
+          priority: 1,
+        };
+      })
+    );
+  }
 
+  /* Geographies */
   const client = new ApiClient();
   const {
     data: { geographies: geographiesData },
@@ -37,6 +62,10 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     changeFrequency: "daily",
     priority: 0.75,
   }));
+
+  const deduplicatedFamiliesSiteMap = familiesSiteMap.filter(
+    (family) => !UNSubmissionsSiteMap.some((unSubmission) => unSubmission.url === family.url)
+  );
 
   // The manually added pages are taken from the footer
   return [
@@ -52,7 +81,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       changeFrequency: "daily",
       priority: 1,
     },
-    ...familiesSiteMap,
+    ...deduplicatedFamiliesSiteMap,
     ...geographiesSiteMap,
     {
       url: "https://app.climatepolicyradar.org/terms-of-use",
