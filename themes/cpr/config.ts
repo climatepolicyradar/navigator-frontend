c265255a6936012bcdea7692cccff241a1387138
Jessica Kelsall <jessica.kelsall@climatepolicyradar.org>
2025-11-11 13:55:20 +0000

    getGeographyMetadata tweaks, add MetadataBlock to CPR geography page

--- themes/cpr/config.ts
+++ themes/cpr/config.ts
@@ -441,7 +441,7 @@ const config: TThemeConfig = {
   defaultDocumentCategory: "Laws",
   pageBlocks: {
     family: ["summary", "metadata"],
-    geography: [],
+    geography: ["statistics"],
   },
   tutorials: ["knowledgeGraph"],
   features: {
