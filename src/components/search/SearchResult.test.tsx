cc678f4ec3ab63c508c5a131b420dde67da1175b
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2025-12-23 14:26:50 +0000

    Fix missing type and missing fields

--- src/components/search/SearchResult.test.tsx
+++ src/components/search/SearchResult.test.tsx
@@ -1,16 +1,26 @@
 import { screen } from "@testing-library/react";
 
 import { renderWithAppContext } from "@/mocks/renderWithAppContext";
+import { TMatchedFamily } from "@/types";
 
 import SearchResult from "./SearchResult";
 
+type TSearchResultProps = {
+  family: TMatchedFamily;
+  active: boolean;
+  onClick?: () => void;
+};
+
 describe("SearchResult", () => {
   it("displays all geographies as links if family has multiple geographies", async () => {
-    const searchResultProps = {
-      themeConfig: { features: {} },
+    const searchResultProps: TSearchResultProps = {
       family: {
-        corpus_type_name: "",
-        family_category: "",
+        corpus_import_id: "1",
+        family_description_match: false,
+        family_title_match: false,
+        total_passage_hits: 0,
+        corpus_type_name: "Reports",
+        family_category: "REPORTS",
         family_description: "",
         family_documents: [],
         family_geographies: ["AUS", "ARG"],
@@ -31,11 +41,14 @@ describe("SearchResult", () => {
   });
 
   it("displays all subdivision links if family has subdivision geographies", async () => {
-    const searchResultProps = {
-      themeConfig: { features: {} },
+    const searchResultProps: TSearchResultProps = {
       family: {
-        corpus_type_name: "",
-        family_category: "",
+        corpus_import_id: "1",
+        family_description_match: false,
+        family_title_match: false,
+        total_passage_hits: 0,
+        corpus_type_name: "Reports",
+        family_category: "REPORTS",
         family_description: "",
         family_documents: [],
         family_geographies: ["AUS", "AU-NSW", "AU-QLD"],
