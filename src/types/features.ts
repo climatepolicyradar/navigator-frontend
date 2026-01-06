e859e1e8018c29b5657b607bd05dd3c5f157b981
Jessica Kelsall <jessica.kelsall@climatepolicyradar.org>
2026-01-06 11:21:19 +0000

    Add useFeatures hook

--- src/types/features.ts
+++ src/types/features.ts
@@ -1,7 +1,11 @@
+// All available feature flags. If adding a new one, make sure to add it here or it won't be available via useFeatures
 export const featureFlagKeys = ["concepts-v1", "litigation"] as const;
-type TFeatureFlag = (typeof featureFlagKeys)[number];
+export type TFeatureFlag = (typeof featureFlagKeys)[number];
 export type TFeatureFlags = Record<TFeatureFlag, boolean>;
 
+// All available theme config features. Each must be explicitly defined in each config's features object
 export const configFeatureKeys = ["familyConceptsSearch", "knowledgeGraph", "litigation", "searchFamilySummary"] as const;
-type TConfigFeature = (typeof configFeatureKeys)[number];
+export type TConfigFeature = (typeof configFeatureKeys)[number];
 export type TConfigFeatures = Record<TConfigFeature, boolean>;
+
+export type TFeatures = Record<TFeatureFlag | TConfigFeature, boolean>;
