a7e5e3327bb38e80fea396c710c36401a323cb62
James Gorrie <james.gorrie@climatepolicyradar.org>
2026-01-06 13:02:40 +0000

    fix: add adaptation to root topics

--- src/constants/topics.ts
+++ src/constants/topics.ts
@@ -10,8 +10,22 @@ export const ROOT_TOPICS = {
   Q1367: "Public finance actor",
   Q47: "Just transition",
   Q567: "Renewable energy",
+  Q557: "Adaptation",
 } satisfies Record<string, string>;
 
 type TRootTopicId = keyof typeof ROOT_TOPICS;
 
-export const ORDERED_ROOT_TOPIC_IDS: TRootTopicId[] = ["Q1651", "Q1171", "Q975", "Q672", "Q47", "Q567", "Q638", "Q218", "Q709", "Q1343", "Q1367"];
+export const ORDERED_ROOT_TOPIC_IDS: TRootTopicId[] = [
+  "Q1651",
+  "Q1171",
+  "Q975",
+  "Q672",
+  "Q47",
+  "Q567",
+  "Q638",
+  "Q218",
+  "Q709",
+  "Q1343",
+  "Q1367",
+  "Q557",
+];
