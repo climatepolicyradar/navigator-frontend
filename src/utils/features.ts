4b3ed0f5ad71a44d78b708abea286dd259ac77c2
Jessica Kelsall <jessica.kelsall@climatepolicyradar.org>
2026-01-20 14:51:37 +0000

    APP-1609: Add A/B tests as a type of feature flag (#1018)
    * Add A/B tests as a type of feature flag
    
    * Fix markdown linting errors

--- src/utils/features.ts
+++ src/utils/features.ts
@@ -1,4 +1,6 @@
-import { configFeatureKeys, featureFlagKeys, TConfigFeature, TFeatureFlag, TFeatureFlags, TFeatures, TTheme, TThemeConfig } from "@/types";
+import { useFeatureFlagEnabled } from "posthog-js/react";
+
+import { abTestKeys, configFeatureKeys, featureFlagKeys, TFeatureFlags, TFeatures, TThemeConfig } from "@/types";
 
 interface IArgs {
   configFeature?: boolean;
@@ -16,7 +18,7 @@ export const isFeatureEnabled = ({ configFeature, featureFlag }: IArgs): boolean
 // Prefer FeaturesContext over getFeatures where possible. Use getFeatures in getServerSideProps or inside page components
 export const getFeatures = (themeConfig: TThemeConfig, featureFlags: TFeatureFlags) => {
   // Only keys defined in these two array will be included
-  const featureKeys: (TFeatureFlag | TConfigFeature)[] = Array.from(new Set([...featureFlagKeys, ...configFeatureKeys]));
+  const featureKeys: (keyof TFeatures)[] = Array.from(new Set([...featureFlagKeys, ...abTestKeys, ...configFeatureKeys]));
 
   // Construct an object of feature keys and boolean values. This simplifies the relation between feature flags and config features
   return Object.fromEntries(
@@ -29,4 +31,16 @@ export const getFeatures = (themeConfig: TThemeConfig, featureFlags: TFeatureFla
       return [featureKey, isEnabled];
     })
   ) as TFeatures;
+};
+
+// Add A/B test feature flags into TFeatures. Does not work if called in getServerSideProps
+export const getFeaturesWithABTests = (features: TFeatures): TFeatures => {
+  const featuresWithABTests = { ...features };
+
+  abTestKeys.forEach((key) => {
+    const abTestIsEnabled = useFeatureFlagEnabled(key);
+    featuresWithABTests[key] = Boolean(abTestIsEnabled); // May be undefined if not yet initialised
+  });
+
+  return featuresWithABTests;
 };
