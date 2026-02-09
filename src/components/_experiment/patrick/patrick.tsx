12526ee75cf7bbea07b2f9258c745a15ac5ff8ea
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2026-02-09 12:30:56 +0000

    APP-1686 shadow search page (#1062)

---
+++ src/components/_experiment/patrick/patrick.tsx
@@ -0,0 +1,8 @@
+export const PatrickComponent = () => {
+  return (
+    <div>
+      <h1>Patrick's Experiment Component</h1>
+      <p>This is a test component for Patrick's experiment.</p>
+    </div>
+  );
+};
