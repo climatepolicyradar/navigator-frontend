1eb196e118a2c1e1594b5b0e6b32e46e65bcc54f
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2026-02-10 16:42:12 +0000

    Add low fidelity components for search sandbox page

--- src/pages/_search/index.tsx
+++ src/pages/_search/index.tsx
@@ -1,7 +1,8 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import { useState } from "react";
-
+import { Input } from "@base-ui/react/input";
+import { Button } from "@base-ui/react/button";
 import { ApiClient } from "@/api/http-common";
 import { PatrickComponent } from "@/components/_experiment/patrick/patrick";
 import { Debug } from "@/components/atoms/debug/Debug";
@@ -18,29 +19,147 @@ import { getFeatures } from "@/utils/features";
 import { fetchAndProcessTopics } from "@/utils/fetchAndProcessTopics";
 import { readConfigFile } from "@/utils/readConfigFile";
 
-const TOPICS = ["Flood defence", "Targets"];
+const TOPICS = ["flood defence", "targets"];
+const GEOS = ["spain"];
 
 type TProps = InferGetServerSidePropsType<typeof getServerSideProps>;
+
+const findMatches = (searchTerm: string) => {
+  if (!searchTerm) return { matchedConcepts: [], matchedGeos: [], matchedYears: [] };
+
+  const matchedYears: string[] = [];
+  const rawSearchTermParts = searchTerm.trim().split(" ");
+  for (let i = 0; i < rawSearchTermParts.length; i++) {
+    const year = parseInt(rawSearchTermParts[i]);
+    if (!isNaN(year) && year >= 1900 && year <= 2100) {
+      matchedYears.push(year.toString());
+    }
+  }
+
+  const matchedConcepts = TOPICS.filter((topic) => searchTerm.toLowerCase().includes(topic.toLowerCase()));
+  const matchedGeos = GEOS.filter((geo) => searchTerm.toLowerCase().includes(geo.toLowerCase()));
+  return { matchedConcepts, matchedGeos, matchedYears };
+};
 
 const ShadowSearch = ({ theme, themeConfig, features, topicsData, familyConceptsData }: TProps) => {
   const configQuery = useConfig();
   const { data: { regions = [], countries = [], corpus_types = {} } = {} } = configQuery;
 
   const [searchTerm, setSearchTerm] = useState("");
-  const [selectedTopics, setSelectedTopics] = useState<TTopic[]>([]);
+  const [rawSearchTerm, setRawSearchTerm] = useState("");
+  const [selectedTopics, setSelectedTopics] = useState<string[]>([]);
   const [selectedGeos, setSelectedGeos] = useState<string[]>([]);
+  const [selectedYears, setSelectedYears] = useState<string[]>([]);
+
+  const { matchedConcepts, matchedGeos, matchedYears } = findMatches(searchTerm);
 
   return (
-    // <Layout theme={theme as TTheme} themeConfig={themeConfig} metadataKey="search">
     <FeaturesContext.Provider value={features}>
       <TopicsContext.Provider value={topicsData}>
         <WikiBaseConceptsContext.Provider value={familyConceptsData || []}>
-          <PatrickComponent />
           <section>
-            <h1>_Shadow Search Page</h1>
-            <Debug title="Features" data={features} />
-            <Debug title="Topics Data" data={topicsData} />
-            <Debug title="Family Concepts Data" data={familyConceptsData} />
+            <>
+              <Input
+                placeholder="Search"
+                onChange={(e) => {
+                  setSearchTerm(e.target.value);
+                }}
+                value={searchTerm}
+              />
+
+              <Button onClick={() => setSearchTerm("")}>x</Button>
+            </>
+
+            <div>
+              <ul>
+                <li> Based on your search: "{searchTerm}", we found the following topics, geos, and years:</li>
+                {matchedConcepts
+                  .filter((concept) => !selectedTopics.includes(concept))
+                  .map((concept) => (
+                    <li key={concept}>
+                      <Button onClick={() => setSelectedTopics([...selectedTopics, concept])}>{concept}</Button>
+                    </li>
+                  ))}
+                {matchedGeos
+                  .filter((geo) => !selectedGeos.includes(geo))
+                  .map((geo) => (
+                    <li key={geo}>
+                      <Button onClick={() => setSelectedGeos([...selectedGeos, geo])}>{geo}</Button>
+                    </li>
+                  ))}
+                {matchedYears
+                  .filter((year) => !selectedYears.includes(year))
+                  .map((year) => (
+                    <li key={year}>
+                      <Button onClick={() => setSelectedYears([...selectedYears, year])}>{year}</Button>
+                    </li>
+                  ))}
+                {(matchedConcepts.length > 0 || matchedGeos.length > 0 || matchedYears.length > 0) && (
+                  <>
+                    <Button
+                      onClick={() => {
+                        setSelectedTopics(matchedConcepts);
+                        setSelectedGeos(matchedGeos);
+                        setSelectedYears(matchedYears);
+                        setRawSearchTerm(searchTerm);
+                        setSearchTerm("");
+                      }}
+                    >
+                      Apply all filters
+                    </Button>
+                    <p>or</p>
+                  </>
+                )}
+                <Button onClick={() => setSearchTerm(searchTerm)}>Search "{searchTerm}"?</Button>
+              </ul>
+            </div>
+            <br />
+
+            <div>
+              <p>Filters:</p>
+              <p>
+                Topics:{" "}
+                {selectedTopics.map((topic) => (
+                  <Button key={topic} onClick={() => setSelectedTopics(selectedTopics.filter((topicToRemove) => topicToRemove !== topic))}>
+                    {topic}
+                  </Button>
+                ))}
+              </p>
+              <p>
+                Geos:{" "}
+                {selectedGeos.map((geo) => (
+                  <Button key={geo} onClick={() => setSelectedGeos(selectedGeos.filter((geoToRemove) => geoToRemove !== geo))}>
+                    {geo}
+                  </Button>
+                ))}
+              </p>
+              <p>
+                Years:{" "}
+                {selectedYears.map((year) => (
+                  <Button key={year} onClick={() => setSelectedYears(selectedYears.filter((yearToRemove) => yearToRemove !== year))}>
+                    {year}
+                  </Button>
+                ))}
+              </p>
+            </div>
+
+            <br />
+            <div>
+              <p>Results for your query:</p>
+              <p>"{rawSearchTerm}"</p>
+
+              <p>This has been transformed into a filtered view...:</p>
+              <Button
+                onClick={() => {
+                  setSearchTerm(rawSearchTerm);
+                  setSelectedTopics([]);
+                  setSelectedGeos([]);
+                  setSelectedYears([]);
+                }}
+              >
+                Reset
+              </Button>
+            </div>
           </section>
         </WikiBaseConceptsContext.Provider>
       </TopicsContext.Provider>
