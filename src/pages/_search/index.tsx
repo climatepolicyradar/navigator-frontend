10fcd87fd9f9d9c08e02a269194c3c1f6c49cb48
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2026-02-10 14:28:00 +0000

    Add some state to shadow search page

--- src/pages/_search/index.tsx
+++ src/pages/_search/index.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import { useState } from "react";
 
 import { ApiClient } from "@/api/http-common";
 import { PatrickComponent } from "@/components/_experiment/patrick/patrick";
@@ -8,6 +10,7 @@ import { withEnvConfig } from "@/context/EnvConfig";
 import { FeaturesContext } from "@/context/FeaturesContext";
 import { TopicsContext } from "@/context/TopicsContext";
 import { WikiBaseConceptsContext } from "@/context/WikiBaseConceptsContext";
+import useConfig from "@/hooks/useConfig";
 import { TTheme, TTopic, TTopics } from "@/types";
 import { FamilyConcept, mapFamilyConceptsToConcepts } from "@/utils/familyConcepts";
 import { getFeatureFlags } from "@/utils/featureFlags";
@@ -15,25 +18,33 @@ import { getFeatures } from "@/utils/features";
 import { fetchAndProcessTopics } from "@/utils/fetchAndProcessTopics";
 import { readConfigFile } from "@/utils/readConfigFile";
 
+const TOPICS = ["Flood defence", "Targets"];
+
 type TProps = InferGetServerSidePropsType<typeof getServerSideProps>;
 
 const ShadowSearch = ({ theme, themeConfig, features, topicsData, familyConceptsData }: TProps) => {
+  const configQuery = useConfig();
+  const { data: { regions = [], countries = [], corpus_types = {} } = {} } = configQuery;
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectedTopics, setSelectedTopics] = useState<TTopic[]>([]);
+  const [selectedGeos, setSelectedGeos] = useState<string[]>([]);
+
   return (
-    <Layout theme={theme as TTheme} themeConfig={themeConfig} metadataKey="search">
-      <FeaturesContext.Provider value={features}>
-        <TopicsContext.Provider value={topicsData}>
-          <WikiBaseConceptsContext.Provider value={familyConceptsData || []}>
-            <PatrickComponent />
-            <section>
-              <h1>_Shadow Search Page</h1>
-              <Debug title="Features" data={features} />
-              <Debug title="Topics Data" data={topicsData} />
-              <Debug title="Family Concepts Data" data={familyConceptsData} />
-            </section>
-          </WikiBaseConceptsContext.Provider>
-        </TopicsContext.Provider>
-      </FeaturesContext.Provider>
-    </Layout>
+    // <Layout theme={theme as TTheme} themeConfig={themeConfig} metadataKey="search">
+    <FeaturesContext.Provider value={features}>
+      <TopicsContext.Provider value={topicsData}>
+        <WikiBaseConceptsContext.Provider value={familyConceptsData || []}>
+          <PatrickComponent />
+          <section>
+            <h1>_Shadow Search Page</h1>
+            <Debug title="Features" data={features} />
+            <Debug title="Topics Data" data={topicsData} />
+            <Debug title="Family Concepts Data" data={familyConceptsData} />
+          </section>
+        </WikiBaseConceptsContext.Provider>
+      </TopicsContext.Provider>
+    </FeaturesContext.Provider>
   );
 };
 
