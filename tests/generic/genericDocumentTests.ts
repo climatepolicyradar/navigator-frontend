c842ad54739ce5b17c9392e2daa20e4f6379b3fc
Anna Pokorska <anna@climatepolicyradar.org>
2026-02-25 16:36:33 +0000

    Finish first document test

--- tests/generic/genericDocumentTests.ts
+++ tests/generic/genericDocumentTests.ts
@@ -14,7 +14,13 @@ export const runGenericDocumentTests = (theme: TTheme): void => {
       // Load the document page
 
       await documentPage.goToDocument(page, `${slug}?q=${withSearch}&cfn=${withTopic}`);
-      await genericPage.waitUntilLoaded(page);
+      // waiting for state "load" rather than "networkidle" as the latter was waiting for the PDF viewer
+      // which was causing timing issues in the test as it is currently not loading locally
+      // replace with genericPage.waitUntilLoaded(page) if no longer an issue
+      await page.waitForLoadState("load");
+      await expect(page.getByRole("heading", { level: 1 })).toBeVisible();
+
+      await genericPage.dismissPopups(page);
 
       // Enter a different search query
 
@@ -23,7 +29,8 @@ export const runGenericDocumentTests = (theme: TTheme): void => {
       await searchInput.press("Enter");
 
       // Check that Passage matches section is updated with the new search query
-      await expect(page.getByRole("region", { name: "Passage matches" })).toHaveText(/matches for 'emissions'/);
+      // The regex will match both "1 match for 'benchmark'" and "10 matches for 'benchmark'"
+      await expect(page.getByRole("region", { name: "Passage matches" })).toHaveText(/\d+ matche?s? for 'benchmark'/);
     });
   });
 };
