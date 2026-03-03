764475ee4d24d295a1dc7b344d505ac84982e067
Jessica Kelsall <jessica.kelsall@climatepolicyradar.org>
2026-03-03 15:23:55 +0000

    Revert EmbeddedPDF hook dependencies

--- src/components/EmbeddedPDF.tsx
+++ src/components/EmbeddedPDF.tsx
@@ -24,7 +24,8 @@ const EmbeddedPDF = ({ document, documentPassageMatches = [], pageNumber = null,
   // Ensure the instance of the PDF client is not reset on re-render
   // otherwise we lose the ability to interact with the pdf
 
-  const { changePage, registerPassages } = useMemo(() => pdfPreview, [pdfPreview]);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const { changePage, registerPassages } = useMemo(() => pdfPreview, [document, adobeKey]);
 
   useEffect(() => {
     if (pageNumber) changePage(pageNumber);
@@ -42,26 +43,7 @@ const EmbeddedPDF = ({ document, documentPassageMatches = [], pageNumber = null,
           <Loader />
         </div>
       ) : (
-        <>
-          {/* <AnimatePresence initial={false}>
-            {(searchStatus === "loading" || isLoading) && (
-              <motion.div
-                key="content"
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 0.95, clipPath: "inset(0 0 0 0)" }}
-                exit={{ clipPath: "inset(0px 100% 0px 0px)" }}
-                transition={{ duration: 0.5, ease: [0.77, 0, 0.175, 1] }}
-                className="flex flex-col items-center gap-6 flex-1 absolute left-0 right-0 bottom-0 top-0 h-full bg-white pt-10 z-100"
-              >
-                <div className="w-[80px] h-[80px]">
-                  <object className="radar" type="image/svg+xml" data="/images/radar-loader.svg" style={{ width: "80px", height: "80px" }} />
-                </div>
-                <div>Loading the PDF viewer</div>
-              </motion.div>
-            )}
-          </AnimatePresence> */}
-          <div ref={containerRef} id="pdf-div" className="h-full" data-analytics-document={document.content_type}></div>
-        </>
+        <div ref={containerRef} id="pdf-div" className="h-full" data-analytics-document={document.content_type}></div>
       )}
     </>
   );
