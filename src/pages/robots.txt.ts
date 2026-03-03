ac896ee8b12d8997ab5c559b81db9517b37af6bb
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2026-03-03 18:08:16 +0000

    Hotfix robots

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -1,17 +1,17 @@
 import { GetServerSidePropsContext } from "next";
 
-const uncrawlableRoutes = ["/_search"];
+import { ROBOTS_DISALLOW_ROUTES } from "@/constants/robots";
 
 const crawlableRobotsTxt = `User-agent: *
 Allow: /
-${uncrawlableRoutes.map((route) => `Disallow: ${route}`).join("\n")}`;
+${ROBOTS_DISALLOW_ROUTES.map((route) => `Disallow: ${route}`).join("\n")}`;
 
 const uncrawlableRobotsTxt = `User-agent: *\nDisallow: /`;
 
 function Robots() {}
 
 export async function getServerSideProps({ res }: GetServerSidePropsContext) {
-  res.setHeader("Content-Type", "text/plan");
+  res.setHeader("Content-Type", "text/plain; charset=utf-8");
   res.write(process.env.ROBOTS === "true" ? crawlableRobotsTxt : uncrawlableRobotsTxt);
   res.end();
 
