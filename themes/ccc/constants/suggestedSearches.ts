fa141a60109049cbd768085bad3f71ac9b16d193
Jessica Kelsall <jessica.kelsall@climatepolicyradar.org>
2026-03-17 14:39:39 +0000

    APP-1635: CCC suggested searches fixes (#1160)
    * Add min height to CCC homepage hero area
    
    * Fixes CCC suggested searches query params

--- themes/ccc/constants/suggestedSearches.ts
+++ themes/ccc/constants/suggestedSearches.ts
@@ -7,21 +7,21 @@ export const SUGGESTED_SEARCHES: TSuggestedSearch[] = [
   {
     label: "Brazil + Federal Constitution of 1988",
     params: {
-      [QUERY_PARAMS.query_string]: "Federal Constitution of 1988",
+      [QUERY_PARAMS.concept_preferred_label]: ["principal_law/Brazil", "principal_law/Federal Constitution of 1988"],
       [QUERY_PARAMS.country]: "Brazil",
     },
   },
   {
     label: "National Environmental Policy Act (NEPA) + Alaska",
     params: {
+      [QUERY_PARAMS.concept_preferred_label]: ["principal_law/United States", "principal_law/National Environmental Policy Act (NEPA)"],
       [QUERY_PARAMS.subdivision]: "US-AK",
-      [QUERY_PARAMS.query_string]: "National Environmental Policy Act (NEPA)",
     },
   },
   {
     label: "Sub-Saharan Africa + Cases filed in last 5 years",
     params: {
-      [QUERY_PARAMS.region]: "Sub-Saharan Africa",
+      [QUERY_PARAMS.region]: "sub-saharan-africa",
       [QUERY_PARAMS.year_range]: [(thisYear - 5).toString(), thisYear.toString()],
     },
   },
