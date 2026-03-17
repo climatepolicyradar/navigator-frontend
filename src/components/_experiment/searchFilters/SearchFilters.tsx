8d352c36a8801fba223e51e85853d4fb363f6905
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2026-03-17 12:17:19 +0000

    Add filter categories

--- src/components/_experiment/searchFilters/SearchFilters.tsx
+++ src/components/_experiment/searchFilters/SearchFilters.tsx
@@ -1,5 +1,5 @@
 import { Popover as BasePopover } from "@base-ui/react/popover";
-import { ListFilter } from "lucide-react";
+import { ChevronRight, ListFilter } from "lucide-react";
 import { useEffect, useState } from "react";
 
 import { Checkbox } from "@/components/checkbox/Checkbox";
@@ -7,13 +7,18 @@ import { TLabelResult, loadLabels } from "@/hooks/useLabelSearch";
 
 import { TQueryGroup } from "../queryBuilder/QueryBuilder";
 
+type TFILTER_AGGREGATIONS = "concept" | "entity_type" | "geography" | "agent" | "activity_status" | "status";
+const FILTER_AGGREGATIONS: TFILTER_AGGREGATIONS[] = ["concept", "entity_type", "geography", "agent", "activity_status", "status"];
+
 type TProps = {
   filters?: TQueryGroup | null;
+  openFilter?: TFILTER_AGGREGATIONS;
   onChange?: (checked: boolean, label: string) => void;
 };
 
-export function SearchFilters({ filters, onChange }: TProps) {
+export function SearchFilters({ filters, openFilter, onChange }: TProps) {
   const [availableFilters, setAvailableFilters] = useState<TLabelResult[]>([]);
+  const [activeFilter, setActiveFilter] = useState<TFILTER_AGGREGATIONS>(openFilter || "concept");
 
   useEffect(() => {
     // Load some initial filter options on mount (e.g. top labels)
@@ -30,20 +35,45 @@ export function SearchFilters({ filters, onChange }: TProps) {
       <BasePopover.Portal>
         <BasePopover.Positioner positionMethod="fixed" sideOffset={8} side="bottom" align="start" className="z-50">
           <BasePopover.Popup className="w-130 rounded-xl border border-gray-200 bg-white shadow-2xl focus-visible:outline-none">
-            <div className="max-h-[60vh] overflow-y-auto p-4">
+            <div className="">
               {!availableFilters.length && <p className="text-sm text-gray-500">No filter options found.</p>}
               {availableFilters.length > 0 && (
-                <ul>
-                  {availableFilters.map((filter) => {
-                    const isChecked = filters?.filters.some((f) => "value" in f && f.value === filter.value);
+                <div className="flex gap-2">
+                  <div className="basis-1/3 pt-2 border-r border-gray-300">
+                    <ul className="flex flex-col gap-2">
+                      {FILTER_AGGREGATIONS.map((agg) => (
+                        <li key={agg} className="text-sm text-gray-700 mb-1">
+                          <button
+                            className={`relative w-full text-left px-6 py-1 text-sm text-gray-700 hover:bg-gray-300 ${activeFilter === agg ? "bg-brand! text-white!" : ""}`}
+                            onClick={() => setActiveFilter(agg)}
+                          >
+                            {agg.replace("_", " ")}
+                            {activeFilter === agg && (
+                              <ChevronRight
+                                width={20}
+                                height={20}
+                                className="text-white absolute right-2 top-1/2 transform -translate-y-1/2"
+                              ></ChevronRight>
+                            )}
+                          </button>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                  <ul className="flex flex-col gap-1 max-h-[60vh] basis-2/3 p-4 overflow-y-auto text-sm">
+                    {availableFilters
+                      .filter((filter) => filter.type === activeFilter)
+                      .map((filter) => {
+                        const isChecked = filters?.filters.some((f) => "value" in f && f.value === filter.value);
 
-                    return (
-                      <li key={filter.id}>
-                        <Checkbox label={filter.value} checked={isChecked} onChange={(checked) => onChange?.(checked, filter.value)} />
-                      </li>
-                    );
-                  })}
-                </ul>
+                        return (
+                          <li key={filter.id}>
+                            <Checkbox label={filter.value} checked={isChecked} onChange={(checked) => onChange?.(checked, filter.value)} />
+                          </li>
+                        );
+                      })}
+                  </ul>
+                </div>
               )}
             </div>
           </BasePopover.Popup>
--- src/components/checkbox/Checkbox.tsx
+++ src/components/checkbox/Checkbox.tsx
@@ -3,9 +3,9 @@ import { CheckIcon } from "lucide-react";
 
 export const Checkbox = ({ label, checked, onChange }: { label: string; checked?: boolean; onChange?: (checked: boolean) => void }) => {
   return (
-    <label className="flex items-center gap-2 text-base text-gray-900">
+    <label className="flex items-center gap-2 text-gray-900">
       <BaseCheckbox.Root
-        className="flex size-4 items-center justify-center rounded-xs focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-800 data-checked:bg-brand data-unchecked:border data-unchecked:border-gray-300"
+        className="flex size-4 items-center justify-center rounded-xs shrink-0 self-start mt-0.5 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-800 data-checked:bg-brand data-unchecked:border data-unchecked:border-gray-300"
         checked={checked}
         onCheckedChange={onChange}
       >
