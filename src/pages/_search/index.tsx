c87999683b63a012e5eaeb22144c66f88ae9ee68
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2026-03-25 17:35:14 +0000

    Rebuild intellisearch

--- src/pages/_search/index.tsx
+++ src/pages/_search/index.tsx
@@ -19,6 +19,9 @@ import { getFeatureFlags } from "@/utils/featureFlags";
 import { getFeatures } from "@/utils/features";
 import { addLabelRule, extractLabels, removeLabelRule } from "@/utils/filters/advancedFilters";
 import { readConfigFile } from "@/utils/readConfigFile";
+import { joinTailwindClasses } from "@/utils/tailwind";
+
+const columnLayoutCss = "col-start-1 -col-end-1 cols-5:col-start-3 cols-5:-col-end-3";
 
 type TProps = InferGetServerSidePropsType<typeof getServerSideProps>;
 
@@ -49,11 +52,12 @@ const ShadowSearch = ({ theme, themeConfig, features }: TProps) => {
     <FeaturesContext.Provider value={features}>
       {/* <div className="w-3/4 m-auto mt-8 pb-12 flex flex-col gap-4"> */}
       <FiveColumns className="gap-y-4">
-        <div className="col-start-3 -col-end-3">
+        <div className={columnLayoutCss}>
           <h1 className="text-5xl font-bold text-inky-black">Search</h1>
         </div>
-        <div className="col-start-3 -col-end-3">
+        <div className={columnLayoutCss}>
           <IntelliSearch
+            query={query}
             selectedLabels={selectedLabels}
             onSelectSuggestion={(suggestion) => {
               if (suggestion) {
@@ -65,7 +69,7 @@ const ShadowSearch = ({ theme, themeConfig, features }: TProps) => {
             setQuery={setQuery}
           />
         </div>
-        <div className="col-start-3 -col-end-3 flex justify-between items-center">
+        <div className={joinTailwindClasses(columnLayoutCss, "flex justify-between items-center")}>
           <SearchFilters
             availableFilters={availableFilters}
             filters={filters}
@@ -83,7 +87,7 @@ const ShadowSearch = ({ theme, themeConfig, features }: TProps) => {
           <QueryBuilder filters={filters} setFilters={setFilters} />
         </div>
         {!isFilterGroupEmpty(filters) && (
-          <div className="col-start-3 -col-end-3">
+          <div className={columnLayoutCss}>
             <AppliedLabels
               availableFilters={availableFilters}
               query={query}
@@ -98,7 +102,7 @@ const ShadowSearch = ({ theme, themeConfig, features }: TProps) => {
             />
           </div>
         )}
-        <div className="col-start-3 -col-end-3">
+        <div className={columnLayoutCss}>
           <SearchContainer
             query={query}
             onSelectLabel={(label) => {
