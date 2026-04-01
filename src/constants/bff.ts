798967007867fd7f4a3d895780ca36f76bcf1c2a
Jessica Kelsall <jessica.kelsall@climatepolicyradar.org>
2026-04-01 11:16:25 +0100

    Add all Laws & Policies corpora to data-in whitelist (#1197)
    * Ensure errors render in debug block correctly
    
    * Add all laws & policies corpora to data-in whitelist

--- src/constants/bff.ts
+++ src/constants/bff.ts
@@ -1,1 +1,8 @@
-export const BFF_TRANSFORMED_CORPORA: string[] = ["CPR.corpus.i00000001.n0000"];
+export const BFF_TRANSFORMED_CORPORA: string[] = [
+  "CPR.corpus.i00000001.n0000",
+  "CCLW.corpus.i00000001.n0000",
+  "CPR.corpus.Goldstandard.n0000",
+  "CPR.corpus.i00000589.n0000",
+  "CPR.corpus.i00000591.n0000",
+  "CPR.corpus.i00000592.n0000",
+];
