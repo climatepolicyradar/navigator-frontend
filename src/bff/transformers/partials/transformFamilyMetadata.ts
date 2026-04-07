ddb7edeaa097c7dc5dc1f5b458d2e73a58790bbc
Jessica Kelsall <jessica.kelsall@climatepolicyradar.org>
2026-04-07 16:54:04 +0100

    Migrate MCF family metadata (#1207)

--- src/bff/transformers/partials/transformFamilyMetadata.ts
+++ src/bff/transformers/partials/transformFamilyMetadata.ts
@@ -1,15 +1,51 @@
-import { TDataInLabel, TDataInLabelType } from "@/schemas";
+import { TDataInDocumentAttributes, TDataInLabel, TDataInLabelType } from "@/schemas";
 import { API_FAMILY_METADATA_KEY, TApiFamilyMetadata, TApiFamilyMetadataKey } from "@/types";
 import { TItemsByType } from "@/utils/data-in/groupByType";
 
+// For labels and attributes that are now named differently to their corresponding metadata key
+const LABEL_TO_METADATA_MAP: Partial<Record<TDataInLabelType, TApiFamilyMetadataKey>> = {
+  project_status: "status",
+};
+const ATTRIBUTE_TO_METADATA_MAP: Record<string, TApiFamilyMetadataKey> = {
+  "identifier::project_id": "project_id",
+  project_co_financing_usd: "project_value_co_financing",
+  project_fund_spend_usd: "project_value_fund_spend",
+};
+
 const isFamilyMetadataKey = (string: string): string is TApiFamilyMetadataKey => API_FAMILY_METADATA_KEY.includes(string as TApiFamilyMetadataKey);
 
-export const transformFamilyMetadata = (groupedLabels: TItemsByType<TDataInLabel, TDataInLabelType>): TApiFamilyMetadata => {
+export const transformFamilyMetadata = (
+  attributes: TDataInDocumentAttributes,
+  groupedLabels: TItemsByType<TDataInLabel, TDataInLabelType>
+): TApiFamilyMetadata => {
   const familyMetadata: TApiFamilyMetadata = {};
 
+  /* Attributes */
+
+  Object.entries(attributes).forEach(([attributeKey, value]) => {
+    if (value && isFamilyMetadataKey(attributeKey)) {
+      familyMetadata[attributeKey] = [value.toString()];
+    }
+  });
+
+  Object.entries(ATTRIBUTE_TO_METADATA_MAP).forEach(([attributeKey, metadataKey]) => {
+    if (!(attributeKey in attributes)) return;
+    const value = attributes[attributeKey as keyof TDataInDocumentAttributes];
+    familyMetadata[metadataKey] = [value.toString()];
+  });
+
+  /* Labels */
+
   Object.entries(groupedLabels).forEach(([labelType, labels]) => {
-    if (isFamilyMetadataKey(labelType)) {
+    if (labels.length > 0 && isFamilyMetadataKey(labelType)) {
       familyMetadata[labelType] = labels.map((label) => label.value.value);
+    }
+  });
+
+  Object.entries(LABEL_TO_METADATA_MAP).forEach(([labelType, metadataKey]) => {
+    const labels = groupedLabels[labelType as TDataInLabelType];
+    if (labels.length > 0) {
+      familyMetadata[metadataKey] = labels.map((label) => label.value.value);
     }
   });
 
