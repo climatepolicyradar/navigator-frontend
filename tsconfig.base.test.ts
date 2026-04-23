980bb12152fb82c5358039f3c246325d34169819
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2026-04-23 13:54:00 +0100

    Add check for length and be explicit about test condition

--- tsconfig.base.test.ts
+++ tsconfig.base.test.ts
@@ -1,7 +1,12 @@
 import config from "./tsconfig.base.json";
 
 describe("tsconfig.base.json", () => {
-  it("should not be hardcoding the theme alias path, and should contain the wildcard", () => {
-    expect(config.compilerOptions.paths["@/themes/*"]).toContain("./themes/__THEME__/*");
+  describe("the */themes alias", () => {
+    it("should only contain one path", () => {
+      expect(config.compilerOptions.paths["@/themes/*"]).toHaveLength(1);
+    });
+    it("should contain the wildcard", () => {
+      expect(config.compilerOptions.paths["@/themes/*"]).toContain("./themes/__THEME__/*");
+    });
   });
 });
