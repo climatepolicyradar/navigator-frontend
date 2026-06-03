4a6f948b57182c71b3d18706a49b1ee73768e1fd
Jessica Kelsall <jessica.kelsall@climatepolicyradar.org>
2026-06-03 13:53:41 +0100

    Fix tests

--- src/components/atoms/checkbox/Checkbox.test.tsx
+++ src/components/atoms/checkbox/Checkbox.test.tsx
@@ -1,18 +1,19 @@
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { vi } from "vitest";
 
 import { Checkbox } from "./Checkbox";
 
 describe("Checkbox", () => {
   it("does not call onCheckedChange when disabled and clicked", async () => {
-    const onCheckedChange = jest.fn();
+    const onCheckedChange = vi.fn();
     render(<Checkbox label="Test" disabled onCheckedChange={onCheckedChange} />);
     await userEvent.click(screen.getByRole("checkbox"));
     expect(onCheckedChange).not.toHaveBeenCalled();
   });
 
   it("calls onCheckedChange when clicked and not disabled", async () => {
-    const onCheckedChange = jest.fn();
+    const onCheckedChange = vi.fn();
     render(<Checkbox label="Test" onCheckedChange={onCheckedChange} />);
     await userEvent.click(screen.getByRole("checkbox"));
     expect(onCheckedChange).toHaveBeenCalledTimes(1);
