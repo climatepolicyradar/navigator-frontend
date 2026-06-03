f58ab17426f1375ab312777e10c3a120c7c5aee8
Jessica Kelsall <jessica.kelsall@climatepolicyradar.org>
2026-06-03 12:44:34 +0100

    Tests

--- src/components/atoms/checkbox/Checkbox.test.tsx
+++ src/components/atoms/checkbox/Checkbox.test.tsx
@@ -1,10 +1,20 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import { Checkbox } from "./Checkbox";
 
 describe("Checkbox", () => {
-  it("renders without crashing", () => {
-    render(<Checkbox />);
-    expect(screen.getByRole("checkbox")).toBeInTheDocument();
+  it("does not call onCheckedChange when disabled and clicked", async () => {
+    const onCheckedChange = jest.fn();
+    render(<Checkbox label="Test" disabled onCheckedChange={onCheckedChange} />);
+    await userEvent.click(screen.getByRole("checkbox"));
+    expect(onCheckedChange).not.toHaveBeenCalled();
+  });
+
+  it("calls onCheckedChange when clicked and not disabled", async () => {
+    const onCheckedChange = jest.fn();
+    render(<Checkbox label="Test" onCheckedChange={onCheckedChange} />);
+    await userEvent.click(screen.getByRole("checkbox"));
+    expect(onCheckedChange).toHaveBeenCalledTimes(1);
   });
 });
