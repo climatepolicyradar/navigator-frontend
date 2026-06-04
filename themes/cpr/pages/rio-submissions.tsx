12723e0c7fe9d61543e5711296e51956d5dc21b1
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2026-06-04 15:37:49 +0100

    Adjust colours for Rio submissions in line with new standard

--- themes/cpr/pages/rio-submissions.tsx
+++ themes/cpr/pages/rio-submissions.tsx
@@ -57,23 +57,23 @@ const RioSubmissions = () => {
       <NavBarGradient className="static!" />
       <FiveColumns>
         <div className="pt-20 col-start-1 -col-end-1 cols-3:col-start-2 cols-3:-col-end-2 cols-4:-col-end-3 cols-5:col-start-3 cols-5:-col-end-4">
-          <span className="block text-gray-700 leading-tight">Rio Policy Radar</span>
-          <h1 className="mt-3 mb-6 text-4xl cols-2:text-6xl text-gray-950 font-heavy leading-10 cols-2:leading-14 tracking-[-0.9px]">
+          <span className="block text-[#374151] leading-tight">Rio Policy Radar</span>
+          <h1 className="mt-3 mb-6 text-4xl cols-2:text-6xl text-[#030712] font-heavy leading-10 cols-2:leading-14 tracking-[-0.9px]">
             Submissions to&nbsp;the Rio&nbsp;Conventions
           </h1>
-          <p className="my-6 text-xl text-gray-950 leading-6">
+          <p className="my-6 text-xl text-[#030712] leading-6">
             Explore our curated collection that brings together and opens up dense, disparate documents on climate, nature and land.
           </p>
           <div className="flex gap-2">
             <Link
               href={{ pathname: "/search", query: { [QUERY_PARAMS.category]: "UN-submissions", [QUERY_PARAMS.author_type]: "Party" } }}
-              className="px-4 py-3 bg-brand rounded-md text-white font-medium leading-tight"
+              className="px-4 py-3 bg-inky-blue rounded-md text-white font-medium leading-tight hover:bg-inky-blue/90"
             >
               Get started
             </Link>
             <a
               href="#examples"
-              className="px-4 py-3 bg-white hover:bg-gray-100 border border-gray-300 rounded-md text-gray-700 font-medium leading-tight"
+              className="px-4 py-3 bg-white hover:bg-[#f3f4f6] border border-[#d1d5db] rounded-md text-[#374151] font-medium leading-tight"
             >
               See some examples
             </a>
@@ -95,11 +95,11 @@ const RioSubmissions = () => {
             alt="Climate Policy Radar's search page filtered by UN Submissions"
             width={1200}
             height={699}
-            className="w-full bg-white border border-gray-200 rounded-2xl shadow-[0_20px_50px_-12px_rgba(0,0,0,0.10)]0"
+            className="w-full bg-white border border-[#d1d5db] rounded-2xl shadow-[0_20px_50px_-12px_rgba(0,0,0,0.10)]0"
           />
         </Link>
         <div className="flex flex-col gap-5 items-center col-start-1 -col-end-1">
-          <span className="block text-sm text-gray-950 font-medium leading-4">In collaboration with</span>
+          <span className="block text-sm text-[#030712] font-medium leading-4">In collaboration with</span>
           <div className="flex flex-col cols-2:flex-row gap-3">
             <Image src="/images/rio/rio-unccd.svg" alt="United Nations Convention to Combat Desertification logo" width={166} height={48} />
             <Image src="/images/rio/rio-uncbd.svg" alt="Convention on Biological Diversity logo" width={166} height={48} />
@@ -110,7 +110,7 @@ const RioSubmissions = () => {
           className="mt-16 cols-2:mt-32 mb-18 col-start-1 -col-end-1 cols-4:col-start-2 cols-4:-col-end-2 cols-5:col-start-3 cols-5:-col-end-3"
           id="examples"
         >
-          <p className="mb-3 text-xl text-gray-950 font-heavy leading-7">
+          <p className="mb-3 text-xl text-[#030712] font-heavy leading-7">
             Use our collection and full-text searchable app to search and explore submissions to the Rio&nbsp;Conventions from around the world. For
             example:
           </p>
@@ -120,18 +120,18 @@ const RioSubmissions = () => {
             <li>Identify indicators used in different regions in country drought plans to the UNCCD</li>
           </ul>
           <p className="mt-10 mb-4 font-medium">Try these examples:</p>
-          <div className="flex flex-col cols-2:flex-row gap-2 cols-2:gap-8 mb-20 text-brand text-center font-medium leading-tight">
+          <div className="flex flex-col cols-2:flex-row gap-2 cols-2:gap-8 mb-20 text-text-brand text-center font-medium leading-tight">
             {QUERY_EXAMPLES.map(({ label, href }, index) => (
               <Link
                 key={index}
                 href={href}
-                className="flex-1 flex items-center justify-center px-4 py-6 border border-gray-300 rounded-md hover:bg-gray-100"
+                className="flex-1 flex items-center justify-center px-4 py-6 border border-[#d1d5db] rounded-md hover:bg-[#f3f4f6]"
               >
                 {label}
               </Link>
             ))}
           </div>
-          <h2 className="mb-2 text-xl text-gray-950 font-heavy leading-7">About</h2>
+          <h2 className="mb-2 text-xl text-[#030712] font-heavy leading-7">About</h2>
           <p className="mb-6">
             Our collection currently represents the most recent submissions from across the three Rio Conventions. We will be continually updating
             this with both historical and the latest submissions.
@@ -144,7 +144,7 @@ const RioSubmissions = () => {
             </ExternalLink>
             .
           </p>
-          <h2 className="mb-2 text-xl text-gray-950 font-heavy leading-7">Methodology</h2>
+          <h2 className="mb-2 text-xl text-[#030712] font-heavy leading-7">Methodology</h2>
           <p className="mb-6">
             This collection is powered by Climate Policy Radar, turning documents from every country into searchable, accessible, and useful
             information.
