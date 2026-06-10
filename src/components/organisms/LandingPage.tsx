2c86ba25ee32d4cf9015be3dd13709413c2f83d1
Jessica Kelsall <jessica.kelsall@climatepolicyradar.org>
2026-06-10 11:16:12 +0100

    Update grid layout at each breakpoint

--- src/components/organisms/LandingPage.tsx
+++ src/components/organisms/LandingPage.tsx
@@ -61,18 +61,20 @@ export const LandingPage = ({ config }: TProps) => {
       <Header landingPage />
       <div className="py-4 cols-4:py-12 cols-5:py-24 border-t border-t-border-light">
         <FiveColumns>
-          <div className="col-start-1 -col-end-1 cols-3:col-start-2 cols-3:-col-end-2 cols-4:col-start-1 cols-4:-col-end-3">
+          <div className="col-start-1 -col-end-1 cols-2:-col-end-2 cols-3:col-end-5 cols-4:col-end-6 cols-5:col-start-2">
             <span className="text-text-secondary leading-6">{config.hero.taxonomy}</span>
-            <h1 className="mt-0.5 mb-4 text-5xl text-text-primary font-heavy tracking-[-0.4px]">{config.hero.title}</h1>
-            <p className="text-xl text-text-primary leading-6">{config.hero.description}</p>
+            <h1 className="mt-0.5 mb-4 text-5xl text-text-primary text-balance font-heavy tracking-[-0.4px]">{config.hero.title}</h1>
+          </div>
+          <div className="col-start-1 -col-end-1 cols-5:col-start-2 cols-5:-col-end-2">
+            <p className="text-xl text-text-primary text-balance leading-6">{config.hero.description}</p>
           </div>
         </FiveColumns>
         <FiveColumns className="pt-8 cols-4:pt-12 cols-5:pt-24">
           <div className={config.background.classes}>
             <Image {...config.background.image} alt={config.background.image.alt} />
           </div>
-          <main className="col-start-1 -col-end-1 cols-3:col-start-2 cols-3:-col-end-2 cols-4:col-start-1 cols-4:-col-end-3 flex flex-col gap-8 cols-4:gap-10 cols-5:gap-12">
-            <div>
+          <main className="col-start-1 -col-end-1 cols-3:col-end-5 cols-4:col-end-7 cols-5:col-start-2 grid grid-cols-subgrid gap-y-8 cols-4:gap-y-10 cols-5:gap-y-12">
+            <div className="col-start-1 -col-end-1 cols-2:-col-end-2 cols-3:-col-end-1 cols-4:-col-end-2">
               <PageLink href="/search" query={config.search.button.query}>
                 <Button className="w-full mb-6 p-4! bg-[#005296]!">
                   <LucideSearch size={16} />
@@ -92,14 +94,14 @@ export const LandingPage = ({ config }: TProps) => {
               </ul>
             </div>
             {config.textContent.map(({ title, content }, contentIndex) => (
-              <div key={contentIndex} className="text-text-primary leading-6">
+              <div key={contentIndex} className="col-start-1 -col-end-1 text-text-primary leading-6">
                 <h2 className="text-lg font-heavy mb-3">{title}</h2>
                 {content}
               </div>
             ))}
           </main>
-          <aside className="col-start-1 -col-end-1 cols-2:col-end-3 cols-3:col-start-2 cols-3:col-end-5 cols-4:-col-start-3 cols-4:-col-end-1">
-            <div className="cols-5:min-w-50 px-5 py-4 mt-8 cols-4:mt-0 bg-white border border-border-light rounded-xl">
+          <aside className="col-span-2 cols-3:-col-end-1 cols-5:-col-end-2">
+            <div className="cols-5:min-w-50 px-5 py-4 mt-8 cols-3:mt-0 bg-white border border-border-light rounded-xl">
               <Image {...config.organisation.logoImage} alt={config.organisation.logoImage.alt} className="w-full max-w-85 mb-1" />
               <ul>
                 {config.organisation.links.map(({ externalHref, label }, linkIndex) => {
