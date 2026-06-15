af93308225a7746f1975453146d0fa16795ce942
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2026-06-15 15:03:14 +0100

    Add drag control to content within the drawer

--- src/components/atoms/drawer/DrawerNew.tsx
+++ src/components/atoms/drawer/DrawerNew.tsx
@@ -26,15 +26,21 @@ export const Drawer = ({ children, title, className, direction = "right", ...roo
     <BaseDrawer.Root {...rootProps} swipeDirection={swipeDirectionMap[direction]}>
       <BaseDrawer.Portal>
         <BaseDrawer.Backdrop className="fixed inset-0 bg-inky-black duration-200 [--backdrop-opacity:0.2] min-h-dvh opacity-[calc(var(--backdrop-opacity)*(1-var(--drawer-swipe-progress)))] transition-opacity ease-[cubic-bezier(0.32,0.72,0,1)] data-swiping:duration-0 data-ending-style:opacity-0 data-starting-style:opacity-0" />
-        <BaseDrawer.Viewport className={joinTailwindClasses(styles.Viewport, className)} data-direction={direction}>
-          <BaseDrawer.Popup className={styles.Popup} data-direction={direction}>
-            <div className="flex items-start justify-between">
-              {!!title && <BaseDrawer.Title className="text-xl font-semibold flex-1 mr-4">{title}</BaseDrawer.Title>}
+        <BaseDrawer.Viewport className={joinTailwindClasses(styles.DrawerViewport, className)} data-direction={direction}>
+          <BaseDrawer.Popup className={styles.DrawerContent} data-direction={direction}>
+            <div className="flex items-start justify-between mb-2 pb-2 border-b border-neutral-200">
+              {!!title && (
+                <BaseDrawer.Title className="text-xl font-semibold flex-1 mr-4" data-base-ui-swipe-ignore>
+                  {title}
+                </BaseDrawer.Title>
+              )}
               <BaseDrawer.Close className="text-neutral-500 hover:text-neutral-800 shrink-0 justify-end">
                 <LucideX width={20} height={20} />
               </BaseDrawer.Close>
             </div>
-            {children}
+            <div data-base-ui-swipe-ignore className="overflow-y-auto">
+              {children}
+            </div>
           </BaseDrawer.Popup>
         </BaseDrawer.Viewport>
       </BaseDrawer.Portal>
