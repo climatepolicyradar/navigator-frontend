25ec7fb79e2f03d7b4bf076f49cbcbb713d7e48e
Jessica Kelsall <jessica.kelsall@climatepolicyradar.org>
2026-07-01 15:12:27 +0100

    SearchFiltersPopover story

---
+++ src/components/molecules/searchFiltersPopover/SearchFiltersPopover.stories.tsx
@@ -0,0 +1,59 @@
+import { Meta, StoryObj } from "@storybook/nextjs-vite";
+
+import { FiltersContext } from "@/context/FiltersContext";
+import { TNestedSearchLabel } from "@/types";
+
+import { SearchFiltersPopover } from "./SearchFiltersPopover";
+
+const meta = {
+  title: "Molecules/SearchFiltersPopover",
+  component: SearchFiltersPopover,
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: {},
+  render: (props) => (
+    <FiltersContext
+      // eslint-disable-next-line no-console
+      value={{ checkedLabelPaths: [], clearFilters: () => {}, toggleFilter: (labelPath, checked) => console.info({ labelPath, checked }) }}
+    >
+      <SearchFiltersPopover {...props} />
+    </FiltersContext>
+  ),
+} satisfies Meta<typeof SearchFiltersPopover>;
+type TStory = StoryObj<typeof SearchFiltersPopover>;
+
+const generateLabel = (value: string): TNestedSearchLabel => ({
+  id: `concept::${value}`,
+  type: "region",
+  value,
+  children: [],
+});
+
+const CONCEPT_LABELS = [
+  "Adaptation enabling factor",
+  "Adaptation finance",
+  "Adaptation/resilience",
+  "Agriculture sector",
+  "Air pollution risk",
+  "Aligning skills",
+  "Ban",
+  "Bioenergy",
+  "Carbon dioxide",
+  "Central bank",
+  "Chemical impact",
+  "Climate finance",
+].map(generateLabel);
+
+export default meta;
+
+export const Default: TStory = {
+  args: {
+    filterGroup: {
+      title: "Topics",
+      container: "popover",
+      rootLabelTypes: ["concept"],
+      nestedLabels: CONCEPT_LABELS,
+    },
+  },
+};
