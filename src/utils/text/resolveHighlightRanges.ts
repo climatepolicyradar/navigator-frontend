de70652026c1c3fc77249cf987ccc98ef9a66dc6
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2026-08-26 15:29:08 +0100

    Commenst for human-readability

--- src/utils/text/resolveHighlightRanges.ts
+++ src/utils/text/resolveHighlightRanges.ts
@@ -6,12 +6,16 @@ export const resolveHighlightRanges = (text: string, ranges: THighlightRange[]):
   const length = Array.from(text).length;
   const claimedBy: (string | null)[] = new Array(length).fill(null);
 
+  // 1. Identify which highlight claims each character
+  // This is more optimal than handling the 5 cases of overlaps
+  // (no overlap, overlaps the front, sits entirely inside, overlaps the back, entirely swallows)
   ranges.forEach(({ start, end, className }) => {
     for (let index = Math.max(0, start); index < Math.min(end, length); index++) {
       if (claimedBy[index] === null) claimedBy[index] = className;
     }
   });
 
+  // 2. Resolve the claimed characters into distinct ranges
   const resolved: THighlightRange[] = [];
   claimedBy.forEach((className, index) => {
     if (className === null) return;
