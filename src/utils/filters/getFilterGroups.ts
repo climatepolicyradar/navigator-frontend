e82cc384de09fafa0d121205b16a426584ea00a2
James Gorrie <jamesgorrie@users.noreply.github.com>
2026-09-23 14:04:42 +0100

    fix: add theme filters behind feature flag (#1603)
    * fix: add theme filters behind feature flag
    
    * fix: use conditional in getFilterGroups to add themes & categories
    
    * fix: depluralise themes as per other filters
    
    * fix: use Lucide* for icons
    
    * fix: replicate filter to category in getFilterGroups
    
    * fix: title change over new category block

--- src/utils/filters/getFilterGroups.ts
+++ src/utils/filters/getFilterGroups.ts
@@ -1,4 +1,4 @@
-import { LucideEarth, LucideListFilter } from "lucide-react";
+import { LucideBook, LucideEarth, LucideListFilter } from "lucide-react";
 
 import { FilterHeaderTopics } from "@/components/fragments/filters/FilterHeaderTopics";
 import { TAppDictionaryKey } from "@/constants/text";
@@ -25,11 +25,8 @@ interface IProps {
   theme: TTheme;
 }
 
-// TODO use `features` for themes (nature project) [FUS-438]
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const getFilterGroups = ({ features, getAppText, theme }: IProps): TFiltersGroupConfig[] => {
   const filterGroups: TFiltersGroupConfig[] = [];
-
   // Filters
   filterGroups.push({
     container: "drawer",
@@ -37,8 +34,19 @@ export const getFilterGroups = ({ features, getAppText, theme }: IProps): TFilte
     prepareRootLabels: FILTER_PREP_DICTIONARY[theme] || undefined,
     rootLabelTypes: ["category"],
     subtitle: "Choose themes and specific filters to refine your search",
-    title: getAppText("filterGroupFilters"),
+    title: features.themes ? getAppText("filterGroupFilters") : "Category",
   });
+
+  if (features.themes) {
+    // Theme
+    filterGroups.push({
+      title: "Theme",
+      subtitle: "Narrow results to documents focused on specific topics",
+      Icon: LucideBook,
+      container: "drawer",
+      rootLabelTypes: ["domain"],
+    });
+  }
 
   // Geography
   filterGroups.push({
